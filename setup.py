cd1658fe0d872a376e49cd37bc7a457b14ea19d7
Zachary L Lentz <zlentz@slac.stanford.edu>
2017-03-14 13:54:10 -0700

    Put in setup.py, conda-recipe, versioneer, license, gitignore, and one device.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import versioneer
+from setuptools import (setup, find_packages)
+
+setup(name     = 'pcds-devices',
+      version  = versioneer.get_version(),
+      cmdclass = versioneer.get_cmdclass(),
+      license  = 'BSD',
+      author   = 'SLAC National Accelerator Laboratory',
+
+      packages    = find_packages(),
+      description = 'IOC definitions for LCLS Beamline Devices',
+
+    )
