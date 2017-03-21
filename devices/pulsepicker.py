43a79ae7662dadfc99c73b0fcd667e27b0e3442c
Zachary Lentz <zlentz@slac.stanford.edu>
2017-03-21 09:22:47 -0700

    Use FormattedComponent in pulsepicker

--- devices/pulsepicker.py
+++ devices/pulsepicker.py
@@ -3,7 +3,7 @@
 """
 Module to define the PulsePicker device subclass.
 """
-from ophyd import Component, EpicsSignalRO
+from ophyd import Component, FormattedComponent, EpicsSignalRO
 from .lclsdevice import LCLSDevice
 from .state import InOutStates, InOutCCMStates
 
@@ -12,11 +12,14 @@ class PulsePicker(LCLSDevice):
     """
     Device that lets us pick which beam pulses reach the sample.
     """
-    position = Component(InOutStates)
+    position = FormattedComponent(InOutStates, "{self._in_out}")
     mode = Component(EpicsSignalRO, ":SE")
 
-    def __init__(self, prefix, *, in_out_prefix=None, ioc="",
+    def __init__(self, prefix, *, in_out_prefix="", ioc="",
                  read_attrs=None, name=None, **kwargs):
+        self._in_out = in_out_prefix
+        append_kwargs = dict(ioc=ioc, read_attrs=read_attrs, name=name)
+        kwargs.update(append_kwargs)
         super().__init__(self, prefix, **kwargs)
 
 
