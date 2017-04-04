bcd0d7ed92a123b21f2d40cd176de6f37685c9e6
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-04-04 12:00:23 -0700

    Explicit import of Stopper classes

--- devices/__init__.py
+++ devices/__init__.py
@@ -1,5 +1,6 @@
 from .imsmotor import IMSMotor
 from .valve    import GateValve
+from .stopper  import Stopper, PPSStopper
 
 from ._version import get_versions
 __version__ = get_versions()['version']
