ae114e89a8d0b683b43c96a0159cf67800b24a6a
Zachary Lentz <zlentz@slac.stanford.edu>
2017-04-17 13:47:10 -0700

    Basic pv definitions

--- pcdsdevices/epics/ipm.py
+++ pcdsdevices/epics/ipm.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .iocdevice import IocDevice
+from .states import statesrecord_class, InOutStates
+from .component import Component, FormattedComponent
+from .signal import EpicsSignalRO
+
+TargetStates = statesrecord_class("TargetStates", ":OUT", ":TARGET1",
+                                  ":TARGET2", ":TARGET3", ":TARGET4")
 
 
 class IPM(IocDevice):
-    pass
+    diode = Component(InOutStates, ":DIODE")
+    target = Component(TargetStates, ":TARGET")
+    data = FormattedComponent(EpicsSignalRO, "{self._data}")
