b90952883cc1a4a777a6cb84edceecea391f4c48
Zachary Lentz <zlentz@slac.stanford.edu>
2017-04-18 17:08:19 -0700

    move requires_epics skipper to central module and fix strange typo

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -13,19 +13,7 @@ Errors will be one of:
     2. Our interface is wrong
     3. The IOC has changed or is down
 """
-import pytest
-try:
-    import epics
-    pv = epics.PV("XCS:USR:MMS:01")
-    try:
-        val = pv.get()
-    except:
-        val = None
-except:
-    val = None
-epics_subnet = val is not None
-requires_epics = pytest.mark.skipif(not epics_subnet,
-                                    reason="Could not connect to sample PV")
+from conftest import requires_epics
 
 
 @requires_epics
@@ -41,7 +29,7 @@ def recursive_not_none(namedtuple, name=""):
         if hasattr(value, "_asdict"):
             recursive_not_none(value, name)
         else:
-            assert val is not None, "Failed to find {}".format(name)
+            assert value is not None, "Failed to find {}".format(name)
 
 
 @requires_epics
