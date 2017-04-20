35fb1640a35f201496d198c778331682c35edc8a
Zachary Lentz <zlentz@slac.stanford.edu>
2017-04-19 18:27:11 -0700

    use pytest-timeout to cause epics tests to time out

--- tests/epics/test_lodcm.py
+++ tests/epics/test_lodcm.py
@@ -3,11 +3,15 @@
 """
 Test functions in lodcm that don't change live devices
 """
+import pytest
+
 from pcdsdevices.epics import lodcm
+
 from conftest import requires_epics
 
 
 @requires_epics
+@pytest.mark.timeout(10)
 def test_lodcm_reads():
     lom = lodcm.LODCM("XCS:LODCM", ioc="IOC:XCS:LODCM")
     assert(lom.destination() in dir(lom.light_states))
--- tests/epics/test_pim.py
+++ tests/epics/test_pim.py
@@ -3,11 +3,15 @@
 """
 Test functions in pim that don't change live devices
 """
+import pytest
+
 from pcdsdevices.epics import pim
+
 from conftest import requires_epics
 
 
 @requires_epics
+@pytest.mark.timeout(10)
 def test_pim_reads():
     pim6 = pim.PIM("XCS:SB2:PIM6", ioc="IOC:XCS:SB2:PIM06:IMS")
     assert(isinstance(pim6.blocking, bool))
