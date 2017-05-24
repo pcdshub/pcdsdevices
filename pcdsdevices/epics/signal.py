2db2538726f5097dada8769479531b99cac7ae9c
apra93 <dd.apra93@gmail.com>
2017-05-24 09:37:10 -0700

    Switched the class inheritance order. MRO *should* have ignored overwritten methods.

--- pcdsdevices/epics/signal.py
+++ pcdsdevices/epics/signal.py
@@ -5,16 +5,17 @@ Overrides for Epics Signals
 """
 import logging
 import ophyd.signal
+import ophyd.base
 from ..signal import Signal
 
 logger = logging.getLogger(__name__)
 
 
-class EpicsSignalBase(ophyd.signal.EpicsSignalBase, Signal):
+class EpicsSignalBase(Signal, ophyd.signal.EpicsSignalBase):
     pass
 
 
-class EpicsSignal(ophyd.signal.EpicsSignal, EpicsSignalBase):
+class EpicsSignal(EpicsSignalBase, ophyd.signal.EpicsSignal):
     def put(self, value, force=False, connection_timeout=1.0,
             use_complete=None, **kwargs):
         logger.debug("Putting PV value of %s from %s to %s",
@@ -22,6 +23,5 @@ class EpicsSignal(ophyd.signal.EpicsSignal, EpicsSignalBase):
         super().put(value, force=force, connection_timeout=connection_timeout,
                     use_complete=use_complete, **kwargs)
 
-
-class EpicsSignalRO(ophyd.signal.EpicsSignalRO, EpicsSignalBase):
+class EpicsSignalRO(EpicsSignalBase, ophyd.signal.EpicsSignalRO):
     pass
