1c2ef9755ec3fa85f8043ebe0151b8236c4740f6
apra93 <dd.apra93@gmail.com>
2017-05-24 10:36:45 -0700

    Finished adding cams.

--- pcdsdevices/epics/areadetector/cam.py
+++ pcdsdevices/epics/areadetector/cam.py
@@ -15,95 +15,95 @@ class CamBase(ophyd.cam.CamBase, ADBase):
     pass
 
 
-class AreaDetectorCam(ophyd.cam. , Cambase):
+class AreaDetectorCam(ophyd.cam.AreaDetectorCam, Cambase):
     pass
 
 
-class SimDetectorCam(ophyd.cam. , Cambase):
+class SimDetectorCam(ophyd.cam.SimDetectorCam, Cambase):
     pass
 
 
-class AdscDetectorCam(ophyd.cam. , Cambase):
+class AdscDetectorCam(ophyd.cam.AdscDetectorCam, Cambase):
     pass
 
 
-class AndorDetectorCam(ophyd.cam. , Cambase):
+class AndorDetectorCam(ophyd.cam.AndorDetectorCam, Cambase):
     pass
 
 
-class Andor3DetectorCam(ophyd.cam. , Cambase):
+class Andor3DetectorCam(ophyd.cam.Andor3DetectorCam, Cambase):
     pass
 
 
-class BrukerDetectorCam(ophyd.cam. , Cambase):
+class BrukerDetectorCam(ophyd.cam.BrukerDetectorCam, Cambase):
     pass
 
 
-class FirewireLinDetectorCam(ophyd.cam. , Cambase):
+class FirewireLinDetectorCam(ophyd.cam.FirewireLinDetectorCam, Cambase):
     pass
 
 
-class FirewireWinDetectorCam(ophyd.cam. , Cambase):
+class FirewireWinDetectorCam(ophyd.cam.FirewireWinDetectorCam, Cambase):
     pass
 
 
-class LightFieldDetectorCam(ophyd.cam. , Cambase):
+class LightFieldDetectorCam(ophyd.cam.LightFieldDetectorCam, Cambase):
     pass
 
 
-class Mar345DetectorCam(ophyd.cam. , Cambase):
+class Mar345DetectorCam(ophyd.cam.Mar345DetectorCam, Cambase):
     pass
 
 
-class MarCCDDetectorCam(ophyd.cam. , Cambase):
+class MarCCDDetectorCam(ophyd.cam.MarCCDDetectorCam, Cambase):
     pass
 
 
-class PcoDetectorCam(ophyd.cam. , Cambase):
+class PcoDetectorCam(ophyd.cam.PcoDetectorCam, Cambase):
     pass
 
 
-class PcoDetectorIO(ADBase):
+class PcoDetectorIO(ophyd.cam.PcoDetectorIO, ADBase):
     pass
 
 
-class PcoDetectorSimIO(ADBase):
+class PcoDetectorSimIO(ophyd.cam.PcoDetectorSimIO, ADBase):
     pass
 
 
-class PerkinElmerDetectorCam(ophyd.cam. , Cambase):
+class PerkinElmerDetectorCam(ophyd.cam.PerkinElmerDetectorCam, Cambase):
     pass
 
 
-class PSLDetectorCam(ophyd.cam. , Cambase):
+class PSLDetectorCam(ophyd.cam.PSLDetectorCam, Cambase):
     pass
 
 
-class PilatusDetectorCam(ophyd.cam. , Cambase):
+class PilatusDetectorCam(ophyd.cam.PilatusDetectorCam, Cambase):
     pass
 
 
-class PixiradDetectorCam(ophyd.cam. , Cambase):
+class PixiradDetectorCam(ophyd.cam.PixiradDetectorCam, Cambase):
     pass
 
 
-class PointGreyDetectorCam(ophyd.cam. , Cambase):
+class PointGreyDetectorCam(ophyd.cam.PointGreyDetectorCam, Cambase):
     pass
 
 
-class ProsilicaDetectorCam(ophyd.cam. , Cambase):
+class ProsilicaDetectorCam(ophyd.cam.ProsilicaDetectorCam, Cambase):
     pass
 
 
-class PvcamDetectorCam(ophyd.cam. , Cambase):
+class PvcamDetectorCam(ophyd.cam.PvcamDetectorCam, Cambase):
     pass
 
 
-class RoperDetectorCam(ophyd.cam. , Cambase):
+class RoperDetectorCam(ophyd.cam.RoperDetectorCam, Cambase):
     pass
 
 
-class URLDetectorCam(ophyd.cam. , Cambase):
+class URLDetectorCam(ophyd.cam.URLDetectorCam, Cambase):
     pass
 
 
