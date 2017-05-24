2cbb6b60101ae94d95f2b08ce27d5bccb04fac7a
apra93 <dd.apra93@gmail.com>
2017-05-24 10:06:29 -0700

    Reverting inheritance order.

--- pcdsdevices/epics/signal.py
+++ pcdsdevices/epics/signal.py
@@ -5,17 +5,16 @@ Overrides for Epics Signals
 """
 import logging
 import ophyd.signal
-import ophyd.base
 from ..signal import Signal
 
 logger = logging.getLogger(__name__)
 
 
-class EpicsSignalBase(Signal, ophyd.signal.EpicsSignalBase):
+class EpicsSignalBase(ophyd.signal.EpicsSignalBase, Signal):
     pass
 
 
-class EpicsSignal(EpicsSignalBase, ophyd.signal.EpicsSignal):
+class EpicsSignal(ophyd.signal.EpicsSignal, EpicsSignalBase):
     def put(self, value, force=False, connection_timeout=1.0,
             use_complete=None, **kwargs):
         logger.debug("Putting PV value of %s from %s to %s",
@@ -23,5 +22,6 @@ class EpicsSignal(EpicsSignalBase, ophyd.signal.EpicsSignal):
         super().put(value, force=force, connection_timeout=connection_timeout,
                     use_complete=use_complete, **kwargs)
 
-class EpicsSignalRO(EpicsSignalBase, ophyd.signal.EpicsSignalRO):
+
+class EpicsSignalRO(ophyd.signal.EpicsSignalRO, EpicsSignalBase):
     pass
