cbdab8a0649a2725bd6f5149597e066032268aef
apra93 <dd.apra93@gmail.com>
2017-05-28 23:40:10 -0700

    Added tests for all the classes in pim

--- tests/epics/test_pim.py
+++ tests/epics/test_pim.py
@@ -10,14 +10,27 @@ from pcdsdevices.epics import pim
 from conftest import requires_epics
 
 
-@requires_epics
-@pytest.mark.timeout(3)
-def test_pim_reads():
-    pim6 = pim.PIM("XCS:SB2:PIM6")
-    assert(isinstance(pim6.blocking, bool))
+# @requires_epics
+# @pytest.mark.timeout(3)
+# def test_pim_motor_reads():
+#     pim6 = pim.PIMMotor("XCS:SB2:PIM6")
+#     assert(pim6.read())
 
-@requires_epics
-@pytest.mark.timeout(3)
-def test_pim_imager_reads():
-    dg3 = pim.PIMYag("HFX:DG3:PIM", imager="HFX:DG3:CVV:01")
+# @requires_epics
+# @pytest.mark.timeout(3)
+# def test_pim_motor_blocking_is_bool():
+#     pim6 = pim.PIMMotor("XCS:SB2:PIM6")
+#     assert(isinstance(pim6.blocking, bool))
+
+# @requires_epics
+# @pytest.mark.timeout(3)
+# def test_pim_pulnix_detector_reads():
+#     dg3 = pim.PIMPulnixDetector("HFX:DG3:CVV:01")
+#     assert(dg3.read())
+
+# @requires_epics
+# @pytest.mark.timeout(3)
+# def test_pim_reads():
+#     dg3_pim = pim.PIM("HFX:DG3:PIM", imager="HFX:DG3:CVV:01")
+#     assert(dg3_pim.read())
 
