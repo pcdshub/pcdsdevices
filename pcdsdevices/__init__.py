7ba0dc990ad1d0e4a314d11ecf5fcb5ef4920cc4
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-08-03 13:59:13 -0700

    MAINT: Added a warning for failed import from epics
    The try / except block failed silently on an ImportError from the epics
    sub-directory. This bamboozled me for a minute or two. Figured we should have
    this fail louder

--- pcdsdevices/__init__.py
+++ pcdsdevices/__init__.py
@@ -5,12 +5,13 @@ del get_versions
 #import logging # NOQA
 #logging.basicConfig(level=logging.INFO)
 #logger = logging.getLogger(__name__)
-
+import warnings
 from .device import Device # NOQA
 
 # Let the submodules decide what to push up to the top level
 # Assume ImportError means that we can't use the optional submodule
 try:
     from .epics import * # NOQA
-except ImportError:
-    pass
+except ImportError as e:
+    warnings.warn("epics submodule did not import {} correctly"
+                  "".format(e), RuntimeWarning)
