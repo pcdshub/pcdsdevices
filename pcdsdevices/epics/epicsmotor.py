95323f4dfe8d013dd865fd3a5749458afe0e4cbf
apra93 <dd.apra93@gmail.com>
2017-09-01 11:20:26 -0700

    ENH: Added components for the low and high soft limits, offset, direction and zero_all along with convenience properties and methods.

--- pcdsdevices/epics/epicsmotor.py
+++ pcdsdevices/epics/epicsmotor.py
@@ -1,12 +1,112 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import ophyd
-from .device import Device
+############
+# Standard #
+############
+import logging
+from enum import Enum
 
-class EpicsMotor(ophyd.EpicsMotor, Device):
+###############
+# Third Party #
+###############
+from ophyd import EpicsMotor
+
+##########
+# Module #
+##########
+from .device import Device
+from .component import Component
+from .signal import EpicsSignal
+
+logger = logging.getLogger(__name__)
+
+
+class DirectionEnum(Enum): 
+    positive = 0
+    pos = 0
+    negative = 1
+    neg = 1
+
+class EpicsMotor(EpicsMotor, Device):
     """
     Epics motor for PCDS.
     """
+    llm = Component(EpicsSignal, ".LLM")
+    hlm = Component(EpicsSignal, ".HLM")
+    offset_val = Component(EpicsSignal, ".OFF")
+    direction_enum = Component(EpicsSignal, ".DIR")
+    zero_all_proc = Component(EpicsSignal, ".ZERO_P.PROC")
+
+    @EpicsMotor.low_limit.setter
+    def low_limit(self, value):
+        """
+        Sets the low limit for the motor.
+
+        Returns
+        -------
+        status : StatusObject
+            Status object of the set.
+        """
+        return self.llm.set(value)
+
+    @EpicsMotor.high_limit.setter
+    def low_limit(self, value):
+        """
+        Sets the low limit for the motor.
+
+        Returns
+        -------
+        status : StatusObject
+            Status object of the set.
+        """
+        return self.hlm.set(value)
+
+    @property
+    def direction(self):
+        """
+        Returns the current direction of the motor.
+        """
+        return DirectionEnum(self.direction_enum.value).name
+
+    @direction.setter
+    def direction(self, val):
+        """
+        Sets the direction of the motor
+
+        Parameters
+        ----------
+        val : int, str
+            The desired direction as an enum or string.
+        """
+        if isinstance(val, int):
+            self.direction_enum.put(DirectionEnum(val).value)
+        else:
+            self.direction_enum.put(DirectionEnum[val.lower()].value)
+
+    @property
+    def offset(self):
+        """
+        Returns the current offset of the motor.
+        
+        Returns
+        -------
+        offset : float
+            Current user off set of the motor.
+        """
+        return self.offset_val.value
+
+    @offset.setter
+    def offset(self, val):
+        """
+        Sets the offset of the motor.
+
+        Parameters
+        ----------
+        val : float
+            The desired offset.
+        """
+        self.offset_val.put(val)
+
     def move_rel(self, rel_position, *args, **kwargs):
         """
         Move relative to the current position, optionally waiting for motion to
@@ -78,3 +178,14 @@ class EpicsMotor(ophyd.EpicsMotor, Device):
             Current readback position of the motor.
         """
         return self.position
+
+    def zero_all(self):
+        """
+        Sets the current position to be the zero position of the motor.
+
+        Returns
+        -------
+        status : StatusObject        
+            Status object for the set.
+        """
+        self.zero_all_proc.set(1)
