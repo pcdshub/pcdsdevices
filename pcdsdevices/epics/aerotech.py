e70f490ab3bde5ca0b6ff377b4e74349d670a7b0
apra93 <dd.apra93@gmail.com>
2017-08-31 20:16:38 -0700

    MAINT: Moving move_rel and move aliases to the base EpicsMotor class.

--- pcdsdevices/epics/aerotech.py
+++ pcdsdevices/epics/aerotech.py
@@ -107,78 +107,6 @@ class AeroBase(EpicsMotor):
             raise MotorDisabled(err)
         return super().move(position, *args, **kwargs)
 
-    def move_rel(self, rel_position, *args, **kwargs):
-        """
-        Move relative to the current position, optionally waiting for motion to
-        complete.
-
-        Parameters
-        ----------
-        rel_position
-            Relative position to move to
-
-        moved_cb : callable
-            Call this callback when movement has finished. This callback must
-            accept one keyword argument: 'obj' which will be set to this
-            positioner instance.
-
-        timeout : float, optional
-            Maximum time to wait for the motion. If None, the default timeout
-            for this positioner is used.
-
-        Returns
-        -------
-        status : MoveStatus        
-            Status object for the move.
-        
-        Raises
-        ------
-        TimeoutError
-            When motion takes longer than `timeout`
-        
-        ValueError
-            On invalid positions
-        
-        RuntimeError
-            If motion fails other than timing out        
-        """
-        return self.move(rel_position + self.position, *args, **kwargs)
-
-    def mv(self, position, *args, **kwargs):
-        """
-        Move to a specified position, optionally waiting for motion to
-        complete. Alias for move().
-
-        Returns
-        -------
-        status : MoveStatus        
-            Status object for the move.
-        """
-        return self.move(position, *args, **kwargs)
-
-    def mvr(self, rel_position, *args, **kwargs):
-        """
-        Move relative to the current position, optionally waiting for motion to
-        complete. Alias for move_rel().
-
-        Returns
-        -------
-        status : MoveStatus        
-            Status object for the move.
-        """
-        return self.move_rel(rel_position, *args, **kwargs)
-
-    def wm(self):
-        """
-        Returns the current position of the motor.
-
-        Returns
-        -------
-        position : float
-            Current readback position of the motor.
-        """
-        return self.position
-
     def enable(self):
         """
         Enables the motor power.
@@ -236,7 +164,7 @@ class AeroBase(EpicsMotor):
         return self.config.set(1)
 
     @property
-    def fault(self):
+    def faulted(self):
         """
         Returns the current fault with the motor.
         
--- pcdsdevices/epics/epicsmotor.py
+++ pcdsdevices/epics/epicsmotor.py
@@ -4,4 +4,77 @@ import ophyd
 from .device import Device
 
 class EpicsMotor(ophyd.EpicsMotor, Device):
-    pass
+    """
+    Epics motor for PCDS.
+    """
+    def move_rel(self, rel_position, *args, **kwargs):
+        """
+        Move relative to the current position, optionally waiting for motion to
+        complete.
+
+        Parameters
+        ----------
+        rel_position
+            Relative position to move to
+
+        moved_cb : callable
+            Call this callback when movement has finished. This callback must
+            accept one keyword argument: 'obj' which will be set to this
+            positioner instance.
+
+        timeout : float, optional
+            Maximum time to wait for the motion. If None, the default timeout
+            for this positioner is used.
+
+        Returns
+        -------
+        status : MoveStatus        
+            Status object for the move.
+        
+        Raises
+        ------
+        TimeoutError
+            When motion takes longer than `timeout`
+        
+        ValueError
+            On invalid positions
+        
+        RuntimeError
+            If motion fails other than timing out        
+        """
+        return self.move(rel_position + self.position, *args, **kwargs)
+
+    def mv(self, position, *args, **kwargs):
+        """
+        Move to a specified position, optionally waiting for motion to
+        complete. Alias for move().
+
+        Returns
+        -------
+        status : MoveStatus        
+            Status object for the move.
+        """
+        return self.move(position, *args, **kwargs)
+
+    def mvr(self, rel_position, *args, **kwargs):
+        """
+        Move relative to the current position, optionally waiting for motion to
+        complete. Alias for move_rel().
+
+        Returns
+        -------
+        status : MoveStatus        
+            Status object for the move.
+        """
+        return self.move_rel(rel_position, *args, **kwargs)
+
+    def wm(self):
+        """
+        Returns the current position of the motor.
+
+        Returns
+        -------
+        position : float
+            Current readback position of the motor.
+        """
+        return self.position
