68dc93c380514d01ce5162d12bddbb9083d5ae4b
Zachary Lentz <zlentz@slac.stanford.edu>
2017-10-19 13:26:25 -0700

    ENH: Closer to being done

--- pcdsdevices/epics/lodcm.py
+++ pcdsdevices/epics/lodcm.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from enum import Enum
-
 from ..interface import BranchingInterface
 from .device import Device
-from .state import statesrecord_class
 from .state import statesrecord_class, InOutStates
 from .component import Component
 
@@ -18,7 +15,7 @@ FoilStates = statesrecord_class("FoilStates", ":OUT", ":Mo", ":Zr", ":Zn",
                                 ":Cu", ":Ni", ":Fe", "Ti")
 
 
-class LODCM(Device metaclass=BranchingInterface):
+class LODCM(Device, metaclass=BranchingInterface):
     h1n_state = Component(LodcmStates, ":H1N")
     h2n_state = Component(LodcmStates, ":H2N")
     yag_state = Component(YagLomStates, ":DV")
@@ -26,58 +23,67 @@ class LODCM(Device metaclass=BranchingInterface):
     diode_state = Component(InOutStates, ":DIODE")
     foil_state = Component(FoilStates, ":FOIL")
 
-    light_states = Enum("LightStates", "BLOCKED MAIN MONO BOTH UNKNOWN",
-                        start=0)
-
     def __init__(self, prefix, *, name, main_name='MAIN', mono_name='MONO',
                  **kwargs):
         self.main_name = main_name
         self.mono_name = mono_name
         super().__init__(prefix, name=name, **kwargs)
 
-    def destination(self, line=None):
+    @property
+    def destination(self):
         """
-        Return where the light is going for a given line at the current LODCM
+        Return where the light is going at the current LODCM
         state.
-
-        Parameters
-        ----------
-        line: str or int, optional
-            The starting line for the beam. If this is 1 or "MAIN", light is
-            coming in on H1N from the main line. If this is 2 or "MONO", light
-            is coming in on H2N to the mono line (e.g. XCS periscope)
 
         Returns
         -------
-        destination: str
-            "BLOCKED" if the light (probably) does not go through.
-            "MAIN" if the light continues on the main line.
-            "MONO" if the light continues on the mono line.
-            "BOTH" if the light continues on both lines.
-            "UNKNOWN" if the state is strange.
+        destination: list of str
+            self.main_name if the light continues on the main line.
+            self.mono_name if the light continues on the mono line.
         """
-        if line is None:
-            line = self.light_states.MAIN
+        # H2N:     OUT      C       Si
+        table = [["MAIN", "MAIN", "MAIN"],  # H1N at OUT
+                 ["MAIN", "BOTH", "MAIN"],  # H1N at C
+                 ["BLOCKED", "BLOCKED", "MONO"]]  # H1N at Si
+        n1 = ("OUT", "C", "Si").index(self.h1n_state.value)
+        n2 = ("OUT", "C", "Si").index(self.h2n_state.value)
+        state = table[n1][n2]
+        if state == "MAIN":
+            return [self.main_name]
+        elif state == "BLOCKED":
+            return []
         else:
-            try:
-                line = self.light_states[line]
-            except:
-                line = self.light_states(line)
-        if line == self.light_states.MAIN:
-            # H2N:     OUT      C       Si
-            table = [["MAIN", "MAIN", "MAIN"],  # H1N at OUT
-                     ["MAIN", "BOTH", "MAIN"],  # H1N at C
-                     ["BLOCKED", "BLOCKED", "MONO"]]  # H1N at Si
-            try:
-                n1 = ("OUT", "C", "Si").index(self.h1n_state.value)
-                n2 = ("OUT", "C", "Si").index(self.h2n_state.value)
-            except ValueError:
-                return "UNKNOWN"
-            return table[n1][n2]
-        elif line == self.light_states.MONO:
-            table = ["MONO", "BLOCKED", "BLOCKED"]
-            try:
-                n2 = ("OUT", "C", "Si").index(self.h2n_state.value)
-            except ValueError:
-                return "UNKNOWN"
-            return table[n2]
+            if state == "MONO" and self.diag_clear:
+                return [self.mono_name]
+            if state == "BOTH":
+                if self.diag_clear:
+                    return [self.main_name, self.mono_name]
+                else:
+                    return [self.main_name]
+        return []
+
+    @property
+    def branches(self):
+        return [self.main_name, self.mono_name]
+
+    @property
+    def diag_clear(self):
+        yag_clear = self.yag_state.value == 'OUT'
+        dectris_clear = self.dectris_state.value in ('OUT', 'OUTLOW')
+        diode_clear = self.diode_state.value in ('IN', 'OUT')
+        foil_clear = self.foil_state.value == 'OUT'
+        return all(yag_clear, dectris_clear, diode_clear, foil_clear)
+
+    @property
+    def inserted(self):
+        return not (self.destination and self.diag_clear)
+
+    @property
+    def removed(self):
+        return self.diag_clear or self.destination == self.main
+
+    def remove(self, wait=False, timeout=None, finished_cb=None, **kwargs):
+        yag_status = self.yag_state.move('OUT')
+        dectris_status = self.dectris_state.move('OUT')
+        diode_status = self.diode_state.move('OUT')
+        foil_status = self.foil_state.move('OUT')
