1d01096643084fbeafef63e18b8cbea955bbbb41
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-10-19 11:24:49 -0700

    MAINT: Add :COM to prefix in Attenuator __init__

--- pcdsdevices/epics/attenuator.py
+++ pcdsdevices/epics/attenuator.py
@@ -490,7 +490,9 @@ class AttenuatorBase(BasicAttenuatorBase):
 
     def __init__(self, prefix, *, name=None, read_attrs=None,
                  stage_setting=0, **kwargs):
-        self._filter_prefix = prefix.rstrip(':COM')
+        #Modify prefixes
+        self._filter_prefix = prefix
+        prefix += ':COM'
         if read_attrs is None:
             read_attrs = ["transmission", "transmission_3rd"]
         super().__init__(prefix, name=name, read_attrs=read_attrs,
