7a98d42f497e1f3045e18acab63c19daf5187d36
Zachary Lentz <zlentz@slac.stanford.edu>
2017-10-18 17:53:41 -0700

    ENH: Add basic reflaser with lightinterface

---
+++ pcdsdevices/epics/reflaser.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from ..interface import LightInterface
+from .device import Device
+from .component import Component as Cmp
+from .state import InOutStates, statesrecord_class
+
+class Reflaser(Device):
+    state = Cmp(InOutStates, '')
+
+    @property
+    def inserted(self):
+        return self.state.value == 'IN'
+
+    @property
+    def removed(self):
+        return self.state.value == 'OUT'
+
+    def remove(self, wait=False, timeout=None, finished_cb=None, **kwargs):
+        return self.state.move('OUT', moved_cb=finished_cb, timeout=timeout,
+                               wait=wait, **kwargs)
+
+    def subscribe(self, cb, event_type=None, run=False, **kwargs):
+        self.state.subscribe(cb, event_type=event_type, run=run, **kwargs)
+
+
+TTStates = statesrecord_class('TTStates', ':TT', ':REFL', ':OUT')
+
+class TTReflaser(Reflaser):
+    state = Cmp(TTStates, '')
+
+    @property
+    def inserted(self):
+        return self.state.value in ('TT', 'REFL')
