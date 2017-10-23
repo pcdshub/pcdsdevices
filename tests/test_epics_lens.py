576a83cc78f169ea486ee9e192c3bdbc1c210bed
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-10-23 16:24:59 -0700

    TST: Basic testing of XFLS functionality

---
+++ tests/test_epics_lens.py
@@ -0,0 +1,51 @@
+############
+# Standard #
+############
+from unittest.mock import Mock
+
+###############
+# Third Party #
+###############
+import pytest
+
+##########
+# Module #
+##########
+from pcdsdevices.sim.pv import using_fake_epics_pv
+from pcdsdevices.epics  import XFLS
+
+
+@using_fake_epics_pv
+@pytest.fixture(scope='function')
+def xfls():
+    return XFLS("TST:XFLS")
+
+
+@using_fake_epics_pv
+def test_xfls_states(xfls):
+    #Remove
+    xfls.state._read_pv.put(4)
+    assert xfls.removed
+    assert not xfls.inserted
+    #Insert
+    xfls.state._read_pv.put(3)
+    assert not xfls.removed
+    assert xfls.inserted
+    #Unknown
+    xfls.state._read_pv.put(0)
+    assert not xfls.removed
+    assert not xfls.inserted
+
+@using_fake_epics_pv
+def test_xfls_motion(xfls):
+    xfls.remove()
+    assert xfls.state._write_pv.get() == 4
+
+@using_fake_epics_pv
+def test_xfls_subscriptions(xfls):
+    #Subscribe a pseudo callback
+    cb = Mock()
+    xfls.subscribe(cb, run=False)
+    #Change readback state
+    xfls.state._read_pv.put(4)
+    assert cb.called
