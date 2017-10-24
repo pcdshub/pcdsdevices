f68a680f2fd26b176c87493bdf860f29efec4827
Zachary Lentz <zlentz@slac.stanford.edu>
2017-10-24 14:41:37 -0700

    TST: Add super basic tests

---
+++ tests/test_lodcm.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+
+from pcdsdevices.sim.pv import using_fake_epics_pv
+from pcdsdevices.epics.lodcm import LODCM
+
+
+@pytest.fixture(scope='function')
+def lodcm():
+    return LODCM('FAKE:LOM', name='fake_lom')
+
+
+# TBH we'll just call all light interface items and hope for the best
+@using_fake_epics_pv
+def test_destination(lodcm):
+    dest = lodcm.destination
+    assert isinstance(dest, list)
+    for d in dest:
+        assert isinstance(d, str)
+
+
+@using_fake_epics_pv
+def test_branches(lodcm):
+    branches = lodcm.branches
+    assert isinstance(branches, list)
+    for b in branches:
+        assert isinstance(b, str)
+
+
+@using_fake_epics_pv
+def test_inserted(lodcm):
+    inserted = lodcm.inserted
+    assert isinstance(inserted, bool)
+
+
+@using_fake_epics_pv
+def test_removed(lodcm):
+    removed = lodcm.removed
+    assert isinstance(removed, bool)
+
+
+@using_fake_epics_pv
+def test_remove(lodcm):
+    lodcm.remove()
