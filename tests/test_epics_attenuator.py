e0bc1efab9a43d15311adfe8c9346634fc8bbc28
Zachary Lentz <zlentz@slac.stanford.edu>
2017-11-02 11:15:10 -0700

    TST: No longer guaranteed that FakePV callbacks run instantly, more closely copies real PV.

--- tests/test_epics_attenuator.py
+++ tests/test_epics_attenuator.py
@@ -2,6 +2,7 @@ import logging
 
 import pytest
 from unittest.mock import Mock
+from ophyd.status import wait as status_wait
 
 from pcdsdevices.sim.pv import using_fake_epics_pv
 from pcdsdevices.epics import FeeAtt
@@ -31,13 +32,15 @@ def test_attenuator_states(attenuator):
 
 @using_fake_epics_pv
 def test_attenuator_motion(attenuator):
-    # Remove Attenuator Filters
+    # Call remove method
     status = attenuator.remove(wait=False)
     # Check we wrote to the correct position
     assert attenuator.go_cmd.get() == 0
-    # Remove filters
+    # Remove fake pv filters (IOC normally does this)
     for filt in attenuator.filters:
         filt.state_sig._read_pv.put(filt.filter_states.OUT.value)
+    # Not guaranteed that callback comes in right away
+    status_wait(status, timeout=1)
     # Check status has been marked as completed
     assert status.done and status.success
 
@@ -49,4 +52,8 @@ def test_attenuator_subscriptions(attenuator):
     attenuator.subscribe(cb, event_type=attenuator.SUB_STATE,  run=False)
     # Change the target state
     attenuator.filter1.state_sig._read_pv.put(1)
+    # Not guaranteed that callback comes in right away
+    tmo = 1
+    while not cb.called and tmo > 0:
+        tmo -= 0.1
     assert cb.called
