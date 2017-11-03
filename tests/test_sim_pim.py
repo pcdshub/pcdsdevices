86009b01de265acb17b2a762aafef304e9974107
Zachary Lentz <zlentz@slac.stanford.edu>
2017-11-02 18:42:32 -0700

    TST: name is now a required kwarg

--- tests/test_sim_pim.py
+++ tests/test_sim_pim.py
@@ -20,21 +20,21 @@ from pcdsdevices.sim.pim import (PIMPulnixDetector, PIMMotor, PIM)
 # PIMPulnixDetector Tests
 
 def test_PIMPulnixDetector_instantiates():
-    assert(PIMPulnixDetector("TEST"))
+    assert(PIMPulnixDetector("TEST", name="test"))
 
 def test_PIMPulnixDetector_runs_ophyd_functions():
-    pim_pdet = PIMPulnixDetector("TEST")
+    pim_pdet = PIMPulnixDetector("TEST", name="test")
     assert(isinstance(pim_pdet.read(), OrderedDict))
     assert(isinstance(pim_pdet.describe(), OrderedDict))
     assert(isinstance(pim_pdet.describe_configuration(), OrderedDict))
     assert(isinstance(pim_pdet.read_configuration(), OrderedDict))
 
 def test_PIMPulnixDetector_stats_plugin_reads():
-    pim_pdet = PIMPulnixDetector("TEST")
+    pim_pdet = PIMPulnixDetector("TEST", name="test")
     assert(isinstance(pim_pdet.stats2.read(), OrderedDict))
 
 def test_PIMPulnixDetector_centroid_noise():
-    pim_pdet = PIMPulnixDetector("TEST", noise_x=5, noise_y=20)
+    pim_pdet = PIMPulnixDetector("TEST", name="test", noise_x=5, noise_y=20)
     pim_pdet.stats2.centroid.x.put(300)
     pim_pdet.stats2.centroid.y.put(400)
     test_x = 300 + int(np.round(np.random.uniform(-1,1)*5))
@@ -45,7 +45,7 @@ def test_PIMPulnixDetector_centroid_noise():
            np.isclose(pim_pdet.centroid_y, 400, atol=20))
     
 def test_PIMPulnixDetector_centroid_override():
-    pim_pdet = PIMPulnixDetector("TEST")
+    pim_pdet = PIMPulnixDetector("TEST", name="test")
     def centroid_override_x():
         val = pim_pdet.stats2.centroid.x._raw_readback + (
           pim_pdet.stats2.centroid.x._raw_readback or 5)
@@ -66,7 +66,7 @@ def test_PIMPulnixDetector_centroid_override():
     assert(pim_pdet.centroid_y == 3)
 
 def test_PIMPulnixDetector_zeros_centroid_outside_yag():
-    pim_pdet = PIMPulnixDetector("TEST", zero_outside_yag=True)
+    pim_pdet = PIMPulnixDetector("TEST", name="test", zero_outside_yag=True)
     pim_pdet.stats2.centroid.x.put(300)
     pim_pdet.stats2.centroid.y.put(400)
     assert(pim_pdet.centroid_x == 300)
@@ -107,17 +107,17 @@ def test_PIMPulnixDetector_zeros_centroid_outside_yag():
 # PIMMotor Tests
 
 def test_PIMMotor_instantiates():
-    assert(PIMMotor("TEST"))
+    assert(PIMMotor("TEST", name="test"))
 
 def test_PIMMotor_runs_ophyd_functions():
-    pmotor = PIMMotor("TEST")
+    pmotor = PIMMotor("TEST", name="test")
     assert(isinstance(pmotor.read(), OrderedDict))
     assert(isinstance(pmotor.describe(), OrderedDict))
     assert(isinstance(pmotor.describe_configuration(), OrderedDict))
     assert(isinstance(pmotor.read_configuration(), OrderedDict))
 
 def test_PIMMotor_transitions_states_correctly():
-    pmotor = PIMMotor("TEST")
+    pmotor = PIMMotor("TEST", name="test")
     status = pmotor.move("OUT")
     assert(pmotor.position == "OUT")
     assert(status.success)
@@ -129,14 +129,14 @@ def test_PIMMotor_transitions_states_correctly():
     assert(status.success)
 
 def test_PIMMotor_noise_on_read():
-    pmotor = PIMMotor("TEST", pos_in=5, noise=True, noise_kwargs={'scale':0.1})
+    pmotor = PIMMotor("TEST", name="test", pos_in=5, noise=True, noise_kwargs={'scale':0.1})
     status = pmotor.move("IN")
     assert(status.success)
     assert(pmotor._pos.value !=  5 and np.isclose(
         pmotor._pos.value, 5, atol=0.1))
 
 def test_PIMMotor_noise_changes_every_read():
-    pmotor = PIMMotor("TEST", pos_in=5, noise=True, noise_kwargs={'scale':0.1})
+    pmotor = PIMMotor("TEST", name="test", pos_in=5, noise=True, noise_kwargs={'scale':0.1})
     status = pmotor.move("IN")
     assert(status.success)
     pos = [pmotor._pos.value for i in range(10)]
@@ -145,22 +145,22 @@ def test_PIMMotor_noise_changes_every_read():
 # PIM Tests
 
 def test_PIM_instantiates():
-    assert(PIM("TEST"))
+    assert(PIM("TEST", name="test"))
 
 def test_PIM_runs_ophyd_functions():
-    pim = PIM("TEST")
+    pim = PIM("TEST", name="test")
     assert(isinstance(pim.read(), OrderedDict))
     assert(isinstance(pim.describe(), OrderedDict))
     assert(isinstance(pim.describe_configuration(), OrderedDict))
     assert(isinstance(pim.read_configuration(), OrderedDict))
     
 def test_PIM_has_centroid():
-    pim = PIM("TEST")
+    pim = PIM("TEST", name="test")
     assert(isinstance(pim.detector.stats2.centroid.x.read(), dict))
     assert(isinstance(pim.detector.stats2.centroid.y.read(), dict))
     
 def test_PIM_settle_time():
-    pim = PIM("TEST")
+    pim = PIM("TEST", name="test")
     pim.move("OUT")
     pim.settle_time = 1
     t0 = time.time()
@@ -171,13 +171,13 @@ def test_PIM_settle_time():
     assert(status.success)
 
 def test_PIM_noise():
-    pim = PIM("TEST", y=5, noise=True, noise_kwargs={'scale':0.1})
+    pim = PIM("TEST", name="test", y=5, noise=True, noise_kwargs={'scale':0.1})
     status = pim.move("IN")
     assert(status.success)
     assert(pim.sim_y.value != 5 and np.isclose(pim.sim_y.value, 5, atol=0.1))
 
 def test_PIMMotor_stage():
-    pim = PIMMotor("TEST", pos_in=5)
+    pim = PIMMotor("TEST", name="test", pos_in=5)
     pim.move_out(wait=True)
     pim.stage()
     assert(pim.position == "OUT")
@@ -190,6 +190,6 @@ def test_PIMMotor_stage():
 @pytest.mark.skipif(True, reason="No proper states sim class yet")
 def test_PIM_timeout():
     tmo = 1.0
-    pim = PIM("TEST", timeout=tmo)
+    pim = PIM("TEST", name="test", timeout=tmo)
     pim.move_in()
     assert(pim.states.timeout == tmo)
