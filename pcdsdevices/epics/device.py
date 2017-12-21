6786b759a30b4747b1b6dd6160b47510afbb7a7f
teddyrendahl <trendahl@slac.stanford.edu>
2017-12-21 13:28:42 -0800

    DEP: Deprecate ophyd overrides

--- pcdsdevices/epics/device.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Overrides to ophyd.Device that are relevant for epics devices.
-"""
-from ..device import (Device, DynamicDeviceComponent)
-# Nothing here right now, but there could be later
