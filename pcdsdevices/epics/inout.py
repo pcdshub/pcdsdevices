82a035869683a5a7bd7408fd93c8a4d1ed91e46d
Zachary Lentz <zlentz@slac.stanford.edu>
2018-01-09 11:28:38 -0800

    MNT: Rewrite inout to use new state positioner

--- pcdsdevices/epics/inout.py
+++ pcdsdevices/epics/inout.py
@@ -1,64 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from ophyd import Device, Component as C
+from enum import Enum
 
-from ..state import InOutStates, statesrecord_class
+from ..state import StatePositioner
 
 
-class InOutDevice(Device):
-    """
-    Device that has two states, IN and OUT, that blocks the beam while IN.
-    """
-    state = C(InOutStates, '')
-    #Subscription types
-    SUB_STATE = 'sub_state_changed'
-    _default_sub = SUB_STATE
-
-    def __init__(self, *args, **kwargs):
-        self._has_subscribed = False
-        super().__init__(*args, **kwargs)
+class InOutPositioner(StatePositioner):
+    _states_enum = Enum('InOutState', 'IN OUT')
 
     @property
     def inserted(self):
-        return self.state.value == 'IN'
+        return self.position == 'IN'
 
     @property
     def removed(self):
-        return self.state.value == 'OUT'
+        return self.position == 'OUT'
 
     def remove(self, wait=False, timeout=None, finished_cb=None, **kwargs):
         return self.state.move('OUT', moved_cb=finished_cb, timeout=timeout,
                                wait=wait, **kwargs)
 
-    def subscribe(self, cb, event_type=None, run=False):
-        """
-        Subscribe to changes of the InOutDevice
 
-        Parameters
-        ----------
-        cb : callable
-            Callback to be run
-
-        event_type : str, optional
-            Type of event to run callback on
-
-        run : bool, optional
-            Run the callback immediatelly
-        """
-        if not self._has_subscribed:
-            self.state.subscribe(self._on_state_change, run=False)
-            self._has_subscribed = True
-        super().subscribe(cb, event_type=event_type, run=run)
-
-    def _on_state_change(self, **kwargs):
-        """
-        Callback run on state change
-        """
-        kwargs.pop('sub_type', None)
-        kwargs.pop('obj', None)
-        self._run_subs(sub_type=self.SUB_STATE, obj=self, **kwargs)
-
-class Reflaser(InOutDevice):
+class Reflaser(InOutPositioner):
     """
     Mirror that is inserted into the beam to point a reference laser along the
     beam path.
@@ -70,9 +31,8 @@ class TTReflaser(Reflaser):
     """
     Motor stack that includes both a timetool and a reflaser.
     """
-    TTStates = statesrecord_class('TTStates', ':TT', ':REFL', ':OUT')
-    state = C(TTStates, '')
+    _states_enum = Enum('TTStates', 'TT REFL OUT')
 
     @property
     def inserted(self):
-        return self.state.value in ('TT', 'REFL')
+        return self.state.position in ('TT', 'REFL')
