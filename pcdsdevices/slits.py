9a257414e5cc1eef71a0d1a108daf79bc3f80f14
Teddy Rendahl <trendahl@slac.stanford.edu>
2018-01-09 17:37:18 -0800

    ENH: Add hints to Slits object

--- pcdsdevices/slits.py
+++ pcdsdevices/slits.py
@@ -253,6 +253,12 @@ class Slits(Device):
         """
         return self.move(size, wait=False)
 
+    @property
+    def hints(self):
+        """Device hints"""
+        return {'fields': [self.xwidth.readback.name,
+                           self.ywidth.readback.name]}
+
     def open(self):
         """
         Uses the built-in `OPEN` record to move open the aperature
