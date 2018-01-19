2136980a6f2074073a943cb3f78eee09462321be
Zachary Lentz <zlentz@slac.stanford.edu>
2018-01-19 13:36:10 -0800

    ENH: Rework inout to generalize inserting/removing devices for lightpath interface

--- pcdsdevices/epics/inout.py
+++ pcdsdevices/epics/inout.py
@@ -1,28 +1,91 @@
-from ..state import StateRecordPositioner
+import math
+
+from .state import StatePositioner, StateRecordPositioner, PVStatePositioner
 
 
-class InOutPositioner(StateRecordPositioner):
+class InOutPositioner(StatePositioner):
+    """
+    Basic InOut StatePositioner. It can be inserted and removed and queried for
+    insertion and removal. It can also define transmission values for the
+    various states.
+
+    Attributes
+    ----------
+    in_states: list of strings
+        State values that should be considered 'in'.
+
+    out_states: list of strings
+        State values that should be considered 'out'.
+
+    _tranmsission: dict{str: number}
+        Mapping from each state to the transition coefficient. This should be a
+        number from 0 to 1.
+    """
     states_list = ['IN', 'OUT']
+    in_states = ['IN']
+    out_states = ['OUT']
+    _transmission = {}
+
+    def __init__(self, prefix, *, name, **kwargs):
+        super().__init__(prefix, name=name, **kwargs)
+        self._trans_enum = {}
+        self._extend_trans_enum(self.in_states, 0)
+        self._extend_trans_enum(self.out_states, 1)
 
     @property
     def inserted(self):
-        return self.position == 'IN'
+        return self._pos_in_list(self.in_states)
 
     @property
     def removed(self):
-        return self.position == 'OUT'
+        return self._pos_in_list(self.out_states)
 
-    def remove(self, wait=False, timeout=None, finished_cb=None, **kwargs):
-        return self.move('OUT', moved_cb=finished_cb, timeout=timeout,
-                         wait=wait, **kwargs)
+    def insert(self, moved_cb=None, timeout=None, wait=False):
+        """
+        Macro to move this device to the first state on the in_states list.
+        """
+        return self.move(self.in_states[0], moved_cb=moved_cb,
+                         timeout=timeout, wait=wait)
+
+    def remove(self, moved_cb=None, timeout=None, wait=False):
+        """
+        Macro to move this device to the first state on the out_states list.
+        """
+        return self.move(self.out_states[0], moved_cb=moved_cb,
+                         timeout=timeout, wait=wait)
+
+    @property
+    def transmission(self):
+        state = self.get_state(self.position)
+        return self._trans_enum.get(state, math.nan)
+
+    def _extend_trans_enum(self, state_list, default):
+        for state in state_list:
+            enumst = self.get_state(state)
+            self._trans_enum[enumst] = self._transmission.get(state, default)
+
+    def _pos_in_list(self, state_list):
+        current_state = self.get_state(self.position)
+        for state in state_list:
+            try:
+                if current_state == self.get_state(state):
+                    return True
+            except ValueError:
+                pass
+        return False
 
 
-class TTReflaser(InOutPositioner):
+class InOutRecordPositioner(StateRecordPositioner, InOutPositioner):
+    pass
+
+
+class TTReflaser(InOutRecordPositioner):
     """
     Motor stack that includes both a timetool and a reflaser.
     """
     states_list = ['TT', 'REFL', 'OUT']
+    in_states = ['TT', 'REFL']
 
-    @property
-    def inserted(self):
-        return self.state.position in ('TT', 'REFL')
+
+class InOutPVStatePositioner(PVStatePositioner, InOutPositioner):
+    pass
