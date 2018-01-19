698a76a145a66b84061799fc2c793e2f3e5e3c09
Zachary Lentz <zlentz@slac.stanford.edu>
2018-01-19 14:35:14 -0800

    TST: Normalize style and hit 100% coverage

--- tests/test_inout.py
+++ tests/test_inout.py
@@ -1,3 +1,4 @@
+import logging
 from unittest.mock import Mock
 
 from pcdsdevices.sim.pv import using_fake_epics_pv
@@ -5,44 +6,57 @@ from pcdsdevices.inout import InOutRecordPositioner
 
 from .conftest import attr_wait_true, connect_rw_pvs
 
-
-def fake_ref():
-    ref = InOutRecordPositioner("Test:Ref", name="test")
-    connect_rw_pvs(ref.state)
-    ref.state._write_pv.put('Unknown')
-    ref.wait_for_connection()
-    return ref
+logger = logging.getLogger(__name__)
 
 
-@using_fake_epics_pv
-def test_ref_states():
-    ref = fake_ref()
-    # Inserted
-    ref.state.put("IN")
-    assert ref.inserted
-    assert not ref.removed
-    # Removed
-    ref.state.put("OUT")
-    assert not ref.inserted
-    assert ref.removed
+def fake_inout():
+    inout = InOutRecordPositioner('Test:Ref', name='test')
+    connect_rw_pvs(inout.state)
+    inout.state._write_pv.put('Unknown')
+    inout.wait_for_connection()
+    return inout
 
 
 @using_fake_epics_pv
-def test_ref_motion():
-    ref = fake_ref()
-    ref.remove()
-    assert ref.position == 'OUT'
-    ref.insert()
-    assert ref.position == 'IN'
+def test_inout_states():
+    logger.debug('test_inout_states')
+    inout = fake_inout()
+    inout.state.put('IN')
+    assert inout.inserted
+    assert not inout.removed
+    inout.state.put('OUT')
+    assert not inout.inserted
+    assert inout.removed
 
 
 @using_fake_epics_pv
-def test_ref_subscriptions():
-    ref = fake_ref()
+def test_inout_trans():
+    logger.debug('test_inout_trans')
+    inout = fake_inout()
+    inout.state.put('IN')
+    assert inout.transmission == 0
+    inout.state.put('OUT')
+    assert inout.transmission == 1
+
+
+@using_fake_epics_pv
+def test_inout_motion():
+    logger.debug('test_inout_motion')
+    inout = fake_inout()
+    inout.remove()
+    assert inout.position == 'OUT'
+    inout.insert()
+    assert inout.position == 'IN'
+
+
+@using_fake_epics_pv
+def test_inout_subscriptions():
+    logger.debug('test_inout_subscriptions')
+    inout = fake_inout()
     # Subscribe a pseudo callback
     cb = Mock()
-    ref.subscribe(cb, event_type=ref.SUB_STATE, run=False)
+    inout.subscribe(cb, event_type=inout.SUB_STATE, run=False)
     # Change the target state
-    ref.state.put('OUT')
+    inout.state.put('OUT')
     attr_wait_true(cb, 'called')
     assert cb.called
