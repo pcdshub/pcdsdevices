9bf4b90028b61d4964966aa87f541e869be6cf45
Zachary Lentz <zlentz@slac.stanford.edu>
2018-01-22 11:20:08 -0800

    TST: Expand tests to good baseline, expected to fail with current code

--- tests/test_attenuator.py
+++ tests/test_attenuator.py
@@ -33,6 +33,7 @@ def test_make_feeatt():
 
 @using_fake_epics_pv
 def test_attenuator_states():
+    logger.debug('test_attenuator_states')
     att = fake_att()
     # Insert filters
     for filt in att.filters:
@@ -46,39 +47,58 @@ def test_attenuator_states():
     assert not att.inserted
 
 
+def fake_move_transition(att, status, dest, unk_trans=True):
+    """
+    Set to the PVs sort of like it would happen in the real world and check the
+    status
+    """
+    assert not status.done
+    if unk_trans:
+        for filt in att.filters:
+            filt.state._read_pv.put(filt._unknown)
+    for filt in att.filters:
+        filt.state.put(dest)
+    status_wait(status)
+    assert status.done
+    assert status.success
+
+
 @using_fake_epics_pv
 def test_attenuator_motion():
-    # TODO test status on each move
     logger.debug('test_attenuator_motion')
     att = fake_att()
     # Set up the ceil and floor
     att.trans_ceil._read_pv.put(0.8001)
     att.trans_floor._read_pv.put(0.5001)
     # Move to ceil
-    att.move(0.8)
+    status = att.move(0.8)
+    fake_move_transition(att, status, 'IN')
     assert att.setpoint.value == 0.8
     assert att.actuate_value == 2
     # Move to floor
-    att.move(0.5)
+    status = att.move(0.5)
+    fake_move_transition(att, status, 'IN')
     assert att.setpoint.value == 0.5
     assert att.actuate_value == 3
     # Call remove method
-    att.remove()
+    status = att.remove()
+    fake_move_transition(att, status, 'OUT')
     assert att.setpoint.value == 1
     # Call insert method
-    att.insert()
+    status = att.insert()
+    fake_move_transition(att, status, 'IN')
     assert att.setpoint.value == 0
+    # Call insert again, but don't set anything to unknown
+    status = att.insert()
+    fake_move_transition(att, status, 'IN', unk_trans=False)
 
 
 @using_fake_epics_pv
 def test_attenuator_subscriptions():
     logger.debug('test_attenuator_subscriptions')
     att = fake_att()
-    # Subscribe a pseudo callback
     cb = Mock()
     att.subscribe(cb, run=False)
-    # Change the target state
     att.readback._read_pv.put(0.5)
-    # Not guaranteed that callback comes in right away
     attr_wait_true(cb, 'called')
     assert cb.called
