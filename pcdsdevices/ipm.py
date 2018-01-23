92431d31b608cb34559e330406a2b992890fd866
Zachary Lentz <zlentz@slac.stanford.edu>
2018-01-23 11:52:25 -0800

    ENH: Clean up the ipm implementation slightly. It was already fine.

--- pcdsdevices/ipm.py
+++ pcdsdevices/ipm.py
@@ -1,42 +1,29 @@
-from ophyd import Component
+from ophyd import Component as Cmp
+from ophyd.signal import EpicsSignal
 
 from ..inout import InOutRecordPositioner
 
 
 class IPM(InOutRecordPositioner):
     """
-    Standard intensity position monitor motion.
+    Standard intensity position monitor motion. This is a positioner that moves
+    the target position to any of the four set positions, or out. Valid states
+    are (1, 2, 3, 4, 5) or the equivalent (T1, T2, T3, T4, OUT).
+
+    IPMs each also have a diode, which is implemented as the diode attribute of
+    this class. This can easily be controlled using ipm.diode.insert() or
+    ipm.diode.remove().
     """
+    state = Cmp(EpicsSignal, ':TARGET', write_pv=':TARGET:GO')
+    diode = Cmp(InOutRecordPositioner, ":DIODE")
+
     states_list = ['T1', 'T2', 'T3', 'T4', 'OUT']
-    diode = Component(InOutRecordPositioner, ":DIODE")
+    _states_alias = {'T1': ['T1', 'TARGET1', 't1', 'target1'],
+                     'T2': ['T2', 'TARGET2', 't2', 'target2'],
+                     'T3': ['T3', 'TARGET3', 't3', 'target3'],
+                     'T4': ['T4', 'TARGET4', 't4', 'target4']}
 
-    _states_alias = {'T1': ['T1', 'TARGET1', 't1'],
-                     'T2': ['T2', 'TARGET2', 't2'],
-                     'T3': ['T3', 'TARGET3', 't3'],
-                     'T4': ['T4', 'TARGET4', 't4']}
     in_states = ['T1', 'T2', 'T3', 'T4']
+
+    # Assume that having any target in gives transmission 0.8
     _transmission = {'T' + str(n): 0.8 for n in range(1, 5)}
-
-    def target_in(self, target, moved_cb=None, timeout=None, wait=False):
-        """
-        Move the target to one of the target positions
-
-        Parameters
-        ----------
-        target, int
-            Number of which target to move in. Must be one of 1, 2, 3, 4.
-        """
-        target = int(target)
-        self.move(target, moved_cb=moved_cb, timeout=timeout, wait=wait)
-
-    def diode_in(self, moved_cb=None, timeout=None, wait=False):
-        """
-        Move the diode to the in position.
-        """
-        self.diode.insert(moved_cb=moved_cb, timeout=timeout, wait=wait)
-
-    def diode_out(self, moved_cb=None, timeout=None, wait=False):
-        """
-        Move the diode to the out position.
-        """
-        self.diode.remove(moved_cb=moved_cb, timeout=timeout, wait=wait)
