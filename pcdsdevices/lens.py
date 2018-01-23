462d9656a951eb52cb2da474750ec2b58e77228a
Zachary Lentz <zlentz@slac.stanford.edu>
2018-01-23 13:08:27 -0800

    ENH: Clean up lens file

--- pcdsdevices/lens.py
+++ pcdsdevices/lens.py
@@ -1,97 +1,47 @@
 """
 Basic Beryllium Lens XFLS
+
+The scope of this module is picking a lens, not focusing or aligning the
+lenses. This will be expanded in the future.
 """
-from ophyd import Device, EpicsSignal, Component as C
-from ophyd.status import wait as status_wait, SubscriptionStatus
+from .inout import InOutRecordPositioner
 
 
-class XFLS(Device):
+# Most places
+class XFLS(InOutRecordPositioner):
     """
-    XFLS Device
+    XRay Focusing Lens (Be)
 
-    The XFLS device has flexibility on what the state names are ultimately
-    called, therefore this class simply interpets the base signal without
-    providing the ability to check each individual state
+    This is the simple version where the lens positions are named by number.
     """
-    state = C(EpicsSignal,'', write_pv=':GO')
-    SUB_STATE = 'sub_state_changed'
-    _default_sub = SUB_STATE
+    states_list = ['LENS1', 'LENS2', 'LENS3', 'OUT']
+    in_states = ['LENS1', 'LENS2', 'LENS3']
+    # TODO: Choose a transmission for a generic lens
 
-    def __init__(self, prefix, *, name=None, parent=None,
-                 read_attrs=None, **kwargs):
-        self._has_subscribed = False
-        if read_attrs is None:
-            read_attrs = ["state"]
 
-        super().__init__(prefix, name=name, parent=parent,
-                         read_attrs=read_attrs, **kwargs)
-    @property
-    def inserted(self):
-        """
-        Whether the lens is considered in
-        """
-        return self.state.value in list(range(1,4))
+# MFX DIA
+# CXI DG2
+class XFLS2(XFLS):
+    """
+    XRay Focusing Lens (Be)
 
-    @property
-    def removed(self):
-        """
-        Whether the lens is removed
-        """
-        return self.state.value == 4
+    These are the stacks with common names on their states and state config PVs
+    that show the focusing config
+    """
+    states_list = ['6K70', '7K50', '9K45', 'OUT']
+    in_states = ['6K70', '7K50', '9K45']
 
-    def remove(self, wait=False, timeout=None, **kwargs):
-        """
-        Remove XFLS from the beamline
 
-        Parameters
-        ----------
-        wait : bool
+# CXI DS1
+class XFLS3(XFLS):
+    """
+    XRay Focusing Lens (Be)
 
-        timeout : float, optional
-            Default timeout to wait mark the request as a failure
+    This is the stack in CXI with a screwed up IOC. The states list are
+    numbered by lens, but the config PVs are all over the place.
 
-        Returns
-        -------
-        SubscriptionStatus:
-            Future that reports the completion of the request
-        """
-        #Place command
-        self.state.put(4)
-        def cb():
-            return self.state.value == 4
-        status = SubscriptionStatus(self.state, cb, timeout=timeout)
-        #Optional wait
-        if wait:
-            status_wait(status)
-        return status
-
-    def subscribe(self, cb, event_type=None, run=True):
-        """
-        Subscribe to changes of the lenses
-
-        Parameters
-        ----------
-        cb : callable
-            Callback to be run
-
-        event_type : str, optional
-            Type of event to run callback on
-
-        run : bool, optional
-            Run the callback immediatelly
-        """
-        if not self._has_subscribed:
-            #Subscribe to state changes
-            self.state.subscribe(self._on_state_change, run=False)
-            self._has_subscribed = True
-        super().subscribe(cb, event_type=event_type, run=run)
-
-    def _on_state_change(self,  **kwargs):
-        """
-        Performed on a state change
-        """
-        #Avoid duplicate keywords
-        kwargs.pop('sub_type', None)
-        kwargs.pop('obj', None)
-        #Run subscriptions
-        self._run_subs(sub_type=self.SUB_STATE, obj=self, **kwargs)
+    This currently behaves no differently than XFLS, but when we add in the
+    state config these values will matter. I'm saving them here for later.
+    """
+    # Upper/lower case intentional
+    _config_states = ['6K70', '7k45', '9k50']
