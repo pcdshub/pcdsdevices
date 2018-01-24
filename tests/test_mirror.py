b3dad14182bc24360bf6fb03bd24726ee4fb5759
Teddy Rendahl <trendahl@slac.stanford.edu>
2018-01-24 15:23:15 -0800

    TST: Modify tests for InOutRecordPositioner structure

--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -25,11 +25,11 @@ def fake_branching_mirror():
     """
     m = PointingMirror("TST:M1H", prefix_xy="STEP:TST:M1H",
                        xgantry_prefix="GANTRY:M1H:X", name='Test Mirror',
-                       mps_prefix="MIRR:M1H:MPS", state_prefix="TST:M1H",
-                       in_lines=['MFX', 'MEC'], out_lines= ['CXI'])
-    m.state.state._read_pv.enum_strs = ['Unknown', 'IN', 'OUT']
-    connect_rw_pvs(m.state.state)
-    m.state.state._write_pv.put('Unknown')
+                       mps_prefix="MIRR:M1H:MPS", in_lines=['MFX', 'MEC'],
+                       out_lines= ['CXI'])
+    m.state._read_pv.enum_strs = ['Unknown', 'IN', 'OUT']
+    connect_rw_pvs(m.state)
+    m.state._write_pv.put('Unknown')
     m.wait_for_connection()
     return m
 
@@ -55,40 +55,46 @@ def test_mirror_init():
     assert m.ygantry.gantry_prefix == 'GANTRY:TST:M1H:Y'
 
 @using_fake_epics_pv
-def test_branching_mirror():
+def test_branching_mirror_destination():
     branching_mirror = fake_branching_mirror()
     assert branching_mirror.branches == ['MFX', 'MEC', 'CXI']
     #Unknown
-    branching_mirror.state.state._read_pv.put(0)
+    branching_mirror.state._read_pv.put(0)
+    assert branching_mirror.position == 'Unknown'
+    assert not branching_mirror.removed
+    assert not branching_mirror.inserted
     assert branching_mirror.destination == []
     #Inserted
-    branching_mirror.state.state._read_pv.put(1)
+    branching_mirror.state._read_pv.put(1)
     assert branching_mirror.inserted
+    assert not branching_mirror.removed
     assert branching_mirror.destination == ['MFX', 'MEC']
     #Removed
-    branching_mirror.state.state._read_pv.put(2)
+    branching_mirror.state._read_pv.put(2)
     assert branching_mirror.removed
+    assert not branching_mirror.inserted
     assert branching_mirror.destination == ['CXI']
 
 @using_fake_epics_pv
 def test_branching_mirror_moves():
     branching_mirror = fake_branching_mirror()
+    print(branching_mirror.read())
     #With gantry decoupled, should raise RuntimeError
-    branching_mirror.x_gantry_decoupled._read_pv.put(1)
+    branching_mirror.xgantry.decoupled._read_pv.put(1)
     with pytest.raises(RuntimeError):
         branching_mirror.remove()
     with pytest.raises(RuntimeError):
         branching_mirror.insert()
     #Recouple gantry
-    branching_mirror.x_gantry_decoupled._read_pv.put(0)
+    branching_mirror.xgantry.decoupled._read_pv.put(0)
     #Test removal
     branching_mirror.remove()
-    assert branching_mirror.state.state._write_pv.value == 'OUT'
+    assert branching_mirror.state._write_pv.value == 'OUT'
     #Finish simulated move manually
-    branching_mirror.state.state._read_pv.put('OUT')
+    branching_mirror.state._read_pv.put('OUT')
     #Insert 
     branching_mirror.insert()
-    assert branching_mirror.state.state._write_pv.value == 'IN'
+    assert branching_mirror.state._write_pv.value == 'IN'
 
 @using_fake_epics_pv
 def test_epics_mirror_subscription():
@@ -97,6 +103,6 @@ def test_epics_mirror_subscription():
     cb = Mock()
     branching_mirror.subscribe(cb, event_type=branching_mirror.SUB_STATE, run=False)
     #Change the target state
-    branching_mirror.state.state._read_pv.put('IN')
+    branching_mirror.state._read_pv.put('IN')
     attr_wait_true(cb, 'called')
     assert cb.called
