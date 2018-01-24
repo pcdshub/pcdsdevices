bbe2d04b5b45b39afcbcb2fd6bbf337346e6a69d
Zachary Lentz <zlentz@slac.stanford.edu>
2018-01-24 11:30:22 -0800

    MNT: Remove hutch-specific code

--- pcdsdevices/lens.py
+++ pcdsdevices/lens.py
@@ -7,7 +7,6 @@ lenses. This will be expanded in the future.
 from .inout import InOutRecordPositioner
 
 
-# Most places
 class XFLS(InOutRecordPositioner):
     """
     XRay Focusing Lens (Be)
@@ -17,31 +16,3 @@ class XFLS(InOutRecordPositioner):
     states_list = ['LENS1', 'LENS2', 'LENS3', 'OUT']
     in_states = ['LENS1', 'LENS2', 'LENS3']
     # TODO: Choose a transmission for a generic lens
-
-
-# MFX DIA
-# CXI DG2
-class XFLS2(XFLS):
-    """
-    XRay Focusing Lens (Be)
-
-    These are the stacks with common names on their states and state config PVs
-    that show the focusing config
-    """
-    states_list = ['6K70', '7K50', '9K45', 'OUT']
-    in_states = ['6K70', '7K50', '9K45']
-
-
-# CXI DS1
-class XFLS3(XFLS):
-    """
-    XRay Focusing Lens (Be)
-
-    This is the stack in CXI with a screwed up IOC. The states list are
-    numbered by lens, but the config PVs are all over the place.
-
-    This currently behaves no differently than XFLS, but when we add in the
-    state config these values will matter. I'm saving them here for later.
-    """
-    # Upper/lower case intentional
-    _config_states = ['6K70', '7k45', '9k50']
