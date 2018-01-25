16bcd123d805c661fa7638b5a2bc4da607257897
Teddy Rendahl <trendahl@slac.stanford.edu>
2018-01-25 10:39:15 -0800

    STY: PEP8 for mirror tests

--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -1,19 +1,10 @@
-############
-# Standard #
-############
 from unittest.mock import Mock
 
-###############
-# Third Party #
-###############
 import math
 import pytest
 
-##########
-# Module #
-##########
 from pcdsdevices.mirror import OffsetMirror, PointingMirror
-from pcdsdevices.sim.pv       import using_fake_epics_pv
+from pcdsdevices.sim.pv import using_fake_epics_pv
 
 from .conftest import attr_wait_true, connect_rw_pvs
 
@@ -36,16 +27,19 @@ def fake_branching_mirror():
     m.pitch.motor_egu._read_pv.put('urad')
     return m
 
+
 @using_fake_epics_pv
 def test_nan_protection():
     branching_mirror = fake_branching_mirror()
     with pytest.raises(ValueError):
         branching_mirror.pitch.check_value(math.nan)
 
+
 @using_fake_epics_pv
 def test_ommotor_positioner_egu():
     branching_mirror = fake_branching_mirror()
     assert branching_mirror.pitch.egu == 'urad'
+
 
 @using_fake_epics_pv
 def test_mirror_init():
@@ -55,12 +49,13 @@ def test_mirror_init():
     assert bm.xgantry.gantry_prefix == 'GANTRY:M1H:X'
     assert bm.ygantry.prefix == 'STEP:TST:M1H:Y:P'
     assert bm.ygantry.gantry_prefix == 'GANTRY:STEP:TST:M1H:Y'
-    m = OffsetMirror('TST:M1H', name= "Test Mirror")
+    m = OffsetMirror('TST:M1H', name="Test Mirror")
     assert m.pitch.prefix == 'MIRR:TST:M1H'
     assert m.xgantry.prefix == 'TST:M1H:X:P'
     assert m.xgantry.gantry_prefix == 'GANTRY:TST:M1H:X'
     assert m.ygantry.prefix == 'TST:M1H:Y:P'
     assert m.ygantry.gantry_prefix == 'GANTRY:TST:M1H:Y'
+
 
 @using_fake_epics_pv
 def test_offsetmirror_lighpath():
@@ -68,31 +63,33 @@ def test_offsetmirror_lighpath():
     assert m.inserted
     assert not m.removed
 
+
 @using_fake_epics_pv
 def test_branching_mirror_destination():
     branching_mirror = fake_branching_mirror()
     assert branching_mirror.branches == ['MFX', 'MEC', 'CXI']
-    #Unknown
+    # Unknown
     branching_mirror.state._read_pv.put(0)
     assert branching_mirror.position == 'Unknown'
     assert not branching_mirror.removed
     assert not branching_mirror.inserted
     assert branching_mirror.destination == []
-    #Inserted
+    # Inserted
     branching_mirror.state._read_pv.put(1)
     assert branching_mirror.inserted
     assert not branching_mirror.removed
     assert branching_mirror.destination == ['MFX', 'MEC']
-    #Removed
+    # Removed
     branching_mirror.state._read_pv.put(2)
     assert branching_mirror.removed
     assert not branching_mirror.inserted
     assert branching_mirror.destination == ['CXI']
 
+
 @using_fake_epics_pv
 def test_branching_mirror_moves():
     branching_mirror = fake_branching_mirror()
-    #With gantry decoupled, should raise PermissionError
+    # With gantry decoupled, should raise PermissionError
     branching_mirror.xgantry.decoupled._read_pv.put(1)
     with pytest.raises(PermissionError):
         branching_mirror.xgantry.move(0.1, wait=False)
@@ -100,27 +97,29 @@ def test_branching_mirror_moves():
         branching_mirror.remove()
     with pytest.raises(PermissionError):
         branching_mirror.insert()
-    #Recouple gantry
+    # Recouple gantry
     branching_mirror.xgantry.decoupled._read_pv.put(0)
     # Test small move
     branching_mirror.xgantry.move(0.2, wait=False)
     assert branching_mirror.xgantry.setpoint._write_pv.get() == 0.2
-    #Test removal
+    # Test removal
     branching_mirror.remove()
     assert branching_mirror.state._write_pv.value == 'OUT'
-    #Finish simulated move manually
+    # Finish simulated move manually
     branching_mirror.state._read_pv.put('OUT')
-    #Insert 
+    # Insert
     branching_mirror.insert()
     assert branching_mirror.state._write_pv.value == 'IN'
+
 
 @using_fake_epics_pv
 def test_epics_mirror_subscription():
     branching_mirror = fake_branching_mirror()
-    #Subscribe a pseudo callback
+    # Subscribe a pseudo callback
     cb = Mock()
-    branching_mirror.subscribe(cb, event_type=branching_mirror.SUB_STATE, run=False)
-    #Change the target state
+    branching_mirror.subscribe(cb, event_type=branching_mirror.SUB_STATE,
+                               run=False)
+    # Change the target state
     branching_mirror.state._read_pv.put('IN')
     attr_wait_true(cb, 'called')
     assert cb.called
