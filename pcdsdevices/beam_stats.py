4d0db091fbc457cc6640f4a8f3ee8b383058b5f0
Zachary Lentz <zlentz@slac.stanford.edu>
2018-04-11 12:00:55 -0700

    ENH: Add read_attrs, hints, test

--- pcdsdevices/beam_stats.py
+++ pcdsdevices/beam_stats.py
@@ -3,9 +3,16 @@ from ophyd.signal import EpicsSignalRO
 
 
 class BeamStats(Device):
-    ev = Cpt(EpicsSignalRO, 'SIOC:SYS0:ML00:AO541')
     mj = Cpt(EpicsSignalRO, 'GDET:FEE1:241:ENRC')
+    ev = Cpt(EpicsSignalRO, 'SIOC:SYS0:ML00:AO541')
     rate = Cpt(EpicsSignalRO, 'EVNT:SYS0:1:LCLSBEAMRATE')
     owner = Cpt(EpicsSignalRO, 'ECS:SYS0:0:BEAM_OWNER_ID')
 
-    _default_read_attrs = ['ev', 'mj', 'rate']
+    _default_read_attrs = ['mj', 'ev', 'rate']
+
+    def __init__(self, prefix='', name='beam_stats', **kwargs):
+        super.__init__(prefix=prefix, name=name, **kwargs)
+
+    @property
+    def hints(self):
+        return {'fields': [self.mj.name]}
--- tests/test_beam_stats.py
+++ tests/test_beam_stats.py
@@ -1,1 +1,14 @@
-from pcdsdevices import beam_stats
+import logging
+
+from pcdsdevices.beam_stats import BeamStats
+from pcdsdevices.sim import using_fake_epics_pv
+
+logger = logging.getLogger(__name__)
+
+
+@using_fake_epics_pv
+def test_beam_stats():
+    logger.debug('test_beam_stats')
+    stats = BeamStats()
+    stats.read()
+    stats.hints
