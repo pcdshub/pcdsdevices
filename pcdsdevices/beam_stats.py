3e3448d61f2de5875180afd295b467437896a1c0
Zachary Lentz <zlentz@slac.stanford.edu>
2018-04-11 11:17:52 -0700

    ENH: Add basic BeamStats for discussion

---
+++ pcdsdevices/beam_stats.py
@@ -0,0 +1,9 @@
+from ophyd.device import Device, Component as Cpt
+from ophyd.signal import EpicsSignalRO
+
+
+class BeamStats(Device):
+    ev = Cpt(EpicsSignalRO, 'SIOC:SYS0:ML00:AO541')
+    mj = Cpt(EpicsSignalRO, 'GDET:FEE1:241:ENRC')
+    rate = Cpt(EpicsSignalRO, 'EVNT:SYS0:1:LCLSBEAMRATE')
+    owner = Cpt(EpicsSignalRO, 'ECS:SYS0:0:BEAM_OWNER_ID')
