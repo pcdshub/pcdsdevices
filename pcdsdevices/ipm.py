5c569fca3345c6f7f320b91a06060bf26d9a2f8d
Zachary Lentz <zlentz@slac.stanford.edu>
2018-04-16 14:26:15 -0700

    MNT: Adjust ipm for changes to parent classes. Clear out gratuitious aliases. Fix readback. Misc cleanup.

--- pcdsdevices/ipm.py
+++ pcdsdevices/ipm.py
@@ -1,8 +1,8 @@
 """
 Module for the `IPM` intensity position monitor class
 """
-from ophyd import Component as Cmp
-from ophyd.signal import EpicsSignal
+from ophyd import Component as Cpt, FormattedComponent as FCpt
+from ophyd.signal import EpicsSignal, EpicsSignalRO
 
 from .doc_stubs import basic_positioner_init
 from .inout import InOutRecordPositioner
@@ -14,7 +14,8 @@ class IPM(InOutRecordPositioner):
 
     This is an `InOutRecordPositioner` that moves
     the target position to any of the four set positions, or out. Valid states
-    are (1, 2, 3, 4, 5) or the equivalent (T1, T2, T3, T4, OUT).
+    are (1, 2, 3, 4, 5) or the equivalent
+    (TARGET1, TARGET2, TARGET3, TARGET4, OUT).
 
     IPMs each also have a diode, which is implemented as the diode attribute of
     this class. This can easily be controlled using ``ipm.diode.insert()`` or
@@ -22,16 +23,12 @@ class IPM(InOutRecordPositioner):
     """
     __doc__ += basic_positioner_init
 
-    state = Cmp(EpicsSignal, ':TARGET', write_pv=':TARGET:GO')
-    diode = Cmp(InOutRecordPositioner, ":DIODE")
+    state = Cpt(EpicsSignal, ':TARGET', write_pv=':TARGET:GO')
+    diode = Cpt(InOutRecordPositioner, ':DIODE')
+    readback = FCpt(EpicsSignalRO, '{self.prefix}:TARGET:{self._readback}')
 
-    states_list = ['T1', 'T2', 'T3', 'T4', 'OUT']
-    _states_alias = {'T1': ['T1', 'TARGET1', 't1', 'target1'],
-                     'T2': ['T2', 'TARGET2', 't2', 'target2'],
-                     'T3': ['T3', 'TARGET3', 't3', 'target3'],
-                     'T4': ['T4', 'TARGET4', 't4', 'target4']}
-
-    in_states = ['T1', 'T2', 'T3', 'T4']
+    in_states = ['TARGET1', 'TARGET2', 'TARGET3', 'TARGET4']
+    states_list = in_states + ['OUT']
 
     # Assume that having any target in gives transmission 0.8
-    _transmission = {'T' + str(n): 0.8 for n in range(1, 5)}
+    _transmission = {st: 0.8 for st in in_states}
