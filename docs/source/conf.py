8a57d7b1c9a3983db472de0eb53aca9ad680cd2a
Zachary Lentz <zlentz@slac.stanford.edu>
2018-04-20 14:54:40 -0700

    DOC: Use m2r from pip to include README.md in sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'IPython.sphinxext.ipython_directive',
               'IPython.sphinxext.ipython_console_highlighting',
-              'sphinx.ext.autosectionlabel'
+              'sphinx.ext.autosectionlabel',
+              'm2r'
              ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,8 +53,8 @@ autosummary_generate = True
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+# source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
