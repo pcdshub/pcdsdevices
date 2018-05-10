40ece241521c855d9954f37b41a640d3edf8e466
Zachary Lentz <zlentz@slac.stanford.edu>
2018-05-09 17:51:33 -0700

    ENH: Attach averaged energy to BeamStats

--- pcdsdevices/beam_stats.py
+++ pcdsdevices/beam_stats.py
@@ -1,5 +1,7 @@
 from ophyd.device import Device, Component as Cpt
-from ophyd.signal import EpicsSignalRO
+from ophyd.signal import EpicsSignalRO, AttributeSignal
+
+from .signal import AvgSignal
 
 
 class BeamStats(Device):
@@ -8,7 +10,11 @@ class BeamStats(Device):
     rate = Cpt(EpicsSignalRO, 'EVNT:SYS0:1:LCLSBEAMRATE')
     owner = Cpt(EpicsSignalRO, 'ECS:SYS0:0:BEAM_OWNER_ID')
 
-    _default_read_attrs = ['mj', 'ev', 'rate']
+    mj_avg = Cpt(AvgSignal, 'mj', averages=120)
+    mj_buffersize = Cpt(AttributeSignal, 'mj_avg.averages')
+
+    _default_read_attrs = ['mj', 'mj_avg', 'ev', 'rate']
+    _default_config_attrs = ['mj_buffersize']
 
     def __init__(self, prefix='', name='beam_stats', **kwargs):
         super().__init__(prefix=prefix, name=name, **kwargs)
