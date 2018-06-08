855b1a56b0b2753e71641cac16751a41a0422ac7
Zachary Lentz <zlentz@slac.stanford.edu>
2018-06-08 16:21:16 -0700

    TST: switch beam_stats tests to use make_fake_device

--- tests/test_beam_stats.py
+++ tests/test_beam_stats.py
@@ -1,37 +1,32 @@
 import logging
 
+from ophyd.sim import make_fake_device
+
 from pcdsdevices.beam_stats import BeamStats
-from pcdsdevices.sim.pv import using_fake_epics_pv
 
 logger = logging.getLogger(__name__)
 
 
-@using_fake_epics_pv
 def test_beam_stats():
     logger.debug('test_beam_stats')
-    stats = BeamStats()
-    stats.wait_for_connection()
+    stats = make_fake_device(BeamStats)()
     stats.read()
     stats.hints
 
 
-@using_fake_epics_pv
 def test_beam_stats_avg():
     logger.debug('test_beam_stats_avg')
-    stats = BeamStats()
-    stats.mj._read_pv.put(-1)
-    stats.wait_for_connection()
+    stats = make_fake_device(BeamStats)()
+    stats.mj.sim_put(-1)
 
     assert stats.mj_buffersize.value == 120
 
     stats.mj_buffersize.put(10)
 
-    with stats.mj._read_pv._lock:
-        for i in range(10):
-            stats.mj._read_pv._value = i
-            stats.mj._read_pv.run_callbacks()
+    for i in range(10):
+        stats.mj.sim_put(i)
 
-        assert stats.mj_avg.value == sum(range(10))/10
+    assert stats.mj_avg.value == sum(range(10))/10
 
     stats.configure(dict(mj_buffersize=20))
     cfg = stats.read_configuration()
