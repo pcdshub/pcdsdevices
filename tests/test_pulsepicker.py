56dbe73e7a3b7319cb623615e18d56665d7e5faf
Zachary Lentz <zlentz@slac.stanford.edu>
2018-06-10 12:17:21 -0700

    TST: update test_pulsepicker to use make_fake_device

--- tests/test_pulsepicker.py
+++ tests/test_pulsepicker.py
@@ -5,44 +5,44 @@ import logging
 import pytest
 from unittest.mock import Mock
 from ophyd.status import wait as status_wait
+from ophyd.sim import make_fake_device
 
 from pcdsdevices.inout import InOutRecordPositioner
 from pcdsdevices.pulsepicker import PulsePickerInOut
-from pcdsdevices.sim.pv import using_fake_epics_pv
 
-from .conftest import attr_wait_true, connect_rw_pvs
+from .conftest import HotfixFakeEpicsSignal
 
 logger = logging.getLogger(__name__)
 
 
+@pytest.fixture(scope='function')
 def fake_picker():
     """
     Picker starts IN and OPEN
     """
-    picker = PulsePickerInOut('TST:SB1:MMS:35', name='picker')
-    connect_rw_pvs(picker.inout.state)
-    picker.inout.state.put(0)
-    picker.inout.state._read_pv.enum_strs = (['Unknown'] +
-                                             InOutRecordPositioner.states_list)
-    picker.blade._read_pv.put(0)
-    picker.mode._read_pv.put(0)
-    picker.wait_for_connection()
+    FakePicker = make_fake_device(PulsePickerInOut)
+    FakePicker.inout.cls.state.cls = HotfixFakeEpicsSignal
+    picker = FakePicker('TST:SB1:MMS:35', name='picker')
+    picker.inout.state.sim_put(0)
+    picker.inout.state.sim_enum_strs(['Unknown'] +
+                                     InOutRecordPositioner.states_list)
+    picker.blade.sim_put(0)
+    picker.mode.sim_put(0)
     return picker
 
 
 @pytest.mark.timeout(5)
-@using_fake_epics_pv
-def test_picker_states():
+def test_picker_states(fake_picker):
     logger.debug('test_picker_states')
-    picker = fake_picker()
+    picker = fake_picker
     # Insert and OPEN
     picker.inout.state.put('IN')
-    picker.blade._read_pv.put(0)
+    picker.blade.sim_put(0)
     assert not picker.inserted
     assert picker.removed
     assert picker.position == 'OPEN'
     # CLOSE it
-    picker.blade._read_pv.put(1)
+    picker.blade.sim_put(1)
     assert picker.inserted
     assert not picker.removed
     assert picker.position == 'CLOSED'
@@ -54,13 +54,12 @@ def test_picker_states():
 
 
 @pytest.mark.timeout(5)
-@using_fake_epics_pv
-def test_picker_motion():
+def test_picker_motion(fake_picker):
     logger.debug('test_picker_motion')
-    picker = fake_picker()
+    picker = fake_picker
     # Light interface
     status = picker.insert(wait=False)
-    picker.blade._read_pv.put(1)
+    picker.blade.sim_put(1)
     status_wait(status, timeout=1)
     assert status.done
     assert status.success
@@ -81,7 +80,7 @@ def test_picker_motion():
     assert not picker.removed
     assert picker.position == 'CLOSED'
     status = picker.move('OPEN', wait=False)
-    picker.blade._read_pv.put(0)
+    picker.blade.sim_put(0)
     status_wait(status, timeout=1)
     assert status.done
     assert status.success
@@ -100,17 +99,16 @@ def test_picker_motion():
 def put_soon(sig, val):
     def inner():
         time.sleep(0.2)
-        sig._read_pv.put(val)
+        sig.sim_put(val)
     t = threading.Thread(target=inner, args=())
     t.start()
 
 
 @pytest.mark.timeout(5)
-@using_fake_epics_pv
-def test_picker_mode():
+def test_picker_mode(fake_picker):
     logger.debug('test_picker_mode')
-    picker = fake_picker()
-    picker.mode._read_pv.put(1)
+    picker = fake_picker
+    picker.mode.sim_put(1)
     put_soon(picker.mode, 0)
     picker.reset(wait=True)
     assert picker.cmd_reset.get() == 1
@@ -127,10 +125,9 @@ def test_picker_mode():
 
 
 @pytest.mark.timeout(5)
-@using_fake_epics_pv
-def test_picker_mode_wait():
+def test_picker_mode_wait(fake_picker):
     logger.debug('test_picker_mode_waits')
-    picker = fake_picker()
+    picker = fake_picker
 
     put_soon(picker.blade, 0)
     picker.open(wait=True)
@@ -139,24 +136,22 @@ def test_picker_mode_wait():
 
     put_soon(picker.mode, 2)
     picker.flipflop(wait=True)
-    picker.mode._read_pv.put(0)
+    picker.mode.sim_put(0)
     put_soon(picker.mode, 3)
     picker.burst(wait=True)
-    picker.mode._read_pv.put(0)
+    picker.mode.sim_put(0)
     put_soon(picker.mode, 6)
     picker.follower(wait=True)
-    picker.mode._read_pv.put(0)
+    picker.mode.sim_put(0)
 
 
 @pytest.mark.timeout(5)
-@using_fake_epics_pv
-def test_picker_subs():
+def test_picker_subs(fake_picker):
     logger.debug('test_picker_subs')
-    picker = fake_picker()
+    picker = fake_picker
     # Subscribe a pseudo callback
     cb = Mock()
     picker.subscribe(cb, event_type=picker.SUB_STATE, run=False)
     # Change the target state
     picker.insert()
-    attr_wait_true(cb, 'called')
     assert cb.called
