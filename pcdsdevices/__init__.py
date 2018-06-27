62b67328102b5e9cbd1b0e60424c82232baecd8d
Zachary Lentz <zlentz@slac.stanford.edu>
2018-06-27 10:13:58 -0700

    MNT: move hacky hotfix to pcdsdevices from hxrsnd

--- pcdsdevices/__init__.py
+++ pcdsdevices/__init__.py
@@ -1,4 +1,12 @@
 from ._version import get_versions
 
+# Hacky ophyd hotfix
+from ophyd.device import Device
+def __contains__(self, value):
+    return value in self._OphydAttrList__internal_list()
+Device.OphydAttrList.__contains__ = __contains__
+del Device
+del __contains__
+
 __version__ = get_versions()['version']
 del get_versions
