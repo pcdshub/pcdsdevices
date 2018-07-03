6f53137b0585ba934e0cd58d959a7d80c8a6f242
Yesenia Garnica <garnica@slac.stanford.edu>
2018-07-03 08:42:08 -0700

    format edited again

--- pcdsdevices/epics_motor.py
+++ pcdsdevices/epics_motor.py
@@ -2,7 +2,6 @@
 Module for LCLS's special motor records.
 """
 import logging
-import time
 from ophyd.device import Component as Cpt
 from ophyd.epics_motor import EpicsMotor
 from ophyd.signal import Signal, EpicsSignal, EpicsSignalRO
@@ -244,6 +243,7 @@ class PCDSMotorBase(FltMvInterface, EpicsMotor):
         # Pass information to PositionerBase
         super()._pos_changed(timestamp=timestamp, old_value=old_value,
                              value=value, **kwargs)
+
 
 class IMS(PCDSMotorBase):
     """
