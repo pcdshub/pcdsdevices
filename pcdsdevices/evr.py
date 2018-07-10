9afe2d6adee507268de37b457fa6de890553eb3d
teddyrendahl <trendahl@slac.stanford.edu>
2018-07-09 19:32:56 -0700

    STY: Use string specification for Kind

--- pcdsdevices/evr.py
+++ pcdsdevices/evr.py
@@ -1,18 +1,17 @@
-from ophyd import Device, Component as Cpt, EpicsSignal, EpicsSignalRO, Kind
+from ophyd import Device, Component as Cpt, EpicsSignal, EpicsSignalRO
 
 
 class Trigger(Device):
     """
     Class for an individual Trigger
     """
-    eventcode = Cpt(EpicsSignal, ':EC_RBV', write_pv=':TEC', kind=Kind.config)
-    eventrate = Cpt(EpicsSignalRO, ':RATE', kind=Kind.normal)
-    label = Cpt(EpicsSignal, ':TCTL.DESC', kind=Kind.omitted)
-    ns_delay = Cpt(EpicsSignal, ':BW_TDES', write_pv=':TDES', kind=Kind.hinted)
-    polarity = Cpt(EpicsSignal, ':TPOL', kind=Kind.config)
-    width = Cpt(EpicsSignal, ':BW_TWIDCALC', write_pv=':TWID',
-                kind=Kind.normal)
-    enable_cmd = Cpt(EpicsSignal, ':TCTL', kind=Kind.omitted)
+    eventcode = Cpt(EpicsSignal, ':EC_RBV', write_pv=':TEC', kind="config")
+    eventrate = Cpt(EpicsSignalRO, ':RATE', kind="normal")
+    label = Cpt(EpicsSignal, ':TCTL.DESC', kind="omitted")
+    ns_delay = Cpt(EpicsSignal, ':BW_TDES', write_pv=':TDES', kind="hinted")
+    polarity = Cpt(EpicsSignal, ':TPOL', kind="config")
+    width = Cpt(EpicsSignal, ':BW_TWIDCALC', write_pv=':TWID', kind="normal")
+    enable_cmd = Cpt(EpicsSignal, ':TCTL', kind="omitted")
 
     def enable(self):
         """Enable the trigger"""
