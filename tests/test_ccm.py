a654eb7cffd23bd48399ebed77366297d7ea0d31
Zachary Lentz <zlentz@slac.stanford.edu>
2018-07-10 18:39:09 -0700

    TST: rewrite ccm tests

--- tests/test_ccm.py
+++ tests/test_ccm.py
@@ -1,9 +1,10 @@
 import logging
 
 import numpy as np
+from ophyd.sim import make_fake_device
+import pytest
 
 import pcdsdevices.ccm as ccm
-from pcdsdevices.sim.pv import using_fake_epics_pv
 
 logger = logging.getLogger(__name__)
 
@@ -16,16 +17,16 @@ SAMPLE_WAVELENGTH = 10  # xray
 # Make sure the calcs are properly inverted
 def test_theta_alio_inversion():
     logger.debug('test_theta_alio_inversion')
-    theta = ccm.alio_to_theta(SAMPLE_ALIO, ccm.gTheta0, ccm.gR, ccm.gD)
-    alio_calc = ccm.theta_to_alio(theta, ccm.gTheta0, ccm.gR, ccm.gD)
+    theta = ccm.alio_to_theta(SAMPLE_ALIO, ccm.theta0, ccm.gr, ccm.gd)
+    alio_calc = ccm.theta_to_alio(theta, ccm.theta0, ccm.gr, ccm.gd)
     # Unlike the other inversions, this is just an approximation
     assert np.isclose(alio_calc, SAMPLE_ALIO)
 
 
 def test_wavelength_theta_inversion():
     logger.debug('test_wavelength_theta_inversion')
-    wavelength = ccm.wavelength_to_theta(SAMPLE_THETA, ccm.gdspacing)
-    theta_calc = ccm.theta_to_wavelength(wavelength, ccm.gdspacing)
+    wavelength = ccm.wavelength_to_theta(SAMPLE_THETA, ccm.dspacing)
+    theta_calc = ccm.theta_to_wavelength(wavelength, ccm.dspacing)
     assert theta_calc == SAMPLE_THETA
 
 
@@ -36,74 +37,75 @@ def test_energy_wavelength_inversion():
     assert wavelength_calc == SAMPLE_WAVELENGTH
 
 
+@pytest.fixture(scope='function')
+def fake_ccm():
+    FakeCCM = make_fake_device(ccm.CCM)
+    fake_ccm = FakeCCM(x_down='X:DOWN', x_up='X:UP', y_down='Y:DOWN',
+                       y_up_north='Y:UP:NORTH', y_up_south='Y:UP:SOUTH',
+                       alio='ALIO', theta2fine='THETA', inpos=8, outpos=0,
+                       name='fake_ccm')
+    fake_ccm.calc.alio.readback.sim_put(SAMPLE_ALIO)
+    fake_ccm.calc.alio.setpoint.sim_put(SAMPLE_ALIO)
+    fake_ccm.x.down.user_readback.sim_put(0)
+    fake_ccm.x.up.user_readback.sim_put(0)
+    fake_ccm.x.down.user_setpoint.sim_put(0)
+    fake_ccm.x.up.user_setpoint.sim_put(0)
+    fake_ccm.y.down.user_setpoint.sim_put(0)
+    fake_ccm.y.up_north.user_setpoint.sim_put(0)
+    fake_ccm.y.up_south.user_setpoint.sim_put(0)
+
+
 # Make sure we set up the forward/inverse to use the right methods
-@using_fake_epics_pv
-def test_ccm_calc():
+def test_ccm_calc(fake_ccm):
     logger.debug('test_ccm_calc')
-    calc = ccm.CCMCalc('TEST', name='calc')
-    calc.alio.setpoint._read_pv.put(SAMPLE_ALIO)
-    calc.alio.readback._read_pv.put(SAMPLE_ALIO)
-    calc.wait_for_connection()
+    calc = fake_ccm.calc
 
     theta = calc.theta.position
-    theta_func = ccm.alio_to_theta(SAMPLE_ALIO, calc.gTheta0, calc.gR, calc.gD)
+    theta_func = ccm.alio_to_theta(SAMPLE_ALIO, calc.theta0, calc.gr, calc.gd)
     assert theta == theta_func
 
     wavelength = calc.wavelength.position
-    wavelength_func = ccm.theta_to_wavelength(theta, calc.gdspacing)
+    wavelength_func = ccm.theta_to_wavelength(theta, calc.dspacing)
     assert wavelength == wavelength_func
 
     energy = calc.energy.position
     energy_func = ccm.wavelength_to_energy(energy)
     assert energy == energy_func
 
-    calc.alio.setpoint._read_pv.put(0)
-    calc.alio.readback._read_pv.put(0)
-
+    calc.alio.readback.sim_put(0)
+    calc.alio.setpoint.sim_put(0)
     calc.move(energy, wait=True)
-    assert calc.alio.setpoint._read_pv.get() == SAMPLE_ALIO
+
+    assert calc.alio.setpoint.get() == SAMPLE_ALIO
 
 
 # Make sure sync'd axes work and that unk/in/out states work
-@using_fake_epics_pv
-def test_ccm_main():
-    tst_ccm = ccm.CCM(x_down='X:DOWN', x_up='X:UP', y_down='Y:DOWN',
-                      y_up_north='Y:UPN', y_up_south='Y:UPS', alio='CC:ALIO',
-                      theta2fine='CC:TH', inpos=8, outpos=0, name='tst_ccm')
-    tst_ccm.x.down.user_readback._read_pv.put(0)
-    tst_ccm.x.up.user_readback._read_pv.put(0)
-    tst_ccm.x.down.user_setpoint._write_pv.put(0)
-    tst_ccm.x.up.user_setpoint._write_pv.put(0)
-    tst_ccm.y.down.user_setpoint._write_pv.put(0)
-    tst_ccm.y.up_north.user_setpoint._write_pv.put(0)
-    tst_ccm.y.up_south.user_setpoint._write_pv.put(0)
-    tst_ccm.wait_for_connection()
+def test_ccm_main(fake_ccm):
+    fake_ccm.y.move(5)
+    assert fake_ccm.y.down.user_setpoint.get() == 5
+    assert fake_ccm.y.up_north.user_setpoint.get() == 5
+    assert fake_ccm.y.up_south.user_setpoint.get() == 5
 
-    tst_ccm.y.move(5)
-    assert tst_ccm.y.down.user_setpoint._write_pv.get() == 5
-    assert tst_ccm.y.up_north.user_setpoint._write_pv.get() == 5
-    assert tst_ccm.y.up_south.user_setpoint._write_pv.get() == 5
+    assert fake_ccm.position == 'OUT'
+    assert fake_ccm.removed
+    assert not fake_ccm.inserted
 
-    assert tst_ccm.position == 'OUT'
-    assert tst_ccm.removed
-    assert not tst_ccm.inserted
+    fake_ccm.x.down.user_readback.sim_put(8)
+    fake_ccm.x.up.user_readback.sim_put(8)
+    assert fake_ccm.position == 'IN'
+    assert not fake_ccm.removed
+    assert fake_ccm.inserted
 
-    tst_ccm.x.down.user_readback._read_pv.put(8)
-    tst_ccm.x.up.user_readback._read_pv.put(8)
-    assert tst_ccm.position == 'IN'
-    assert not tst_ccm.removed
-    assert tst_ccm.inserted
+    fake_ccm.x.down.user_readback.sim_put(4)
+    fake_ccm.x.up.user_readback.sim_put(4)
+    assert fake_ccm.position == 'Unknown'
+    assert not fake_ccm.removed
+    assert not fake_ccm.inserted
 
-    tst_ccm.x.down.user_readback._read_pv.put(4)
-    tst_ccm.x.up.user_readback._read_pv.put(4)
-    assert tst_ccm.position == 'Unknown'
-    assert not tst_ccm.removed
-    assert not tst_ccm.inserted
+    fake_ccm.insert()
+    assert fake_ccm.x.down.user_setpoint.get() == 8
+    assert fake_ccm.x.up.user_setpoint.get() == 8
 
-    tst_ccm.insert()
-    assert tst_ccm.x.down.user_setpoint._write_pv.get() == 8
-    assert tst_ccm.x.up.user_setpoint._write_pv.get() == 8
-
-    tst_ccm.remove()
-    assert tst_ccm.x.down.user_setpoint._write_pv.get() == 0
-    assert tst_ccm.x.up.user_setpoint._write_pv.get() == 0
+    fake_ccm.remove()
+    assert fake_ccm.x.down.user_setpoint.get() == 0
+    assert fake_ccm.x.up.user_setpoint.get() == 0
