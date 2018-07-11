437bceeaf4c71c30673fbce9e891dd4f1588ae39
Zachary Lentz <zlentz@slac.stanford.edu>
2018-07-10 17:46:57 -0700

    ENH: Add offsets into the sync move, remove the factory function, adjust tests

--- pcdsdevices/pseudopos.py
+++ pcdsdevices/pseudopos.py
@@ -1,4 +1,3 @@
-import numpy as np
 from ophyd.device import Component as Cpt
 from ophyd.pseudopos import PseudoPositioner, PseudoSingle
 
@@ -7,8 +6,20 @@ class SyncAxesBase(PseudoPositioner):
     """
     Synchronized Axes.
 
-    This will move all axes to the same position at the same time. And report
-    its position as the average value of all axes.
+    This will move all axes in a coordinated way, retaining offsets.
+
+    This can be configured to report its position as the min, max, mean, or any
+    custom function acting on a list of positions. Min is the default.
+
+    You should subclass this by adding real motors as components. The class
+    will pick them up and include them correctly into the coordinated move.
+
+    Parameters
+    ----------
+    position_mode: ``func``, optional
+        The function to apply to the list of current positions to determine
+        the combined position. By default, this is the minimum of all axis
+        positions.
 
     Attributes
     ----------
@@ -17,43 +28,65 @@ class SyncAxesBase(PseudoPositioner):
     """
     pseudo = Cpt(PseudoSingle)
 
+    _default_mode = min
+
+    def __init__(self, *args, position_mode=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._mode = position_mode or self._default_mode
+        self._offsets = {}
+
+    def save_offsets(self, **offsets):
+        """
+        Save the current offsets for the synchronized assembly.
+
+        If not done earlier, this will be automatically run before it is first
+        needed (generally, right before the first move).
+
+        Note that this method does not protect you from getting your offsets
+        into an inconsistent state! Pass arguments into the `offsets` param at
+        your own risk.
+
+        Parameters
+        ----------
+        **offsets: ``int`` or ``float``, optional
+            Mapping of axis name to offset. If omitted, we'll use the current
+            positions. If a partial dictionary is provided, the omitted values
+            are assumed to be either 0 (if no value is available) or unchanged
+            (if previous saved values are available).
+        """
+        if not offsets:
+            pos = self.real_position
+            combo = self._mode(pos)
+            offsets = {fld: getattr(pos, fld) - combo for fld in pos._fields}
+        for fld in self.RealPosition._fields:
+            if fld not in offsets:
+                offsets[fld] = self._offsets.get(fld, 0)
+        self._offsets = offsets
+
     def forward(self, pseudo_pos):
         """
-        Composite axes all move to the combined axis position
+        Composite axes move to the combined axis position plus an offset
         """
+        if not self._offsets:
+            self.save_offsets()
         pseudo_pos = self.PseudoPosition(*pseudo_pos)
-        return self.RealPosition(**{axis: pseudo_pos.pseudo
-                                    for axis in self.axes})
+        real_pos = {}
+        for axis, offset in self._offsets.items():
+            real_pos[axis] = pseudo_pos.pseudo + offset
+        return self.RealPosition(**real_pos)
 
     def inverse(self, real_pos):
         """
         Combined axis readback is the mean of the composite axes
         """
         real_pos = self.RealPosition(*real_pos)
-        return self.PseudoPosition(pseudo=np.mean(real_pos))
+        return self.PseudoPosition(pseudo=self._mode(real_pos))
 
     @property
     def position(self):
+        """
+        Override the position to just show the number.
+
+        This would otherwise be a named tuple with one element, pseudo.
+        """
         return super().position.pseudo
-
-
-def SyncAxes(*args, **kwargs):
-    """
-    Create and instantiate a `SyncAxesBase` subclass with the desired
-    components.
-    """
-    if 'axes' in kwargs:
-        raise ValueError(('axes is a reserved name for SyncAxes and cannot be '
-                          'a component name or a kwarg'))
-    cpt = {}
-    kwd = {}
-
-    for key, value in kwargs.items():
-        if isinstance(value, Cpt):
-            cpt[key] = value
-        else:
-            kwd[key] = value
-    cpt['axes'] = list(cpt.keys())
-
-    cls = type('SyncAxes', (SyncAxesBase,), cpt)
-    return cls(*args, **kwd)
--- tests/test_pseudopos.py
+++ tests/test_pseudopos.py
@@ -1,24 +1,25 @@
 import logging
 import pytest
 
-import numpy as np
 from ophyd.device import Component as Cpt
 from ophyd.positioner import SoftPositioner
 
-from pcdsdevices.pseudopos import SyncAxes
+from pcdsdevices.pseudopos import SyncAxesBase
 
 logger = logging.getLogger(__name__)
 
 
-AXES = ('one', 'two', 'three', 'four', 'five')
+class FiveSyncSoftPositioner(SyncAxesBase):
+    one = Cpt(SoftPositioner, init_pos=0)
+    two = Cpt(SoftPositioner, init_pos=0)
+    three = Cpt(SoftPositioner, init_pos=0)
+    four = Cpt(SoftPositioner, init_pos=0)
+    five = Cpt(SoftPositioner, init_pos=0)
 
 
 @pytest.fixture(scope='function')
 def five_axes():
-    kwargs = dict(name='sync', egu='five')
-    for ax in AXES:
-        kwargs[ax] = Cpt(SoftPositioner, init_pos=0)
-    return SyncAxes(**kwargs)
+    return FiveSyncSoftPositioner(name='sync', egu='five')
 
 
 def test_sync_passthrough(five_axes):
@@ -27,24 +28,34 @@ def test_sync_passthrough(five_axes):
     assert five_axes.egu == 'five'
 
 
-def test_sync_move(five_axes):
-    logger.debug('test_sync_move')
+def test_sync_basic(five_axes):
+    logger.debug('test_sync_basic')
     five_axes.move(5)
-    for ax in AXES:
-        axis = getattr(five_axes, ax)
-        assert axis.position == 5
+    for pos in five_axes.real_position:
+        assert pos == 5
     assert five_axes.position == 5
 
 
-def test_sync_mean(five_axes):
-    logger.debug('test_sync_mean')
-    for i, ax in enumerate(AXES):
-        axis = getattr(five_axes, ax)
-        axis.move(i)
-    assert five_axes.position == np.mean(range(len(AXES)))
-
-
-def test_bad_sync_kwarg():
-    logger.debug('test_bad_sync_kwarg')
-    with pytest.raises(ValueError):
-        SyncAxes(axes=Cpt(SoftPositioner, init_pos=0))
+def test_sync_offset(five_axes):
+    logger.debug('test_sync_offset')
+    five_axes.one.move(1)
+    five_axes.two.move(2)
+    five_axes.three.move(3)
+    five_axes.four.move(4)
+    five_axes.five.move(5)
+    five_axes.save_offsets()
+    assert five_axes.position == 1
+    five_axes.move(10)
+    assert five_axes.one.position == 10
+    assert five_axes.two.position == 11
+    assert five_axes.three.position == 12
+    assert five_axes.four.position == 13
+    assert five_axes.five.position == 14
+    five_axes.save_offsets(two=10)
+    five_axes.move(11)
+    assert five_axes.one.position == 11
+    assert five_axes.two.position == 21
+    assert five_axes.three.position == 13
+    five_axes._mode = max
+    five_axes.save_offsets()
+    assert five_axes.position == 21
