62c14085f22373f5280d2511f62fde96af264434
Zachary Lentz <zlentz@slac.stanford.edu>
2018-12-06 19:10:21 -0800

    MNT: consolidate all run requirements into requirements.txt file

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import versioneer
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().split()
+
 setup(name='pcdsdevices',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       license='BSD',
       author='SLAC National Accelerator Laboratory',
       packages=find_packages(),
+      install_requires=requirements,
       description='IOC definitions for LCLS Beamline Devices',
       )
