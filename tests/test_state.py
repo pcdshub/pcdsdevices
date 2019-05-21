cedb496db58a70f713a4905011aec42fb99d1ebd
Zachary Lentz <zlentz@slac.stanford.edu>
2019-05-20 18:42:58 -0700

    STY: we use Cpt not Cmp

--- tests/test_state.py
+++ tests/test_state.py
@@ -2,7 +2,7 @@ import logging
 from unittest.mock import Mock
 
 import pytest
-from ophyd.device import Component as Cmp
+from ophyd.device import Component as Cpt
 from ophyd.signal import Signal
 from ophyd.sim import make_fake_device
 
@@ -19,8 +19,8 @@ class PrefixSignal(Signal):
 
 # Define the class
 class LimCls(PVStatePositioner):
-    lowlim = Cmp(PrefixSignal, 'lowlim')
-    highlim = Cmp(PrefixSignal, 'highlim')
+    lowlim = Cpt(PrefixSignal, 'lowlim')
+    highlim = Cpt(PrefixSignal, 'highlim')
 
     _state_logic = {'lowlim': {0: 'in',
                                1: 'defer'},
@@ -44,7 +44,7 @@ class LimCls2(LimCls):
 
 # For additional tests
 class IntState(StatePositioner):
-    state = Cmp(PrefixSignal, 'int', value=2)
+    state = Cpt(PrefixSignal, 'int', value=2)
     states_list = [None, 'UNO', 'OUT']
     _states_alias = {'UNO': ['IN', 'in']}
 
