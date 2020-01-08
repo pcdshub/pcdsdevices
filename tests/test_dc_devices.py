3f3d2bdbeae448c713caf1bd273bc02875371aee
Sameen Yunus <42983327+sfsyunusSLAC@users.noreply.github.com>
2020-01-08 10:04:07 -0800

    TST: correct ch names

--- tests/test_dc_devices.py
+++ tests/test_dc_devices.py
@@ -10,16 +10,16 @@ logger = logging.getLogger(__name__)
 def fake_ICT():
     FakeICT = make_fake_device(ICT)
     PDU = FakeICT('TST:PDU:ICT', name='test')
-    PDU.ch1A.ch_status.sim_set_enum_strs(['ENABLED', 'DISABLED'])
-    PDU.ch1A.ch_status.sim_put('ENABLED')
+    PDU.ch_1A.ch_status.sim_set_enum_strs(['ENABLED', 'DISABLED'])
+    PDU.ch_1A.ch_status.sim_put('ENABLED')
     return PDU
 
 
 def test_PDU_status(fake_ICT):
     logger.debug('test_ICT')
     PDU = fake_ICT
-    assert PDU.ch1A.ch_status.get() == 'ENABLED'
-    PDU.ch1A.off()
-    assert PDU.ch1A.ch_status.get() == 'DISABLED'
-    PDU.ch1A.on()
-    assert PDU.ch1A.ch_status.get() == 'ENABLED'
+    assert PDU.ch_1A.ch_status.get() == 'ENABLED'
+    PDU.ch_1A.off()
+    assert PDU.ch_1A.ch_status.get() == 'DISABLED'
+    PDU.ch_1A.on()
+    assert PDU.ch_1A.ch_status.get() == 'ENABLED'
