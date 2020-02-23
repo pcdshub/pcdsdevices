0180938715887e8a3bab6faf8ef84dabbcd7f864
Alex Wallace <awallace@slac.stanford.edu>
2020-02-23 11:19:33 -0800

    Update kmono.py
    flake8 style

--- pcdsdevices/kmono.py
+++ pcdsdevices/kmono.py
@@ -10,18 +10,18 @@ class Kmono(Device):
     K-edge Monochromator: Used for Undulator tuning and other experiments.
 
     This device has 3 main members: the crystal (xtal), the reticle
-    (ret), and the diode (diode). 
-    
-    The crystal angle motion has a pitch of 1000:1 HD of 0.6 mm. The crystal 
-    angle motor has 1000 steps. The crystal vertical motor moves the in and 
-    out of the beam path. 
+    (ret), and the diode (diode).
+
+    The crystal angle motion has a pitch of 1000:1 HD of 0.6 mm. The crystal
+    angle motor has 1000 steps. The crystal vertical motor moves the in and
+    out of the beam path.
 
     The reticle horizontal motor allows for adjustments in the +/- x direction.
-    The reticle vertical motor moves reticle in and out of the beam path in 
-    the +/- y direction. 
-    
-    The diode horizontal motor allows for adjustments in the +/- x direction. 
-    The diode vertical motor moves reticle in and out of the beam path in 
+    The reticle vertical motor moves reticle in and out of the beam path in
+    the +/- y direction.
+
+    The diode horizontal motor allows for adjustments in the +/- x direction.
+    The diode vertical motor moves reticle in and out of the beam path in
     the +/- y direction.
     """
 
