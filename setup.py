ffb3c86bca271cb7be11aeaed344ce93d569dc62
K Lauer <klauer@slac.stanford.edu>
2020-03-20 16:36:49 -0700

    STY: fix flake8 for setup.py + change to pcdsdevices name

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import find_packages, setup
 
 import versioneer
 
-setup(name='pcdsdevices',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      license='BSD',
-      author='SLAC National Accelerator Laboratory',
-      packages=find_packages(),
-      description='IOC definitions for LCLS Beamline Devices',
-      entry_points={
-            'happi.containers': ['lcls = pcdsdevices.happi.containers'],
-      }
+setup(
+    name='pcdsdevices',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='BSD',
+    author='SLAC National Accelerator Laboratory',
+    packages=find_packages(),
+    description='IOC definitions for LCLS Beamline Devices',
+    entry_points={
+        'happi.containers': ['pcdsdevices = pcdsdevices.happi.containers'],
+    }
 )
