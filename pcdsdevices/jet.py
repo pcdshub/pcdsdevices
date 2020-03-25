d004d49bfe9c23fd5a65c7375cb7a894e32a1a6e
Tyler Pennebaker <tntp@stanford.edu>
2020-03-24 18:27:59 -0700

    WIP: First attempt at Beckhoff liquid jet implementation

---
+++ pcdsdevices/jet.py
@@ -0,0 +1,39 @@
+"""
+Module for the liquid jet classes
+"""
+
+from ophyd import Component as Cpt
+from ophyd import Device
+
+from .epics_motor import BeckhoffAxis
+from .interface import BaseInterface
+
+
+class BeckhoffJetManipulator(Device, BaseInterface):
+    """Jet Manipulator controlled by Beckhoff PLC"""
+
+    tab_component_names = True
+
+    x = Cpt(BeckhoffAxis, ':X', kind='normal')
+    y = Cpt(BeckhoffAxis, ':Y', kind='normal')
+    z = Cpt(BeckhoffAxis, ':Z', kind='normal')
+
+
+class BeckhoffJetSlits(Device, BaseInterface):
+    """Pair of Beckhoff-controlled slits where each blade has X & Y motors"""
+    tab_component_names = True
+
+    top_x = Cpt(BeckhoffAxis, ':TOP_X', kind='normal')
+    top_y = Cpt(BeckhoffAxis, ':TOP_Y', kind='normal')
+    bot_x = Cpt(BeckhoffAxis, ':BOT_X', kind='normal')
+    bot_y = Cpt(BeckhoffAxis, ':BOT_Y', kind='normal')
+
+
+class BeckhoffJet(Device, BaseInterface):
+    """Full liquid jet setup controlled by a Beckhoff PLC"""
+
+    tab_component_names = True
+
+    jet = Cpt(BeckhoffJetManipulator, ':JET', kind='normal')
+    ss = Cpt(BeckhoffJetSlits, ':SLIT', kind='normal')
+    vh_epix_x = Cpt(BeckhoffAxis, ':DET:X', kind='normal')
