d5b7e48e79c7cc0b1fc797dddebd0a1e27cb4d64
Tyler Pennebaker <tntp@stanford.edu>
2020-04-08 09:58:55 -0700

    ENH: Add new devices to device_types.py, plus some missing ones

--- pcdsdevices/device_types.py
+++ pcdsdevices/device_types.py
@@ -2,20 +2,29 @@
 from .analog_signals import Acromag
 from .areadetector.detectors import PCDSAreaDetector
 from .attenuator import Attenuator
+from .beam_stats import BeamStats, SxrGmd
 from .ccm import CCM
+from .dc_devices import ICT
 from .epics_motor import (IMS, PMC100, BeckhoffAxis, DelayNewport, EpicsMotor,
                           Motor, Newport)
 from .evr import Trigger
 from .gauge import GaugeSet
+from .gon import BaseGon, GonWithDetArm, Kappa, SamPhi
 from .inout import Reflaser, TTReflaser
-from .ipm import IPM
-from .lens import XFLS
+from .ipm import IPM, IPM_IPIMB, IPM_Wave8
+from .jet import BeckhoffJet
+from .lens import XFLS, Prefocus
 from .lodcm import LODCM
 from .mirror import OffsetMirror, PointingMirror
 from .movablestand import MovableStand
+from .mps import MPS
 from .pim import PIM, PPM, XPIM, PIMWithBoth, PIMWithFocus, PIMWithLED
+from .pseudopos import DelayBase
 from .pulsepicker import PulsePicker
 from .pump import IonPump
+from .sensors import RTD, TwinCATThermocouple
 from .sequencer import EventSequencer
 from .slits import Slits
+from .spectrometer import Kmono, VonHamos4Crystal
+from .tt import TimeTool, TimeToolWithNav
 from .valve import GateValve, Stopper
