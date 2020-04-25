94c64e59d5b02e27d194a5ebe451d0f196e62f6b
Tyler Pennebaker <tntp@stanford.edu>
2020-04-24 18:12:40 -0700

    TST: Add tests for Injector and SDS classes

--- tests/test_jet.py
+++ tests/test_jet.py
@@ -3,7 +3,7 @@ import logging
 import pytest
 from ophyd.sim import make_fake_device
 
-from pcdsdevices.jet import BeckhoffJet
+from pcdsdevices.jet import BeckhoffJet, Injector
 
 logger = logging.getLogger(__name__)
 
@@ -11,11 +11,18 @@ logger = logging.getLogger(__name__)
 @pytest.mark.timeout(5)
 def test_jet_init():
     logger.debug('test_jet_init')
-    FakeVH = make_fake_device(BeckhoffJet)
-    FakeVH('TST', name='test')
+    FakeJet = make_fake_device(Injector)
+    FakeJet('TST', name='test', coarseX_prefix='X', coarseY_prefix='Y',
+            coarseZ_prefix='Z', fineX_prefix='x', fineY_prefix='y',
+            fineZ_prefix='z')
+    FakeJet = make_fake_device(BeckhoffJet)
+    FakeJet('TST', name='test')
 
 
 @pytest.mark.timeout(5)
 def test_jet_disconnected():
     logger.debug('test_jet_disconnected')
+    Injector('TST', name='test', coarseX_prefix='X', coarseY_prefix='Y',
+             coarseZ_prefix='Z', fineX_prefix='x', fineY_prefix='y',
+             fineZ_prefix='z')
     BeckhoffJet('TST', name='test')
