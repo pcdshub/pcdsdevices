80994449589118bf8906712092070d6b88db8acf
K Lauer <klauer@slac.stanford.edu>
2020-06-10 11:08:19 -0700

    MNT: clean command schema + rearrange

--- pcdsdevices/variety.py
+++ pcdsdevices/variety.py
@@ -42,6 +42,75 @@ varieties_by_category = {
 }
 
 
+def _length_validate(min_count, max_count, type_):
+    """
+    Wrapper to validate a value, based on the given length.
+    """
+
+    def validate(value):
+        try:
+            assert min_count <= len(value) <= max_count
+        except Exception:
+            ...
+        else:
+            for item in value:
+                assert isinstance(item, type_)
+            return True
+
+        if min_count == max_count:
+            raise ValueError(
+                f'Expecting a sequence of {min_count} values ({type_})'
+            )
+
+        raise ValueError(
+            f'Expecting a sequence betweeen {min_count} and {max_count} values'
+            f' ({type_})'
+        )
+
+    return validate
+
+
+schema_by_category = {
+    'command': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['command']),
+        schema.Optional('value', default=1): schema.Or(float, int, str),
+        schema.Optional('enum_strings'): [str],
+        # schema.Optional('enum_dict'): dict,
+    }),
+
+    'tweakable': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['tweakable']),
+        'value': schema.Or(float, int),
+        schema.Optional('value_range'): _length_validate(2, 2, (float, int)),
+        schema.Optional('source_signal'): str,
+        schema.Optional('source'): schema.Or('setpoint', 'readback',
+                                             'other-signal'),
+    }),
+
+    'array': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['array']),
+        schema.Optional('shape'): _length_validate(1, 10, int),
+        schema.Optional('dimension'): int,
+    }),
+
+    'scalar': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['scalar']),
+        schema.Optional('range'): _length_validate(2, 2, (float, int)),
+        schema.Optional('range_source'): schema.Or('use_limits', 'custom'),
+    }),
+
+    'text': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['text']),
+        schema.Optional('enum_strings'): [str],
+    }),
+
+    'enum': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['enum']),
+        schema.Optional('enum_strings'): [str],
+    }),
+}
+
+
 def validate_metadata(md):
     """
     Validate a given metadata dictionary.
@@ -77,78 +146,6 @@ def validate_metadata(md):
         ) from None
 
     return schema.validate(md)
-
-
-def _length_validate(min_count, max_count, type_):
-    """
-    Wrapper to validate a value, based on the given length.
-    """
-
-    def validate(value):
-        try:
-            assert min_count <= len(value) <= max_count
-        except Exception:
-            ...
-        else:
-            for item in value:
-                assert isinstance(item, type_)
-            return True
-
-        if min_count == max_count:
-            raise ValueError(
-                f'Expecting a sequence of {min_count} values ({type_})'
-            )
-
-        raise ValueError(
-            f'Expecting a sequence betweeen {min_count} and {max_count} values'
-            f' ({type_})'
-        )
-
-    return validate
-
-
-schema_by_category = {
-    'command': schema.Schema(
-        {
-            'variety': schema.Or(*varieties_by_category['command']),
-            schema.Optional('value', default=1): schema.Or(float, int, str),
-            schema.Optional('enum_strings'): [str],
-            # schema.Optional('enum_dict'): dict,
-        },
-        description="Schema for the 'command' variety"
-    ),
-
-    'tweakable': schema.Schema({
-        'variety': schema.Or(*varieties_by_category['tweakable']),
-        'value': schema.Or(float, int),
-        schema.Optional('value_range'): _length_validate(2, 2, (float, int)),
-        schema.Optional('source_signal'): str,
-        schema.Optional('source'): schema.Or('setpoint', 'readback',
-                                             'other-signal'),
-    }),
-
-    'array': schema.Schema({
-        'variety': schema.Or(*varieties_by_category['array']),
-        schema.Optional('shape'): _length_validate(1, 10, int),
-        schema.Optional('dimension'): int,
-    }),
-
-    'scalar': schema.Schema({
-        'variety': schema.Or(*varieties_by_category['scalar']),
-        schema.Optional('range'): _length_validate(2, 2, (float, int)),
-        schema.Optional('range_source'): schema.Or('use_limits', 'custom'),
-    }),
-
-    'text': schema.Schema({
-        'variety': schema.Or(*varieties_by_category['text']),
-        schema.Optional('enum_strings'): [str],
-    }),
-
-    'enum': schema.Schema({
-        'variety': schema.Or(*varieties_by_category['enum']),
-        schema.Optional('enum_strings'): [str],
-    }),
-}
 
 
 def _initialize_varieties():
