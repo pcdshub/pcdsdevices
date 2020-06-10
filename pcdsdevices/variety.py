e60a52676a63aef10c9720a865e3f48a64270983
K Lauer <klauer@slac.stanford.edu>
2020-06-10 11:02:28 -0700

    REF: refactor to less messy data structures

--- pcdsdevices/variety.py
+++ pcdsdevices/variety.py
@@ -10,48 +10,36 @@ TODO, something like the following:
 """
 import schema
 
-COMMAND_VARIETIES = (
-    'command',
-    'command-proc',
-    'command-enum',
-    'command-setpoint-tracks-readback',
-)
-
-TWEAKABLE_VARIETIES = [
-    'tweakable',
-]
-
-ARRAY_VARIETIES = [
-    'array-timeseries',
-    'array-histogram',
-    'array-image',
-    'array-nd',
-]
-
-SCALAR_VARIETIES = [
-    'scalar',
-    'scalar-range',
-]
-
-TEXT_VARIETIES = [
-    'text',
-    'text-multiline',
-    'text-enum',
-]
-
-ENUM_VARIETIES = [
-    'enum',
-]
-
-OTHER_VARIETIES = [
-    'unknown',
-]
-
-_schemas = {}
-
-
-def add_variety_schema(variety, schema):
-    _schemas[variety] = schema
+_schema_registry = {}
+varieties_by_category = {
+    'command': {
+        'command',
+        'command-proc',
+        'command-enum',
+        'command-setpoint-tracks-readback',
+    },
+    'tweakable': {
+        'tweakable'
+    },
+    'array': {
+        'array-timeseries',
+        'array-histogram',
+        'array-image',
+        'array-nd',
+    },
+    'scalar': {
+        'scalar',
+        'scalar-range',
+    },
+    'text': {
+        'text',
+        'text-multiline',
+        'text-enum',
+    },
+    'enum': {
+        'enum',
+    },
+}
 
 
 def validate_metadata(md):
@@ -81,25 +69,14 @@ def validate_metadata(md):
         ) from None
 
     try:
-        schema = _schemas[variety]
+        schema = _schema_registry[variety]
     except KeyError:
         raise ValueError(
             f'Unexpected variety: {variety!r}.  Valid options: ' +
-            ', '.join(_schemas)
+            ', '.join(_schema_registry)
         ) from None
 
     return schema.validate(md)
-
-
-COMMAND_SCHEMA = schema.Schema(
-    {
-        'variety': schema.Or(*COMMAND_VARIETIES),
-        schema.Optional('value', default=1): schema.Or(float, int, str),
-        schema.Optional('enum_strings'): [str],
-        # schema.Optional('enum_dict'): dict,
-    },
-    description="Schema for the 'command' variety"
-)
 
 
 def _length_validate(min_count, max_count, type_):
@@ -130,66 +107,56 @@ def _length_validate(min_count, max_count, type_):
     return validate
 
 
-TWEAKABLE_SCHEMA = schema.Schema(
-    {
-        'variety': schema.Or(*TWEAKABLE_VARIETIES),
+schema_by_category = {
+    'command': schema.Schema(
+        {
+            'variety': schema.Or(*varieties_by_category['command']),
+            schema.Optional('value', default=1): schema.Or(float, int, str),
+            schema.Optional('enum_strings'): [str],
+            # schema.Optional('enum_dict'): dict,
+        },
+        description="Schema for the 'command' variety"
+    ),
+
+    'tweakable': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['tweakable']),
         'value': schema.Or(float, int),
         schema.Optional('value_range'): _length_validate(2, 2, (float, int)),
         schema.Optional('source_signal'): str,
         schema.Optional('source'): schema.Or('setpoint', 'readback',
                                              'other-signal'),
-    },
-    description="Schema for the 'tweakable' variety"
-)
+    }),
 
-ARRAY_SCHEMA = schema.Schema(
-    {
-        'variety': schema.Or(*ARRAY_VARIETIES),
+    'array': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['array']),
         schema.Optional('shape'): _length_validate(1, 10, int),
         schema.Optional('dimension'): int,
-    },
-    description="Schema for the 'array' variety"
-)
+    }),
 
-SCALAR_SCHEMA = schema.Schema(
-    {
-        'variety': schema.Or(*SCALAR_VARIETIES),
+    'scalar': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['scalar']),
         schema.Optional('range'): _length_validate(2, 2, (float, int)),
         schema.Optional('range_source'): schema.Or('use_limits', 'custom'),
-    },
-    description="Schema for the 'scalar' variety"
-)
+    }),
 
-TEXT_SCHEMA = schema.Schema(
-    {
-        'variety': schema.Or(*TEXT_VARIETIES),
+    'text': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['text']),
         schema.Optional('enum_strings'): [str],
-    },
-    description="Schema for the 'text' variety"
-)
+    }),
 
-ENUM_SCHEMA = schema.Schema(
-    {
-        'variety': schema.Or(*ENUM_VARIETIES),
+    'enum': schema.Schema({
+        'variety': schema.Or(*varieties_by_category['enum']),
         schema.Optional('enum_strings'): [str],
-    },
-    description="Schema for the 'enum' variety"
-)
+    }),
+}
 
-for variety in COMMAND_VARIETIES:
-    add_variety_schema(variety, COMMAND_SCHEMA)
 
-for variety in TWEAKABLE_VARIETIES:
-    add_variety_schema(variety, TWEAKABLE_SCHEMA)
+def _initialize_varieties():
+    """Add all available varieties + schemas to the module-global registry."""
+    for category, varieties in varieties_by_category.items():
+        schema = schema_by_category[category]
+        for variety in varieties:
+            _schema_registry[variety] = schema
 
-for variety in ARRAY_VARIETIES:
-    add_variety_schema(variety, ARRAY_SCHEMA)
 
-for variety in SCALAR_VARIETIES:
-    add_variety_schema(variety, SCALAR_SCHEMA)
-
-for variety in TEXT_VARIETIES:
-    add_variety_schema(variety, TEXT_SCHEMA)
-
-for variety in ENUM_VARIETIES:
-    add_variety_schema(variety, ENUM_SCHEMA)
+_initialize_varieties()
