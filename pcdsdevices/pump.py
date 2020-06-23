58f52f29ee97d6adb5fe8ce924f61792d462444a
n-wbrown <n-wbrown@users.noreply.github.com>
2020-06-23 09:04:20 -0700

    DOC: punctuation
    Co-authored-by: Tyler Pennebaker <tntptntp@gmail.com>

--- pcdsdevices/pump.py
+++ pcdsdevices/pump.py
@@ -214,7 +214,7 @@ class PROPLC(Device):
 
 
 class Ebara_EV_A03_1(PROPLC):
-    """Class for the Ebara EV-A03-1 model of roughing pump"""
+    """Class for the Ebara EV-A03-1 model of roughing pump."""
     remote = Cpt(EpicsSignalWithRBV, ':REMOTE', kind='omitted')
     alarm = Cpt(EpicsSignalRO, ':ALARM_OK_RBV', kind='omitted')
     run_di = Cpt(EpicsSignalWithRBV, ':RUN_DI_RBV', kind='omitted')
