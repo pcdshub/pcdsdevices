f3f9669492b8207f1354e153993b0d3ce3776be9
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2020-06-26 12:31:59 -0700

    BUG: Update some PVs for RO

--- pcdsdevices/pump.py
+++ pcdsdevices/pump.py
@@ -172,10 +172,10 @@ class PIPPLC(Device):
     high_voltage_switch = Cpt(EpicsSignalWithRBV, ':HV_SW', kind='config',
                               doc='epics command to switch on the '
                               'high voltage')
-    plc_ai_offset = Cpt(EpicsSignalWithRBV, ':AI_Offset', kind='config',
+    plc_ai_offset = Cpt(EpicsSignalRO, ':AI_Offset_RBV', kind='config',
                         doc=('Analog input offset must match ion pump '
                              'analog ouput offset. Default: 13'))
-    auto_on = Cpt(EpicsSignalWithRBV, ':Auto_On', kind='config',
+    auto_on = Cpt(EpicsSignalRO, ':Auto_On_RBV', kind='config',
                   doc=('Setting to automatically turn on the ion pump when the'
                        'reference gauge pressure is below protection '
                        'setpoint'))
