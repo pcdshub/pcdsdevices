cefa8d9edaee14f142bae2d7abd071a9534b4432
slactjohnson <tjohnson@slac.stanford.edu>
2020-08-03 10:09:43 -0700

    Fix/add some docstrings, fix Flake8 issues.

--- pcdsdevices/lasers/elliptec.py
+++ pcdsdevices/lasers/elliptec.py
@@ -11,19 +11,8 @@ from ophyd.signal import EpicsSignal, EpicsSignalRO
 class EllBase(Device):
     """
     Base class for Elliptec stages.
-
-    Sufficient for control of ELL17/20 (28/60mm linear stages) and ELL14/18
-    (rotation) stages.
-
-    Parameters
-    ----------
-    prefix : str
-        The PV base of the stage.
-    channel : str 
-        The motor channel on the controller (0-E)
-    port : str
-        The port of the Elliptec controller (typically 0)
     """
+
     set_position = FCpt(EpicsSignal, '{prefix}:M{self._channel}:CURPOS',
                         write_pv='{prefix}:M{self._channel}:MOVE',
                         kind='normal')
@@ -42,12 +31,26 @@ class EllBase(Device):
 
     def __init__(self, prefix, port=0, channel=1, **kwargs):
         self._port = port
-        self._channel=channel
+        self._channel = channel
         super().__init__(prefix, **kwargs)
+
 
 class Ell6(EllBase):
     """
     Class for Thorlabs ELL6 2 position filter slider.
+
+    Parameters
+    ----------
+    prefix : str
+        The PV base of the stage.
+    channel : str
+        The motor channel on the controller (0-F)
+    port : str
+        The port of the Elliptec controller (typically 0)
+
+    Examples
+    --------
+    ell6 = Ell6('LM1K4:COM_DP1_TF1_SL1:ELL', port=0, channel=1, name='ell6')
     """
 
     # Names for slider positions
@@ -60,6 +63,19 @@ class Ell6(EllBase):
 class Ell9(Ell6):
     """
     Class for Thorlabs ELL9 4 position filter slider.
+
+    Parameters
+    ----------
+    prefix : str
+        The PV base of the stage.
+    channel : str
+        The motor channel on the controller (0-F)
+    port : str
+        The port of the Elliptec controller (typically 0)
+
+    Examples
+    --------
+    ell9 = Ell9('LM1K4:COM_DP1_TF1_SL1:ELL', port=0, channel=1, name='ell9')
     """
 
     # Names for slider positions
@@ -72,7 +88,22 @@ class Ell9(Ell6):
 class EllLinear(EllBase):
     """
     Class for Thorlabs ELL17/20 (28/60mm) linear stage.
+
+    Parameters
+    ----------
+    prefix : str
+        The PV base of the stage.
+    channel : str
+        The motor channel on the controller (0-F)
+    port : str
+        The port of the Elliptec controller (typically 0)
+
+    Examples
+    --------
+    ell17 = EllLinear('LM1K4:COM_DP1_TF1_LIN1:ELL', port=0, channel=1,
+                       name='ell17')
     """
+
     _current_precision = FCpt(EpicsSignal,
                               '{prefix}:M{self._channel}:CURPOS.PREC',
                               kind='omitted')
@@ -84,8 +115,25 @@ class EllLinear(EllBase):
     _target_egu = FCpt(EpicsSignal, '{prefix}:M{self._channel}:MOVE.EGU',
                        kind='omitted')
 
- 
+
 class EllRotation(EllLinear):
     """
     Class for Thorlabs ELL14/18 (rotation) stages.
+
+    Parameters
+    ----------
+    prefix : str
+        The PV base of the stage.
+    channel : str
+        The motor channel on the controller (0-F)
+    port : str
+        The port of the Elliptec controller (typically 0)
+
+    Examples
+    --------
+    ell14 = EllRotation('LM1K4:COM_DP1_TF1_ROT1:ELL', port=0, channel=1,
+                         name='ell14')
     """
+    # Currently no difference between rotation implementation and linear
+    # implementation, but there may be eventually, and I couldn't come up with
+    # a good name to encapsulate them both.
