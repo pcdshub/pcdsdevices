049c6874e481ff80c972b084febfa4887d76b2e4
Zachary Lentz <zlentz@slac.stanford.edu>
2020-08-14 16:00:49 -0700

    WIP: vernier

--- pcdsdevices/ccm.py
+++ pcdsdevices/ccm.py
@@ -5,14 +5,13 @@ import numpy as np
 from ophyd.device import Component as Cpt
 from ophyd.device import FormattedComponent as FCpt
 from ophyd.pseudopos import PseudoPositioner
-from ophyd.pv_positioner import PVPositioner
 from ophyd.signal import AttributeSignal, EpicsSignal, EpicsSignalRO, Signal
 
 from .epics_motor import IMS, EpicsMotorInterface
 from .inout import InOutPositioner
 from .interface import FltMvInterface
 from .pseudopos import PseudoSingleInterface, SyncAxesBase
-from .signal import InternalSignal
+from .pv_positioner import PVPositionerIsClose
 
 logger = logging.getLogger(__name__)
 
@@ -27,7 +26,7 @@ default_gr = 3.175
 default_gd = 231.303
 
 
-class CCMMotor(PVPositioner, FltMvInterface):
+class CCMMotor(PVPositionerIsClose):
     """
     Goofy records used in the CCM.
     """
@@ -35,42 +34,19 @@ class CCMMotor(PVPositioner, FltMvInterface):
     setpoint = Cpt(EpicsSignal, ":POSITIONSET", auto_monitor=True)
     readback = Cpt(EpicsSignalRO, ":POSITIONGET", auto_monitor=True,
                    kind='hinted')
-    done = Cpt(InternalSignal, value=0)
-    done_value = 1
 
-    limits = None
+    # Tolerance from old xcs python code
+    atol = 3e-4
 
-    def __init__(self, prefix, *, name, **kwargs):
-        self._last_readback = None
-        self._last_setpoint = None
-        super().__init__(prefix, name=name, **kwargs)
 
-    @readback.sub_value
-    def _update_readback(self, *args, value, **kwargs):
-        """Callback to cache the readback and update done state."""
-        self._last_readback = value
-        self._update_done()
+class VernierMotor(PVPositionerIsClose):
+    """
+    Dummy motor that sets and reads from the vernier PV.
 
-    @setpoint.sub_value
-    def _update_setpoint(self, *args, value, **kwargs):
-        """Callback to cache the setpoint and update done state."""
-        self._last_setpoint = value
-        # Always set done to False when a move is requested
-        # This means we always get a rising edge when finished moving
-        # Even if the move distance is under our done moving tolerance
-        self.done.put(0, force=True)
-        self._update_done()
-
-    def _update_done(self):
-        """
-        Update our status to done if we are within the tolerance.
-
-        This tolerance of 3e-4 was copied as-is from old xcs python code.
-        """
-        if None not in (self._last_readback, self._last_setpoint):
-            is_done = np.isclose(self._last_readback, self._last_setpoint,
-                                 atol=3e-4)
-            self.done.put(int(is_done), force=True)
+    Pseudo positioners need a positioner subclass to work properly.
+    """
+    setpoint = Cpt(EpicsSignal, "", auto_monitor=True)
+    readback = Cpt(EpicsSignal, "", auto_monitor=True)
 
 
 class CCMPico(EpicsMotorInterface):
@@ -109,26 +85,37 @@ class CCMCalc(PseudoPositioner, FltMvInterface):
     """
 
     energy = Cpt(PseudoSingleInterface, egu='keV', kind='hinted')
-    wavelength = Cpt(PseudoSingleInterface, egu='A')
-    theta = Cpt(PseudoSingleInterface, egu='deg')
-    alio = Cpt(CCMMotor, '')
+    wavelength = Cpt(PseudoSingleInterface, egu='A', kind='normal')
+    theta = Cpt(PseudoSingleInterface, egu='deg', kind='normal')
+    energy_with_vernier = Cpt(PseudoSingleInterface, egu='keV',
+                              kind='omitted')
+
+    alio = Cpt(CCMMotor, '', kind='normal')
+    vernier = FCpt(VernierMotor, '{vernier_prefix}', kind='normal')
 
     tab_component_names = True
 
     def __init__(self, *args, theta0=default_theta0, dspacing=default_dspacing,
-                 gr=default_gr, gd=default_gd, **kwargs):
+                 gr=default_gr, gd=default_gd, vernier_prefix='', **kwargs):
         self.theta0 = theta0
         self.dspacing = dspacing
         self.gr = gr
         self.gd = gd
+        self.vernier_prefix = vernier_prefix
         super().__init__(*args, auto_target=False, **kwargs)
 
     def forward(self, pseudo_pos):
-        """Take energy, wavelength, or theta and map to alio."""
+        """
+        Take energy, wavelength, theta, or energy_with_vernier and map to
+        alio and vernier.
+        """
         pseudo_pos = self.PseudoPosition(*pseudo_pos)
         # Figure out which one changed.
-        energy, wavelength, theta = None, None, None
-        if not np.isclose(pseudo_pos.energy, self.energy.position):
+        energy_with_vernier, energy, wavelength, theta = None, None, None, None
+        if not np.isclose(pseudo_pos.energy_with_vernier,
+                          self.energy_with_vernier.position):
+            energy_with_vernier = pseudo_pos.energy_with_vernier
+        elif not np.isclose(pseudo_pos.energy, self.energy.position):
             energy = pseudo_pos.energy
         elif not np.isclose(pseudo_pos.wavelength, self.wavelength.position):
             wavelength = pseudo_pos.wavelength
@@ -136,7 +123,12 @@ class CCMCalc(PseudoPositioner, FltMvInterface):
             theta = pseudo_pos.theta
         else:
             alio = self.alio.position
-        logger.debug((energy, wavelength, theta))
+        logger.debug((energy, wavelength, theta, energy_with_vernier))
+        if energy_with_vernier is not None:
+            energy = energy_with_vernier
+            vernier = energy_with_vernier
+        else:
+            vernier = self.vernier.position
         if energy is not None:
             wavelength = energy_to_wavelength(energy)
         if wavelength is not None:
@@ -144,7 +136,7 @@ class CCMCalc(PseudoPositioner, FltMvInterface):
         if theta is not None:
             alio = theta_to_alio(theta * np.pi/180, self.theta0,
                                  self.gr, self.gd)
-        return self.RealPosition(alio=alio)
+        return self.RealPosition(alio=alio, vernier=vernier)
 
     def inverse(self, real_pos):
         """Take alio and map to energy, wavelength, and theta."""
@@ -154,7 +146,8 @@ class CCMCalc(PseudoPositioner, FltMvInterface):
         energy = wavelength_to_energy(wavelength)
         return self.PseudoPosition(energy=energy,
                                    wavelength=wavelength,
-                                   theta=theta*180/np.pi)
+                                   theta=theta*180/np.pi,
+                                   energy_with_vernier=energy)
 
 
 class CCMX(SyncAxesBase):
---
+++ pcdsdevices/pv_positioner.py
@@ -0,0 +1,86 @@
+import numpy as np
+from ophyd.device import Component as Cpt
+from ophyd.positioner import PVPositioner
+
+from .interface import FltMvInterface
+from .signal import InternalSignal
+
+
+class PVPositionerComparator(PVPositioner, FltMvInterface):
+    """
+    PV Positioner with a software done signal.
+
+    The done state is set by a comparison function defined in the class body.
+    The comparison function takes two arguments, readback and setpoint,
+    returning True if we are close enough to be considered done or False if we
+    are too far away.
+    """
+
+    # Override setpoint, readback in subclass
+    setpoint = None
+    readback = None
+
+    done = Cpt(InternalSignal, value=0)
+    done_value = 1
+
+    # Optionally override limits to a 2-element tuple in subclass
+    limits = None
+
+    def __init__(self, prefix, *, name, **kwargs):
+        self._last_readback = None
+        self._last_setpoint = None
+        super().__init__(prefix, name=name, **kwargs)
+
+    def done_comparator(self, readback, setpoint):
+        """Override done_comparator in subclass."""
+        raise NotImplementedError('Must implement a done comparator!')
+
+    def __init_subclass__(cls, **kwargs):
+        """Set up callbacks in subclass."""
+        super().__init_subclass__(**kwargs)
+        if None not in (cls.setpoint, cls.readback):
+            cls.setpoint.sub_value(cls._update_setpoint)
+            cls.readback.sub_value(cls._update_readback)
+
+    def _update_setpoint(self, *args, value, **kwargs):
+        """Callback to cache the setpoint and update done state."""
+        self._last_setpoint = value
+        # Always set done to False when a move is requested
+        # This means we always get a rising edge when finished moving
+        # Even if the move distance is under our done moving tolerance
+        self.done.put(0, force=True)
+        self._update_done()
+
+    def _update_readback(self, *args, value, **kwargs):
+        """Callback to cache the readback and update done state."""
+        self._last_readback = value
+        self._update_done()
+
+    def _update_done(self):
+        """Update our status to done if we pass the comparator."""
+        if None not in (self._last_readback, self._last_setpoint):
+            is_done = self.done_comparator(self._last_readback,
+                                           self._last_setpoint)
+            self.done.put(int(is_done), force=True)
+
+
+class PVPositionerIsClose(PVPositionerComparator):
+    """
+    PV Positioner that updates done state based on np.isclose.
+
+    The arguments atol and rtol can be set as class attributes or passed as
+    initialization arguments.
+    """
+
+    atol = None
+    rtol = None
+
+    def __init__(self, prefix, *, name, atol=None, rtol=None, **kwargs):
+        if atol is not None:
+            self.atol = atol
+        if rtol is not None:
+            self.rtol = rtol
+        super().__init__(prefix, name=name, **kwargs)
+
+    def done_comparator(self, readback, setpoint):
+        return np.isclose(readback, setpoint, atol=self.atol, rtol=self.rtol)
