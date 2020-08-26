84312a9b31cac40b8f2381c2cc610eb1f7ac04a5
Zachary Lentz <zlentz@slac.stanford.edu>
2020-08-26 14:00:09 -0700

    TST: try to fix tests a bit

--- tests/test_epics_motor.py
+++ tests/test_epics_motor.py
@@ -28,7 +28,8 @@ def motor_setup(motor):
     """
     if isinstance(motor, EpicsMotorInterface):
         motor.user_readback.sim_put(0)
-        motor.limits = (-100, 100)
+        motor.high_limit_travel.put(100)
+        motor.low_limit_travel.put(-100)
 
     if isinstance(motor, PCDSMotorBase):
         motor.motor_spg.sim_put(2)
@@ -97,6 +98,17 @@ def test_epics_motor_soft_limits(fake_epics_motor):
     # Check that we can not move past the soft limits
     with pytest.raises(ValueError):
         m.move(-150)
+    # Try the local soft limits override
+    m.limits = (-50, 50)
+    with pytest.raises(ValueError):
+        m.move(-75)
+    m.low_limit = -25
+    with pytest.raises(ValueError):
+        m.move(-40)
+    m.high_limit = 25
+    with pytest.raises(ValueError):
+        m.move(40)
+
 
 
 def test_epics_motor_tdir(fake_pcds_motor):
