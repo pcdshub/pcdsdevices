3862accbab35a64857a026e3924fedd9408e6ee0
K Lauer <klauer@slac.stanford.edu>
2020-09-16 11:47:19 -0700

    DEV: warn user about missing args

--- docs/pre-release-notes.sh
+++ docs/pre-release-notes.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 ISSUE=$1
 DESCRIPTION=$2
+
+if [[ -z "$ISSUE" || -z "$DESCRIPTION" ]]; then
+    echo "Usage: $0 (ISSUE) '(DESCRIPTION)'"
+    exit 1
+fi
+
 FILENAME=source/upcoming_release_notes/${ISSUE}-${DESCRIPTION}.rst
 
 pushd "$(dirname "$0")"
