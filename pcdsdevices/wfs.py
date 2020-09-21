a5a9ab87e254c58dccabaa9c2a8306fcf04dea5c
Zachary Lentz <zlentz@slac.stanford.edu>
2020-09-21 11:16:35 -0700

    ENH: add wfs target

---
+++ pcdsdevices/wfs.py
@@ -0,0 +1,26 @@
+from ophyd import Component as Cpt
+from ophyd import Device
+
+from .epics_motor import BeckhoffAxis
+from .inout import TwinCATInOutPositioner
+from .interface import BaseInterface, LightpathInOutMixin
+from .sensors import TwinCATThermocouple
+
+
+class WaveFrontSensorTarget(BaseInterface, Device, LightpathInOutMixin):
+    tab_component_names = True
+
+    lightpath_cpts = ['target']
+    _icon = 'fa.ellipsis-v'
+
+    target = Cpt(TwinCATInOutPositioner, ':MMS:STATE', kind='hinted',
+                 doc='Control of the diagnostic stack via saved positions.')
+    y_motor = Cpt(BeckhoffAxis, ':MMS:Y', kind='normal',
+                  doc='Direct control of the diagnostic stack motor.')
+    z_motor = Cpt(BeckhoffAxis, ':MMS:Z', kind='normal',
+                  doc='Z position of target stack for focus control.')
+
+    thermocouple1 = Cpt(TwinCATThermocouple, ':STC:01', kind='normal',
+                        doc='First thermocouple.')
+    thermocouple2 = Cpt(TwinCATThermocouple, ':STC:02', kind='normal',
+                        doc='Second thermocouple.')
