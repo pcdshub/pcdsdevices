ac7dac39aa43e0be7dd921b7727b3d800b30c36a
slactjohnson <tjohnson@slac.stanford.edu>
2020-10-23 11:03:01 -0700

    STY: Fix typos, make names more human readable, add doc kwargs

--- pcdsdevices/lasers/ek9000.py
+++ pcdsdevices/lasers/ek9000.py
@@ -18,14 +18,14 @@ class El3174AiCh(Device):
     prefix : str
         The PV base of the card.
     """
-    measured = Cpt(EpicsSignalRO, '.VAL', kind='normal')
-    raw_adc = Cpt(EpicsSignalRO, '.RVAL', kind='config')
-    egu = Cpt(EpicsSignal, '.EGU', kind='config')
-    # TJ: These may be usefult late, but not now
+    measured = Cpt(EpicsSignalRO, '.VAL', kind='normal', doc='Converted value')
+    raw_adc = Cpt(EpicsSignalRO, '.RVAL', kind='config', doc='Raw ADC count')
+    egu = Cpt(EpicsSignal, '.EGU', kind='config', doc='Engineering units')
+    # TJ: These may be useful later, but not now
     # egu_max = Cpt(EpicsSignal, '.EGUF', kind='config')
     # egu_min = Cpt(EpicsSignal, '.EGUL', kind='config')
-    eslo = Cpt(EpicsSignal, '.ESLO', kind='config')
-    eoff = Cpt(EpicsSignal, '.EOFF', kind='config')
+    slope = Cpt(EpicsSignal, '.ESLO', kind='config', doc='EGU per ADC count')
+    offset = Cpt(EpicsSignal, '.EOFF', kind='config', doc='Offset in EGU')
     conversion = Cpt(EpicsSignal, '.LINR', kind='config')
     precision = Cpt(EpicsSignal, '.PREC', kind='config')
 
