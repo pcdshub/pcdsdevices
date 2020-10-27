ee85742a600dc7d059a68865fe37cfdc1e5c769a
K Lauer <klauer@slac.stanford.edu>
2020-10-27 16:42:59 -0700

    FIX: import laser devices; implicitly test interface through find_all_device_classes

--- pcdsdevices/device_types.py
+++ pcdsdevices/device_types.py
@@ -14,6 +14,11 @@ from .gon import BaseGon, Goniometer, GonWithDetArm, Kappa, SamPhi, XYZStage
 from .inout import Reflaser, TTReflaser
 from .ipm import IPM, IPM_IPIMB, IPM_Wave8
 from .jet import BeckhoffJet
+from .lasers.ek9000 import El3174AiCh, EnvironmentalMonitor
+from .lasers.elliptec import Ell6, Ell9, EllBase, EllLinear, EllRotation
+from .lasers.qmini import QminiSpectrometer
+from .lasers.thorlabsWFS import ThorlabsWfs40
+from .lasers.zoomtelescope import ZoomTelescope
 from .lens import XFLS, Prefocus
 from .lic import LaserInCoupling
 from .lodcm import LODCM
