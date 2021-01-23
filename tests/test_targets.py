aabe4448159077306255da79f9626f811cde36ee
cristinasewell <csewell@slac.stanford.edu>
2021-01-22 17:25:58 -0800

    TST: updated some tests

--- tests/test_targets.py
+++ tests/test_targets.py
@@ -1,7 +1,9 @@
 import pytest
 import numpy as np
 from ophyd.sim import make_fake_device
-from pcdsdevices.targets import XYGridStage
+from pcdsdevices.targets import (XYGridStage, convert_to_physical,
+                                 get_unit_meshgrid, mesh_interpolation,
+                                 snake_grid_list)
 from pcdsdevices.sim import FastMotor
 
 
@@ -10,7 +12,52 @@ def sample_file(tmp_path):
     path = tmp_path / "sub"
     path.mkdir()
     sample_file = path / "samples.yml"
-    sample_file.write_text('')
+    sample_file.write_text("""
+test_sample:
+  time_created: '2021-01-22 14:29:29.681059'
+  top_left:
+  - -20.59374999999996
+  - 26.41445312499999
+  top_right:
+  - -19.838671874999946
+  - 26.408203124999986
+  bottom_right:
+  - -19.426171874999945
+  - 51.39570312500023
+  bottom_left:
+  - -20.176171875000115
+  - 51.414453125000215
+  M: 101
+  N: 4
+  coefficients:
+  - -20.59374999999996
+  - 0.7550781250000149
+  - 0.4175781249998458
+  - -0.005078124999844391
+  - 26.41445312499999
+  - -0.006250000000004974
+  - 25.000000000000224
+  - -0.012499999999977973
+  last_shot_index: -1
+  xx:
+  - -20.59374999999996
+  - -20.342057291666624
+  - -20.090364583333283
+  - -19.838671874999946
+  - -20.589574218749963
+  - -20.33789843749996
+  - -20.08622265624995
+  - -19.834546874999948
+  yy:
+  - 26.41445312499999
+  - 26.412369791666656
+  - 26.41028645833332
+  - 26.408203124999986
+  - 26.664453124999994
+  - 26.66232812499999
+  - 26.660203124999992
+  - 26.65807812499999
+    """)
     return sample_file
 
 
@@ -34,12 +81,13 @@ def fake_grid_stage(sample_file):
 def test_get_samples(fake_grid_stage, sample_file):
     xy = fake_grid_stage
     res = xy.get_samples()
-    assert res == []
+    # existing sample already
+    assert res == ['test_sample']
     xy.save_grid(sample_name='sample1', path=sample_file)
     xy.save_grid(sample_name='sample2', path=sample_file)
     # test all mapped samples
     res = xy.get_samples(path=sample_file)
-    assert res == ['sample1', 'sample2']
+    assert res == ['test_sample', 'sample1', 'sample2']
     with pytest.raises(Exception):
         xy.get_samples(paht='bad_file')
 
@@ -79,75 +127,7 @@ def test_get_sample_map_info(fake_grid_stage, sample_file):
     coeffs = [0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0]
     # test get_sample_map_info - dummy values
     res = xy.get_sample_map_info('sample2')
-    assert res == (5, 5, coeffs)
-
-
-def test_mesh_interpolation(fake_grid_stage):
-    xy = fake_grid_stage
-    a_coeffs, b_coeffs = xy.mesh_interpolation((0, 0), (4, 0), (4, 4),
-                                               (0, 4))
-    assert np.isclose(a_coeffs, [0.0, 4.0,  0.0, 0.0]).all()
-    assert np.isclose(b_coeffs, [0.0, 0.0, 4.0, 0.0]).all()
-    # test with slope -0.25
-    a_coeffs, b_coeffs = xy.mesh_interpolation((0, 0), (4, -1), (5, 3),
-                                               (1, 4))
-    assert np.isclose(a_coeffs, [0.0, 4.0,  1.0, 0.0]).all()
-    assert np.isclose(b_coeffs, [0.0, -1.0, 4.0, 0.0]).all()
-
-
-def test_get_unit_meshgrid(fake_grid_stage):
-    xy = fake_grid_stage
-    grid = xy.get_unit_meshgrid(m_rows=5, n_columns=5)
-    xx_expected = [[0, 0.25, 0.5, 0.75, 1.0],
-                   [0, 0.25, 0.5, 0.75, 1.0],
-                   [0, 0.25, 0.5, 0.75, 1.0],
-                   [0, 0.25, 0.5, 0.75, 1.0],
-                   [0, 0.25, 0.5, 0.75, 1.0]]
-    yy_expected = [[0.0, 0.0, 0.0, 0.0, 0.0],
-                   [0.25, 0.25, 0.25, 0.25, 0.25],
-                   [0.5, 0.5, 0.5, 0.5, 0.5],
-                   [0.75, 0.75, 0.75, 0.75, 0.75],
-                   [1.0, 1.0, 1.0, 1.0, 1.0]]
-
-    assert grid[0].tolist() == xx_expected
-    assert grid[1].tolist() == yy_expected
-
-
-def test_convert_to_physical(fake_grid_stage):
-    a_coeffs = [0.0, 4.0, 0.0, 0.0]
-    b_coeffs = [0.0, 0.0, 4.0, 0.0]
-    # based on these logical values:
-    # xx_expected = [[0, 0.25, 0.5, 0.75, 1.0],
-    #                [0, 0.25, 0.5, 0.75, 1.0],
-    #                [0, 0.25, 0.5, 0.75, 1.0],
-    #                [0, 0.25, 0.5, 0.75, 1.0],
-    #                [0, 0.25, 0.5, 0.75, 1.0]]
-    # yy_expected = [[0.0, 0.0, 0.0, 0.0, 0.0],
-    #                [0.25, 0.25, 0.25, 0.25, 0.25],
-    #                [0.5, 0.5, 0.5, 0.5, 0.5],
-    #                [0.75, 0.75, 0.75, 0.75, 0.75],
-    #                [1.0, 1.0, 1.0, 1.0, 1.0]]
-    # should get these results:
-    # expected_x_points = [0.0, 1.0, 2.0, 3.0, 4.0,
-    #                      0.0, 1.0, 2.0, 3.0, 4.0,
-    #                      0.0, 1.0, 2.0, 3.0, 4.0,
-    #                      0.0, 1.0, 2.0, 3.0, 4.0,
-    #                      0.0, 1.0, 2.0, 3.0, 4.0]
-
-    # expected_y_points = [0.0, 0.0, 0.0, 0.0, 0.0,
-    #                      1.0, 1.0, 1.0, 1.0, 1.0,
-    #                      2.0, 2.0, 2.0, 2.0, 2.0,
-    #                      3.0, 3.0, 3.0, 3.0, 3.0,
-    #                      4.0, 4.0, 4.0, 4.0, 4.0]
-    x, y = fake_grid_stage.convert_to_physical(a_coeffs, b_coeffs, 0, 0)
-    # should be 0, 0
-    assert (x, y) == (0.0, 0.0)
-    x, y = fake_grid_stage.convert_to_physical(a_coeffs, b_coeffs, 0.75, 0.75)
-    # should be 3, 3
-    assert (x, y) == (3.0, 3.0)
-    x, y = fake_grid_stage.convert_to_physical(a_coeffs, b_coeffs, 0.5, 0.0)
-    # should be 2, 0
-    assert (x, y) == (2.0, 0.0)
+    assert res == (5, 5, coeffs, -1)
 
 
 def test_mapping_points(fake_grid_stage):
@@ -278,3 +258,101 @@ def test_set_m_n_points(fake_grid_stage):
     assert stage.m_n_points == (10, 23)
     with pytest.raises(Exception):
         stage.m_n_points = 23
+
+
+def test_mesh_interpolation():
+    a_coeffs, b_coeffs = mesh_interpolation((0, 0), (4, 0), (4, 4),
+                                            (0, 4))
+    assert np.isclose(a_coeffs, [0.0, 4.0,  0.0, 0.0]).all()
+    assert np.isclose(b_coeffs, [0.0, 0.0, 4.0, 0.0]).all()
+    # test with slope -0.25
+    a_coeffs, b_coeffs = mesh_interpolation((0, 0), (4, -1), (5, 3),
+                                            (1, 4))
+    assert np.isclose(a_coeffs, [0.0, 4.0,  1.0, 0.0]).all()
+    assert np.isclose(b_coeffs, [0.0, -1.0, 4.0, 0.0]).all()
+
+
+def test_get_unit_meshgrid():
+    grid = get_unit_meshgrid(m_rows=5, n_columns=5)
+    xx_expected = [[0, 0.25, 0.5, 0.75, 1.0],
+                   [0, 0.25, 0.5, 0.75, 1.0],
+                   [0, 0.25, 0.5, 0.75, 1.0],
+                   [0, 0.25, 0.5, 0.75, 1.0],
+                   [0, 0.25, 0.5, 0.75, 1.0]]
+    yy_expected = [[0.0, 0.0, 0.0, 0.0, 0.0],
+                   [0.25, 0.25, 0.25, 0.25, 0.25],
+                   [0.5, 0.5, 0.5, 0.5, 0.5],
+                   [0.75, 0.75, 0.75, 0.75, 0.75],
+                   [1.0, 1.0, 1.0, 1.0, 1.0]]
+
+    assert grid[0].tolist() == xx_expected
+    assert grid[1].tolist() == yy_expected
+
+
+def test_convert_to_physical():
+    a_coeffs = [0.0, 4.0, 0.0, 0.0]
+    b_coeffs = [0.0, 0.0, 4.0, 0.0]
+    # based on these logical values:
+    # xx_expected = [[0, 0.25, 0.5, 0.75, 1.0],
+    #                [0, 0.25, 0.5, 0.75, 1.0],
+    #                [0, 0.25, 0.5, 0.75, 1.0],
+    #                [0, 0.25, 0.5, 0.75, 1.0],
+    #                [0, 0.25, 0.5, 0.75, 1.0]]
+    # yy_expected = [[0.0, 0.0, 0.0, 0.0, 0.0],
+    #                [0.25, 0.25, 0.25, 0.25, 0.25],
+    #                [0.5, 0.5, 0.5, 0.5, 0.5],
+    #                [0.75, 0.75, 0.75, 0.75, 0.75],
+    #                [1.0, 1.0, 1.0, 1.0, 1.0]]
+    # should get these results:
+    # expected_x_points = [0.0, 1.0, 2.0, 3.0, 4.0,
+    #                      0.0, 1.0, 2.0, 3.0, 4.0,
+    #                      0.0, 1.0, 2.0, 3.0, 4.0,
+    #                      0.0, 1.0, 2.0, 3.0, 4.0,
+    #                      0.0, 1.0, 2.0, 3.0, 4.0]
+
+    # expected_y_points = [0.0, 0.0, 0.0, 0.0, 0.0,
+    #                      1.0, 1.0, 1.0, 1.0, 1.0,
+    #                      2.0, 2.0, 2.0, 2.0, 2.0,
+    #                      3.0, 3.0, 3.0, 3.0, 3.0,
+    #                      4.0, 4.0, 4.0, 4.0, 4.0]
+    x, y = convert_to_physical(a_coeffs, b_coeffs, 0, 0)
+    # should be 0, 0
+    assert (x, y) == (0.0, 0.0)
+    x, y = convert_to_physical(a_coeffs, b_coeffs, 0.75, 0.75)
+    # should be 3, 3
+    assert (x, y) == (3.0, 3.0)
+    x, y = convert_to_physical(a_coeffs, b_coeffs, 0.5, 0.0)
+    # should be 2, 0
+    assert (x, y) == (2.0, 0.0)
+
+
+def test_snake_like_list():
+    xx = np.array([[0, 0.25, 0.5, 0.75, 1.0],
+                   [0, 0.25, 0.5, 0.75, 1.0],
+                   [0, 0.25, 0.5, 0.75, 1.0],
+                   [0, 0.25, 0.5, 0.75, 1.0],
+                   [0, 0.25, 0.5, 0.75, 1.0]])
+    yy = np.array([[0.0, 0.0, 0.0, 0.0, 0.0],
+                   [0.25, 0.25, 0.25, 0.25, 0.25],
+                   [0.5, 0.5, 0.5, 0.5, 0.5],
+                   [0.75, 0.75, 0.75, 0.75, 0.75],
+                   [1.0, 1.0, 1.0, 1.0, 1.0]])
+
+    # expected values:
+    xx_expected = [0, 0.25, 0.5, 0.75, 1.0,
+                   1.0, 0.75, 0.5, 0.25, 0,
+                   0, 0.25, 0.5, 0.75, 1.0,
+                   1.0, 0.75, 0.5, 0.25, 0,
+                   0, 0.25, 0.5, 0.75, 1.0]
+    yy_expected = [0.0, 0.0, 0.0, 0.0, 0.0,
+                   0.25, 0.25, 0.25, 0.25, 0.25,
+                   0.5, 0.5, 0.5, 0.5, 0.5,
+                   0.75, 0.75, 0.75, 0.75, 0.75,
+                   1.0, 1.0, 1.0, 1.0, 1.0]
+
+    xx_res = snake_grid_list(xx)
+    # the y values are basically stying the same
+    # so there is no need to even run thm through this function
+    yy_res = snake_grid_list(yy)
+    assert xx_res == xx_expected
+    assert yy_res == yy_expected
