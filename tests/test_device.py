4674c42c07b5ef1814d1178df2c5bbbe29af65d9
Zachary Lentz <zlentz@slac.stanford.edu>
2021-03-11 10:32:03 -0800

    TST: add test for interface converter

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,12 +1,19 @@
+from ophyd.device import Component as Cpt
+from ophyd.device import Device
 from ophyd.signal import Signal
 
 from pcdsdevices.component import InterfaceComponent as ICpt
-from pcdsdevices.device import InterfaceDevice
+from pcdsdevices.device import InterfaceDevice, to_interface
 
 
 class TwoSignal(InterfaceDevice):
     sig_one = ICpt(Signal)
     sig_two = ICpt(Signal)
+
+
+class SomeDevice(Device):
+    some = Cpt(Signal)
+    where = Cpt(Signal)
 
 
 def test_interface_basic():
@@ -19,3 +26,20 @@ def test_interface_basic():
 
     assert one.get() == 1
     assert two.get() == 2
+
+
+def test_to_interface():
+    SomeDeviceInterface = to_interface(SomeDevice)
+    some = Signal(name='some')
+    where = Signal(name='where')
+
+    sdi = SomeDeviceInterface('', name='sdi', some=some, where=where)
+    sd = SomeDevice('', name='sd')
+
+    sdi.some.put(1)
+    sdi.where.put(2)
+    sd.some.put(3)
+    sd.where.put(4)
+
+    assert some.get() == 1
+    assert where.get() == 2
