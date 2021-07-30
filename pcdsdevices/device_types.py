ae4ae2541e86c9b7275825d3641e971bcf1852e9
Aalayah I Spencer <spencera@slac.stanford.edu>
2021-07-29 19:42:03 -0700

    added mpodApalis module types to device_types

--- pcdsdevices/device_types.py
+++ pcdsdevices/device_types.py
@@ -40,3 +40,4 @@ from .spectrometer import Kmono, VonHamos4Crystal
 from .timetool import Timetool, TimetoolWithNav
 from .valve import GateValve, Stopper
 from .wfs import WaveFrontSensorTarget
+from .mpodApalis import MPODApalisModule4Channel, MPODApalisModule8Channel, MPODApalisModule16Channel, MPODApalisModule24Channel
