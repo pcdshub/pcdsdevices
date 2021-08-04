b3a4fe967217472a6e2b77464300e917c2fee617
Zachary Lentz <zlentz@slac.stanford.edu>
2021-08-04 16:24:10 -0700

    TST: add test that we have ui files at our entry point

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -3,5 +3,9 @@ from typhos.utils import DISPLAY_PATHS
 from pcdsdevices.ui import path
 
 
-def test_ui_included():
+def test_ui_entry_point():
     assert path in DISPLAY_PATHS
+
+
+def test_ui_files_in_distribution():
+    assert len(list(path.glob('*.ui'))) > 0
