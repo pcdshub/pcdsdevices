87f44f11c804f04131372a7e5042ef485fae647a
Zachary Lentz <ZLLentz@users.noreply.github.com>
2021-08-24 14:17:05 -0700

    MNT: simplify isinstance check of a list
    Co-authored-by: Ken Lauer <klauer@users.noreply.github.com>

--- pcdsdevices/device.py
+++ pcdsdevices/device.py
@@ -390,7 +390,7 @@ class GroupDevice(Device):
         for cpt_name in self.component_names:
             cpt = getattr(self, cpt_name)
             # The following types break without parents
-            if not any(isinstance(cpt, cls) for cls in self.needs_parent):
+            if not isinstance(cpt, self.needs_parent):
                 cpt._parent = None
         if self.stage_group is None:
             self.stage_group = []
