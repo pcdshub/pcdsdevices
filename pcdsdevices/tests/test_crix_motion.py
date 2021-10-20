40109ac5c54be86b0642417465478f02efbaa5c3
Zachary Lentz <zlentz@slac.stanford.edu>
2021-10-20 16:41:36 -0700

    TST: make tests pass

--- pcdsdevices/tests/test_crix_motion.py
+++ pcdsdevices/tests/test_crix_motion.py
@@ -1,3 +1,5 @@
+import functools
+
 import numpy as np
 import pytest
 
@@ -16,19 +18,26 @@ def vls():
     return VLSOpticsSim(name='vls')
 
 
+# Example vals only accurate to 2 decimal places at worst
+# Use a less strict comparison for the accuracy checks
+acc_isclose = functools.partial(np.isclose, atol=0.004)
+
+
 def test_calc_reverses(vls):
     for optic in (vls.mirror, vls.grating):
-        lower_mrad, upper_mrad = optic.limits
+        lower_mrad, upper_mrad = optic.calc.limits
         lower_mm = optic.forward(lower_mrad)
         upper_mm = optic.forward(upper_mrad)
-        assert np.isclose(lower_mrad=optic.inverse(lower_mm))
+        assert np.isclose(lower_mrad, optic.inverse(lower_mm))
         assert np.isclose(upper_mrad, optic.inverse(upper_mm))
 
 
 def test_calc_accuracy(vls):
     def compare(pseudopos, answer):
-        assert np.isclose(pseudopos.forward(answer[1]), answer[0])
-        assert np.isclose(pseudopos.inverse(answer[0]), answer[1])
+        forward_calc = pseudopos.forward(answer[1])[0]
+        assert acc_isclose(forward_calc, answer[0])
+        inverse_calc = pseudopos.inverse(answer[0])[0]
+        assert acc_isclose(inverse_calc, answer[1])
 
     compare(vls.mirror, vls_mirror_lower)
     compare(vls.mirror, vls_mirror_upper)
@@ -38,7 +47,7 @@ def test_calc_accuracy(vls):
 
 def test_move_consistency(vls):
     for optic in (vls.mirror, vls.grating):
-        lower_mrad, upper_mrad = optic.limits
+        lower_mrad, upper_mrad = optic.calc.limits
         optic.move(lower_mrad)
         assert np.isclose(optic.position[0], lower_mrad)
         optic.move(upper_mrad)
@@ -48,11 +57,11 @@ def test_move_consistency(vls):
 def test_move_accuracy(vls):
     def test_real_move(pseudopos, answer):
         pseudopos.real.move(answer[0])
-        assert np.isclose(pseudopos.calc.position, answer[1])
+        assert acc_isclose(pseudopos.calc.position, answer[1])
 
     def test_pseudo_move(pseudopos, answer):
         pseudopos.calc.move(answer[1])
-        assert np.isclose(pseudopos.real.position, answer[0])
+        assert acc_isclose(pseudopos.real.position, answer[0])
 
     def test_moves(pseudopos, answer):
         test_real_move(pseudopos, answer)
