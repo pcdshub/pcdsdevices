c0fb5acb5aacbdd719f31dc18511d3d9abeaf6e2
Vincent Esposito <espov@slac.stanford.edu>
2022-01-19 09:58:57 -0800

    add generic n-axis devices for simple motion systems

--- pcdsdevices/n_axis_device.py
+++ pcdsdevices/n_axis_device.py
@@ -6,11 +6,20 @@ This module contains a generic class to handle a simple n-axis device
 from ophyd import Component as Cpt
 from ophyd import Device
 
-from .epics_motor import BeckhoffAxis
+from .epics_motor import BeckhoffAxis, IMS, Newport, PMC100, PCDSMotorBase, SmarAct
 from .interface import BaseInterface
 
 
 MAX_AXIS = 20 # seems reasonable for now, but can be increased if needed
+motor_types = (('MMS', 'MMS', IMS),
+               ('CLZ', 'CLZ', IMS),
+               ('CLF', 'CLF', IMS),
+               ('Newport', 'MMN', Newport),
+               ('PMC100', 'MZN', PMC100),
+               ('Beckhoff', 'MMS', BeckhoffAxis),
+               ('pico', 'PIC', PCDSMotorBase),
+               ('SmarAct', 'MCS', SmarAct),
+               ('MMC100', 'MMC', PCDSMotorBase))
 
 
 class N_axis(BaseInterface, Device):
@@ -23,30 +32,28 @@ class N_axis(BaseInterface, Device):
             except AttributeError:
                 break
         return
-    
+
 
 def _make_axs_classes(max_axis, name):
     """Generate all possible subclasses for n_axis=1..max_axis"""
     axs_classes = {}
-    for i in range(1, max_axis+1):
-        axs = {}
-        for n in range(1, i+1):
-            comp = Cpt(BeckhoffAxis, ':MMS:{:02d}'.format(n), kind='normal')
-            axs['m{}'.format(n)] = comp
+    for cpt, cpt_pv, _type in motor_types:
+        axs_classes[cpt] = {}
+        for i in range(1, max_axis+1):
+            axs = {}
+            for n in range(1, i+1):
+                comp = Cpt(_type, ':{}:{:02d}'.format(cpt_pv, n), kind='normal')
+                axs['m{}'.format(n)] = comp
         
-        cls_name = '{}{}'.format(name, i)
-        cls = type(cls_name, (N_axis,), axs)
-        cls.num_axis = i
-        axs_classes[i] = cls
+            cls_name = '{}{}'.format(name, i)
+            cls = type(cls_name, (N_axis,), axs)
+            cls.num_axis = i
+            axs_classes[cpt][i] = cls
     return axs_classes
 
 _axs_classes = _make_axs_classes(MAX_AXIS, 'n_axis_')
 
 
-def get_n_axis_device(prefix, n_axis, *args, name, **kwargs):
-    cls = _axs_classes[n_axis]
+def get_n_axis_device(prefix, n_axis, *args, name, motor_type='Beckhoff', **kwargs):
+    cls = _axs_classes[motor_type][n_axis]
     return cls(prefix, name=name, **kwargs)
-
-
-
-
