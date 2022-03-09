7e15a4fad5fd715c3e57daf558d77f5c5967f994
tangkong <tangkongrobert@gmail.com>
2022-03-08 17:12:47 -0800

    TST: ophyd to elog tests, with fake html validation

--- pcdsdevices/tests/test_utils.py
+++ pcdsdevices/tests/test_utils.py
@@ -4,8 +4,12 @@ import threading
 import time
 
 import pytest
+from ophyd import Component as Cpt
+from ophyd import Device
 
 from .. import utils
+from ..device import GroupDevice
+from ..utils import post_ophyds_to_elog
 
 try:
     import pty
@@ -106,3 +110,46 @@ def test_get_status_float():
         dummy_dictionary, 'dict1', 'dict2', 'value', precision=3
     )
     assert res == '23.343'
+
+
+class StatusDevice(Device):
+    """ simulate a device with a status method """
+    def status(self):
+        return self.name
+
+
+class BasicGroup(StatusDevice, GroupDevice):
+    one = Cpt(StatusDevice, ':BASIC')
+    two = Cpt(StatusDevice, ':COMPLEX')
+
+
+class SomeDevice(StatusDevice):
+    some = Cpt(StatusDevice, ':SOME')
+    where = Cpt(StatusDevice, ':WHERE')
+
+
+def test_ophyd_to_elog(elog):
+    # make some devices
+
+    group = BasicGroup('GROUP', name='group')
+    some = SomeDevice('SOME', name='some')
+
+    post_ophyds_to_elog([group, some], hutch_elog=elog)
+    assert len(elog.posts) == 1
+    # count number of content entries
+    assert elog.posts[-1][0][0].count('<pre>') == 2
+
+    post_ophyds_to_elog([group.one, some.some], hutch_elog=elog)
+    assert len(elog.posts) == 1  # no children allowed by default
+
+    post_ophyds_to_elog([[group, some], group.one, some.some],
+                        allow_child=True, hutch_elog=elog)
+    assert len(elog.posts) == 2
+    assert elog.posts[-1][0][0].count('<pre>') == 4
+    # two list levels
+    assert elog.posts[-1][0][0].count("class='parent'") == 2
+
+    # half-hearted html validation
+    for post in elog.posts:
+        for tag in ['pre', 'div', 'button']:
+            assert post[0][0].count('<'+tag) == post[0][0].count('</'+tag)
