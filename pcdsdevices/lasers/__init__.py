7a10662aebd14c3314731edbb26dd1c36ae4aea2
Ken Lauer <klauer@slac.stanford.edu>
2022-03-17 16:57:00 -0700

    MNT: adjust lasers init

--- pcdsdevices/lasers/__init__.py
+++ pcdsdevices/lasers/__init__.py
@@ -1,10 +1,12 @@
-from . import ek9000, elliptec, qmini, thorlabsWFS, tuttifrutti, zoomtelescope
+from . import (btps, ek9000, elliptec, qmini, thorlabsWFS, tuttifrutti,
+               zoomtelescope)
 
 __all__ = [
-    'ek9000',
-    'elliptec',
-    'qmini',
-    'thorlabsWFS',
-    'tuttifrutti',
-    'zoomtelescope',
+    "btps",
+    "ek9000",
+    "elliptec",
+    "qmini",
+    "thorlabsWFS",
+    "tuttifrutti",
+    "zoomtelescope",
 ]
