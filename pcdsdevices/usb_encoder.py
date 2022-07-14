bfd3dc03449507fdb157256fce46121b94eac3d2
Vincent Esposito <espov@slac.stanford.edu>
2022-07-14 13:47:59 -0700

    Add a linked stage and a method to reset both stage and encoder to 0

--- pcdsdevices/usb_encoder.py
+++ pcdsdevices/usb_encoder.py
@@ -10,7 +10,7 @@ from .interface import BaseInterface
 
 class USBEncoder(BaseInterface, Device):
     """
-    Delay generator single channel class
+    USB encoder class
 
     Parameters
     ----------
@@ -27,6 +27,26 @@ class USBEncoder(BaseInterface, Device):
 
     tab_whitelist = ['pos', 'set_zero', 'scale', 'offset']
 
+    def __init__(self, stage=None):
+        self.stage = stage
+
     def set_zero(self):
         """ Resets the encoder counts to 0 """
         self.zero.put(1)
+
+    def set_zero_with_stage(self):
+        """
+        Resets the encoder counts to 0 and sets the stage offset
+        so that its position is also 0.
+        """
+        self.zero.put(1)
+        if (
+            self.stage is not None or
+            hasattr(self.stage, 'set_current_position')
+        ):
+            self.stage.set_current_position(0)
+        else:
+            print('No stage associated with that encoder,')
+            print('or stage is not settable.\n')
+            print('Resetting endoder only.')
+        return
