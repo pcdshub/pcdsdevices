658b104be2ef7cf6219755ff701400135345d1e7
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2022-09-16 14:03:03 -0700

    ENH: add class for HPI6012 6030 readout

---
+++ pcdsdevices/radiation.py
@@ -0,0 +1,19 @@
+"""
+Radiation Sesnsor Classes.
+
+This Module contains all the classes relating to Radiation instrumentation
+"""
+from ophyd import Component as Cpt
+from ophyd import EpicsSignalRO
+from ophyd.device import Device
+
+
+class HPI6012(Device):
+    """class for reading out HPI 6012 data"""
+    dose_rate = Cpt(EpicsSignalRO, ':DOSE_RATE', kind='hinted')
+
+    trip_alarm_a1 = Cpt(EpicsSignalRO, ':TRIP_A1')
+    trip_alarm_a2 = Cpt(EpicsSignalRO, ':TRIP_A2')
+    trip_alarm_a3 = Cpt(EpicsSignalRO, ':TRIP_A3')
+
+    fail_time = Cpt(EpicsSignalRO, ':FAIL_TIME')
