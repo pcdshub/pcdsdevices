f8f19723e72a75c87cd5bfc7b5dd95b4b6df2a45
Nick <nrw@berkeley.edu>
2022-11-28 15:46:18 -0800

    WIP: add light support

--- pcdsdevices/pneumatic.py
+++ pcdsdevices/pneumatic.py
@@ -8,11 +8,13 @@ This Module contains all the classes relating to Pneumatic Actuators
 from ophyd import Component as Cpt
 from ophyd import EpicsSignalRO, EpicsSignal
 from ophyd.device import Device
+from pcdsdevices.interface import LightpathMixin
 
-class BeckhoffPneumatic(Device):
+class BeckhoffPneumatic(LightpathMixin):
 	"""
 	Class containing basic Beckhoff Pneumatic support
 	"""
+	lightpath_cpts = ['limit_switch_in', 'limit_switch_out']
 
 	#readouts
 	limit_switch_in = Cpt(EpicsSignalRO, ':PLC:bInLimitSwitch')
@@ -27,8 +29,8 @@ class BeckhoffPneumatic(Device):
 	retract_ok = Cpt(EpicsSignalRO, 'bretractEnable')
 
 	#commands
-	insert = Cpt(EpicsSignal, 'CMD:IN')
-	retract = Cpt(EpicsSignal, 'CMD:OUT')
+	insert_signal = Cpt(EpicsSignal, 'CMD:IN')
+	retract_signal = Cpt(EpicsSignal, 'CMD:OUT')
 
 	#returns
 	busy = Cpt(EpicsSignalRO, ':bBusy')
@@ -38,6 +40,34 @@ class BeckhoffPneumatic(Device):
 	error_id = Cpt(EpicsSignalRO, ':PLC:nErrorId')
 	error_message = Cpt(EpicsSignalRO, ':PLC:sErrorMessage')
 	position_state = Cpt(EpicsSignalRO, ':nPositionState')
+
+	def insert(self):
+		"""
+		Method for inserting Beckhoff Pneumatic Actuator
+		"""
+		if self.insert_ok:
+			self.insert_signal.put(1)
+		else:
+			pass
+			#throw?
+		
+	def retract(self):
+		if self.retract_ok:
+			self.retract_signal.put(1)
+		else:
+			pass
+			#throw?
+
+	trans = 0.0 if limit_switch_in and not limit_switch_out else 1.0
+
+	def calc_lightpath_state(self, limit_switch_in=None, limit_switch_out=None):
+		status = LightpathState(
+			inserted=limit_switch_in and not limit_switch_out,
+			removed=limit_switch_out and not limit_switch_in,
+			output=trans
+		)
+
+
 
 	
 
