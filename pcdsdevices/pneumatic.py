674388c3f1c50e87bc1c17352901120334fa0060
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2022-11-29 15:17:10 -0800

    STY: update style

--- pcdsdevices/pneumatic.py
+++ pcdsdevices/pneumatic.py
@@ -2,77 +2,70 @@
 """
 Pneumatic Classes.
 
-This Module contains all the classes relating to Pneumatic Actuators 
+This Module contains all the classes relating to Pneumatic Actuators
 """
 
+from lightpath import LightpathState
 from ophyd import Component as Cpt
-from ophyd import EpicsSignalRO, EpicsSignal
-from ophyd.device import Device
+from ophyd import EpicsSignal, EpicsSignalRO
+
 from pcdsdevices.interface import LightpathMixin
 
+
 class BeckhoffPneumatic(LightpathMixin):
-	"""
-	Class containing basic Beckhoff Pneumatic support
-	"""
-	lightpath_cpts = ['limit_switch_in', 'limit_switch_out']
+    """
+    Class containing basic Beckhoff Pneumatic support
+    """
+    lightpath_cpts = ['limit_switch_in', 'limit_switch_out']
 
-	#readouts
-	limit_switch_in = Cpt(EpicsSignalRO, ':PLC:bInLimitSwitch')
-	limit_switch_out = Cpt(EpicsSignalRO, ':PLC:bOutLimitSwitch')
-	
-	retract_status = Cpt(EpicsSignalRO, ':bRetractDigitalOutput')
-	insert_status = Cpt(EpicsSignalRO, ':bInsertDigitalOutput')
-    
-	#logic and supervisory
-	interlock_ok = Cpt(EpicsSignalRO, 'bInterlockOK')
-	insert_ok = Cpt(EpicsSignalRO, 'bInsertEnable')
-	retract_ok = Cpt(EpicsSignalRO, 'bretractEnable')
+    # readouts
+    limit_switch_in = Cpt(EpicsSignalRO, ':PLC:bInLimitSwitch')
+    limit_switch_out = Cpt(EpicsSignalRO, ':PLC:bOutLimitSwitch')
 
-	#commands
-	insert_signal = Cpt(EpicsSignal, 'CMD:IN')
-	retract_signal = Cpt(EpicsSignal, 'CMD:OUT')
+    retract_status = Cpt(EpicsSignalRO, ':bRetractDigitalOutput')
+    insert_status = Cpt(EpicsSignalRO, ':bInsertDigitalOutput')
 
-	#returns
-	busy = Cpt(EpicsSignalRO, ':bBusy')
-	done = Cpt(EpicsSignalRO, ':bDone')
-	reset = Cpt(EpicsSignal, ':bReset')
-	error = Cpt(EpicsSignalRO, ':PLC:bError')
-	error_id = Cpt(EpicsSignalRO, ':PLC:nErrorId')
-	error_message = Cpt(EpicsSignalRO, ':PLC:sErrorMessage')
-	position_state = Cpt(EpicsSignalRO, ':nPositionState')
+    # logic and supervisory
+    interlock_ok = Cpt(EpicsSignalRO, 'bInterlockOK')
+    insert_ok = Cpt(EpicsSignalRO, 'bInsertEnable')
+    retract_ok = Cpt(EpicsSignalRO, 'bretractEnable')
 
-	def insert(self):
-		"""
-		Method for inserting Beckhoff Pneumatic Actuator
-		"""
-		if self.insert_ok:
-			self.insert_signal.put(1)
-		else:
-			pass
-			#throw?
-		
-	def retract(self):
-		if self.retract_ok:
-			self.retract_signal.put(1)
-		else:
-			pass
-			#throw?
+    # commands
+    insert_signal = Cpt(EpicsSignal, 'CMD:IN')
+    retract_signal = Cpt(EpicsSignal, 'CMD:OUT')
 
+    # returns
+    busy = Cpt(EpicsSignalRO, ':bBusy')
+    done = Cpt(EpicsSignalRO, ':bDone')
+    reset = Cpt(EpicsSignal, ':bReset')
+    error = Cpt(EpicsSignalRO, ':PLC:bError')
+    error_id = Cpt(EpicsSignalRO, ':PLC:nErrorId')
+    error_message = Cpt(EpicsSignalRO, ':PLC:sErrorMessage')
+    position_state = Cpt(EpicsSignalRO, ':nPositionState')
 
-	def calc_lightpath_state(self, limit_switch_in=None, limit_switch_out=None):
-		trans = 0.0 if limit_switch_in and not limit_switch_out else 1.0
+    def insert(self):
+        """
+        Method for inserting Beckhoff Pneumatic Actuator
+        """
+        if self.insert_ok:
+            self.insert_signal.put(1)
+        else:
+            pass
+            # throw?
 
-		status = LightpathState(
-			inserted=limit_switch_in and not limit_switch_out,
-			removed=limit_switch_out and not limit_switch_in,
-			output={self.output_branches[0]: trans}
-		)
-		return status
+    def remove(self):
+        if self.retract_ok:
+            self.retract_signal.put(1)
+        else:
+            pass
+            # throw?
 
+    def calc_lightpath_state(self, limit_switch_in=None, limit_switch_out=None):
+        trans = 0.0 if limit_switch_in and not limit_switch_out else 1.0
 
-
-	
-
-
-
-
+        status = LightpathState(
+            inserted=limit_switch_in and not limit_switch_out,
+            removed=limit_switch_out and not limit_switch_in,
+            output={self.output_branches[0]: trans}
+        )
+        return status
