ccbe5b9bc875c303690196a9cbec1bd7239c9f7a
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2022-12-02 16:40:49 -0800

    ENH: remove old code fix some python indentation

--- pcdsdevices/pneumatic.py
+++ pcdsdevices/pneumatic.py
@@ -4,11 +4,10 @@ Pneumatic Classes.
 This Module contains all the classes relating to Pneumatic Actuators
 """
 
-import time
-
 from lightpath import LightpathState
 from ophyd import Component as Cpt
 from ophyd import EpicsSignal, EpicsSignalRO
+from ophyd.status import Status
 
 from pcdsdevices.interface import BaseInterface, LightpathMixin
 
@@ -17,7 +16,6 @@ class BeckhoffPneumatic(BaseInterface, LightpathMixin):
     """
     Class containing basic Beckhoff Pneumatic support
     """
-    time_out = 30
     lightpath_cpts = ['limit_switch_in', 'limit_switch_out']
 
     # readouts
@@ -45,27 +43,32 @@ class BeckhoffPneumatic(BaseInterface, LightpathMixin):
     error_message = Cpt(EpicsSignalRO, ':PLC:sErrorMessage')
     position_state = Cpt(EpicsSignalRO, ':nPositionState', kind='hinted')
 
-    def insert(self, time_out=30):
+    def insert(self, wait: bool = False, timeout: float = 10.0) -> Status:
         """
         Method for inserting Beckhoff Pneumatic Actuator
+        default times out at 10 seconds, setting wait to true
+        will block indefinietly until the PLC marks not busy and
+        done or errors.
         """
+        status = Status(timeout=timeout) if not wait else Status()
+
         if self.insert_ok.get():
             self.insert_signal.put(1)
-            start_time = time.time()
-            time_elapsed = 0
-            while (time_elapsed < time_out):
-                time_elapsed = time.time() - start_time
-                if not self.done.get() and self.busy.get():
-                    continue
-                else:
-                    break
-            if self.error.get():
-                raise RuntimeError(self.error_message.get())
-            if time_elapsed >= time_out:
-                raise RuntimeError("Insertion Failed Timed Out")
-
+            # three ways to leave this loop
+            # 1 true done and not busy
+            # 2 error
+            # 3 timeout
+            while (not self.done.get() and self.busy.get()):
+                if self.error.get():
+                    error = Exception(self.error_message.get())
+                    status.set_exception(error)
+                    return status
+            # if we get here, insertion was successful
+            status.set_finished()
         else:
-            raise RuntimeError("Insertion not permitted by PLC")
+            error = Exception("Insertion not permitted by PLC")
+            status.set_exception(error)
+        return status
 
     def remove(self):
         if self.retract_ok.get():
