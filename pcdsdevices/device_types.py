88852dbc22c50f33740b99c8c1d8b11cc223a3f5
Ken Lauer <klauer@slac.stanford.edu>
2023-01-13 12:17:36 -0800

    MNT: revert bad fixes from automated process

--- pcdsdevices/device_types.py
+++ pcdsdevices/device_types.py
@@ -1,1 +1,48 @@
 # flake8: NOQA
+from .analog_signals import Acromag, AcromagChannel
+from .areadetector.detectors import PCDSAreaDetector
+from .atm import ArrivalTimeMonitor
+from .attenuator import Attenuator
+from .beam_stats import BeamStats
+from .ccm import CCM
+from .dc_devices import ICT
+from .epics_motor import (IMS, PMC100, BeckhoffAxis, DelayNewport, EpicsMotor,
+                          Motor, Newport)
+from .evr import Trigger
+from .gauge import GaugeSet
+from .gon import BaseGon, Goniometer, GonWithDetArm, Kappa, SamPhi, XYZStage
+from .inout import Reflaser, TTReflaser
+from .ipm import IPM, IPM_IPIMB, IPM_Wave8
+from .jet import BeckhoffJet
+from .lasers.ek9000 import El3174AiCh, EnvironmentalMonitor
+from .lasers.elliptec import Ell6, Ell9, EllBase, EllLinear, EllRotation
+from .lasers.qmini import QminiSpectrometer
+from .lasers.rfof import (CycleRfofRx, CycleRfofTx, ItechRfofAll,
+                          ItechRfofErrors, ItechRfofRx, ItechRfofStatus,
+                          ItechRfofTx)
+from .lasers.thorlabsWFS import ThorlabsWfs40
+from .lasers.zoomtelescope import ZoomTelescope
+from .lens import XFLS, Prefocus
+from .lic import LaserInCoupling
+from .light_control import LightControl
+from .lodcm import XCSLODCM, XPPLODCM
+from .mirror import OffsetMirror, PointingMirror
+from .movablestand import MovableStand
+from .mpod import MPOD, MPODChannelHV, MPODChannelLV
+from .mpod_apalis import (MPODApalisModule4Channel, MPODApalisModule8Channel,
+                          MPODApalisModule16Channel, MPODApalisModule24Channel)
+from .mps import MPS
+from .pim import PIM, PPM, XPIM, PIMWithBoth, PIMWithFocus, PIMWithLED
+from .pseudopos import DelayBase, DelayMotor
+from .pulsepicker import PulsePicker
+from .pump import IonPump
+from .ref import ReflaserL2SI
+from .sample_delivery import (HPLC, PCM, CoolerShaker, FlowIntegrator,
+                              GasManifold, Selector)
+from .sensors import RTD, TwinCATThermocouple
+from .sequencer import EventSequencer
+from .slits import Slits
+from .spectrometer import Gen1VonHamos4Crystal, Kmono, VonHamos4Crystal
+from .timetool import Timetool, TimetoolWithNav
+from .valve import GateValve, Stopper
+from .wfs import WaveFrontSensorTarget
