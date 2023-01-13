73573b13b93d592c36843ae1b9e2034aebd3469c
Ken Lauer <klauer@slac.stanford.edu>
2023-01-13 09:33:54 -0800

    FIX: support git archival in version handling

--- pcdsdevices/version.py
+++ pcdsdevices/version.py
@@ -2,7 +2,7 @@
 Experimental version handling.
 1. Pick the version for use in __init__.py and cli.py without polluting the namespace.
 2. Defer evaluation of the version until it is checked to save 0.3s on import
-3. Use the git version in a git checkout and _version otherwise.
+3. Use the git version in a git checkout or archive and _version otherwise.
 """
 from collections import UserString
 from pathlib import Path
@@ -15,9 +15,10 @@ class VersionProxy(UserString):
 
     def _get_version(self) -> Optional[str]:
         # Checking for directory is faster than failing out of get_version
-        if (Path(__file__).parent.parent / '.git').exists():
+        repo_root = Path(__file__).resolve().parent.parent
+        if (repo_root / ".git").exists() or (repo_root / ".git_archival.txt").exists():
             try:
-                # Git checkout
+                # Git checkout or git archive
                 from setuptools_scm import get_version
                 return get_version(root="..", relative_to=__file__)
             except (ImportError, LookupError):
