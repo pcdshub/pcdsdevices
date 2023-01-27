25726daa1d5f6811ca9de3742e40d6db35fb4e84
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2023-01-27 13:57:46 -0800

    TST: simple status completion check

--- pcdsdevices/tests/test_pneumatic.py
+++ pcdsdevices/tests/test_pneumatic.py
@@ -10,20 +10,18 @@ def fake_stopper():
     stopper = FakeStopper('TST:ST1', name="Test Stopper")
     stopper.insert_ok.sim_put(1)
     stopper.retract_ok.sim_put(1)
+    stopper.done.sim_put(1)
+    stopper.error.sim_put(0)
     return stopper
 
 
 def test_stopper_inserted(fake_stopper):
-    status = fake_stopper.insert()
+    status = fake_stopper.insert(wait=True)
     assert status.success
     assert status.done
-    assert fake_stopper.limit_switch_in
-    assert not fake_stopper.limit_switch_out
 
 
 def test_stopper_removed(fake_stopper):
-    status = fake_stopper.remove()
+    status = fake_stopper.remove(wait=True)
     assert status.success
     assert status.done
-    assert fake_stopper.limit_switch_out
-    assert not fake_stopper.limit_switch_in
