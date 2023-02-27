f90999c54019cb173f64b7a54027b8582ef6b61e
Ken Lauer <klauer@slac.stanford.edu>
2023-02-27 15:44:54 -0800

    DOC: pre-release notes and guiding comments

---
+++ docs/source/upcoming_release_notes/1113-btps_ld1.rst
@@ -0,0 +1,31 @@
+1113 btps_ld1
+#############
+
+API Changes
+-----------
+- N/A
+
+Features
+--------
+- N/A
+
+Device Updates
+--------------
+- Added laser destination 1 (LD1), where a diagnostics box is installed, to
+  the Laser Beam Transport System (BTPS) state configuration.
+
+New Devices
+-----------
+- N/A
+
+Bugfixes
+--------
+- N/A
+
+Maintenance
+-----------
+- Updated ``BtpsState`` comments and logic to help guide future port additions.
+
+Contributors
+------------
+- klauer
--- pcdsdevices/lasers/btms_config.py
+++ pcdsdevices/lasers/btms_config.py
@@ -255,7 +255,9 @@ class DestinationPosition(str, enum.Enum):
         -------
         str
         """
+        # NOTE: Add new descriptions here.
         return {
+            DestinationPosition.ld1: "Diagnostics",
             DestinationPosition.ld2: "TMO IP3",
             DestinationPosition.ld4: "RIX ChemRIXS",
             DestinationPosition.ld6: "RIX QRIXS",
@@ -324,6 +326,9 @@ AnyPosition = Union[SourcePosition, DestinationPosition]
 
 PORT_SPACING_MM = 215.9  # 8.5 in
 
+# NOTE: This is the primary location where valid ports are listed.
+#       Modify this section if you want the BTPS/BTMS to know about more ports.
+#
 # PV source index (bay) to installed LS port
 valid_sources: tuple[SourcePosition, ...] = (
     SourcePosition.ls1,  # Bay 1
