e158404b578e1904dff4213e7529171b7f381d1c
Basil Aljamal <122060486+baljamal@users.noreply.github.com>
2023-04-12 09:19:34 -0700

    update sqrt-1 class

--- pcdsdevices/sqr1.py
+++ pcdsdevices/sqr1.py
@@ -1,5 +1,6 @@
 """
-Ophyd class for square-one tri-sphere motion system (https://www.sqr-1.com/tsp.html)
+    Ophyd class for square-one tri-sphere motion system
+    (https://www.sqr-1.com/tsp.html)
 """
 
 import logging
@@ -13,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 
 class SQR1Axis(PVPositionerIsClose):
-    setpoint = FCpt(EpicsSignal, '{prefix}:TARGET:{_axis}', kind='normal')
+    setpoint = FCpt(EpicsSignal, '{self.prefix}:TARGET:{_axis}', kind='normal')
     readback = FCpt(EpicsSignal, '{self.prefix}:TARGET:{self._axis}:RBV', kind='hinted')
     actuate = FCpt(EpicsSignal, '{self.prefix}:MOV', kind='normal')
     actuate_value = 1
@@ -24,20 +25,50 @@ class SQR1Axis(PVPositionerIsClose):
     hlm = FCpt(EpicsSignalRO, '{self.prefix}:TARGET:{self._axis}:HLM', kind='normal')
 
     def __init__(self, prefix, axis, **kwargs):
-        self._axis = axis  # axis values {X,Y,Z,rX, rY, rZ}
+        self._axis = axis  # axis values {X, Y, Z, rX, rY, rZ}
         super().__init__(prefix, **kwargs)
 
-        self.setpoint.put(self.readback.get())  # sync position setpoint with readback
-        self._limits = ((self.llm.get(), self.hlm.get()))
+    def move(self, position, wait=True, timeout=None, moved_cb=None):
+        return super().move(position, wait, timeout, moved_cb)
+
+    def check_value(self, pos):
+        self._limits = (((self.llm.get(), self.hlm.get())))
+        super().check_value(pos)
 
 
 class SQR1(Device):
-    def __init__(self, prefix, name) -> None:
-        self.prefix = prefix
-        self.name = name
+    move = FCpt(EpicsSignal, '{self.prefix}:MOV', kind='normal')
+
+    def __init__(self, prefix, name, **kwargs) -> None:
+        super().__init__(prefix, name=name, **kwargs)
         self.x = SQR1Axis(axis="X", prefix=prefix, name=f"{name}_x", rtol=0.01)
         self.y = SQR1Axis(axis="Y", prefix=prefix, name=f"{name}_y", rtol=0.01)
         self.z = SQR1Axis(axis="Z", prefix=prefix, name=f"{name}_z", rtol=0.01)
         self.rx = SQR1Axis(axis="rX", prefix=prefix, name=f"{name}_rx", rtol=0.01)
         self.ry = SQR1Axis(axis="rY", prefix=prefix, name=f"{name}_ry", rtol=0.01)
         self.rz = SQR1Axis(axis="rZ", prefix=prefix, name=f"{name}_rz", rtol=0.01)
+
+    def sync_setpoints(self):
+        self.x.setpoint.put(self.x.readback.get())
+        self.y.setpoint.put(self.y.readback.get())
+        self.z.setpoint.put(self.z.readback.get())
+        self.rx.setpoint.put(self.rx.readback.get())
+        self.ry.setpoint.put(self.ry.readback.get())
+        self.rz.setpoint.put(self.rz.readback.get())
+
+    def multi_axis_move(self, x_sp=None, y_sp=None, z_sp=None, rx_sp=None, ry_sp=None, rz_sp=None):
+        x_sp = x_sp or self.x.readback.get()
+        y_sp = y_sp or self.y.readback.get()
+        z_sp = z_sp or self.z.readback.get()
+        rx_sp = rx_sp or self.rx.readback.get()
+        ry_sp = ry_sp or self.ry.readback.get()
+        rz_sp = rz_sp or self.rz.readback.get()
+
+        self.x.setpoint.put(x_sp)
+        self.y.setpoint.put(y_sp)
+        self.z.setpoint.put(z_sp)
+        self.rx.setpoint.put(rx_sp)
+        self.ry.setpoint.put(ry_sp)
+        self.rz.setpoint.put(rz_sp)
+
+        self.move.put(1)
