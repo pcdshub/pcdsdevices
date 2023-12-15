b4c44e6025b2f81c3f2c69d39fa8f63b3d2bbdf7
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2023-12-15 11:27:28 -0800

    BUG: missing a colon

--- pcdsdevices/digital_signals.py
+++ pcdsdevices/digital_signals.py
@@ -9,7 +9,7 @@ class J120K(BaseInterface, Device):
     """
     A class representing the J120K 24V dry contact cooling flow switch.
     """
-    flow_ok = Cpt(PytmcSignal, 'FSW:FLOW_OK', io='i',
+    flow_ok = Cpt(PytmcSignal, ':FSW:FLOW_OK', io='i',
                   kind='normal', doc='flow rate nominal')
 
     @property
