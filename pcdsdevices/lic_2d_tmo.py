1447601a24947ecf2ebbef7bd60cc2d6f8ae5a1d
tongju12 <tongju@slac.sanford.edu>
2024-03-29 14:05:05 -0700

    building temp gui for li2k4

--- pcdsdevices/lic_2d_tmo.py
+++ pcdsdevices/lic_2d_tmo.py
@@ -5,20 +5,8 @@ from lightpath import LightpathState
 from ophyd.device import Component as Cpt
 
 from .device import GroupDevice
-from .device import UpdateComponent as UpCpt
-from .epics_motor import BeckhoffAxis
+from .epics_motor import BeckhoffAxisEPS
 from .interface import BaseInterface, LightpathMixin
-from .pmps import TwinCATStatePMPS
-
-
-class LaserCouplingTwoD(TwinCATStatePMPS):
-    """
-
-    Two Dimention laser coupling 2D States Setup
-
-    Here, we specify 2 states, which we can support in an EPICS enum and 2 motors for X and Y Axes.
-    """
-    config = UpCpt(state_count=2, motor_count=2)
 
 
 class TMOLaserCouplingTwoDimention(BaseInterface, GroupDevice, LightpathMixin):
@@ -40,9 +28,8 @@ class TMOLaserCouplingTwoDimention(BaseInterface, GroupDevice, LightpathMixin):
     tab_component_names = True
 
     # LaserCoupling LI2K4 x and Y
-    laser_coupling = Cpt(LaserCouplingTwoD, 'LI2K4:IP1', add_prefix=(), kind='normal')
-    li2k4_x = Cpt(BeckhoffAxis, ':MMS:X', doc="X-axis of lasercoupling li2k4", kind='normal')
-    li2k4_y = Cpt(BeckhoffAxis, ':MMS:Y', doc="Y-axis of lasercoupling li2k4", kind='normal')
+    li2k4_x = Cpt(BeckhoffAxisEPS, ':MMS:X', doc="X-axis of lasercoupling li2k4", kind='normal')
+    li2k4_y = Cpt(BeckhoffAxisEPS, ':MMS:Y', doc="Y-axis of lasercoupling li2k4", kind='normal')
     removed = False
     transmission = 1
     SUB_STATE = 'state'
