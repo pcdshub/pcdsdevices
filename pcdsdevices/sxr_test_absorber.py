a4467e92e3936dcf4dc243dcc10a85e7b60b5a2b
tongju12 <tongju@slac.sanford.edu>
2024-06-19 14:01:24 -0700

    removing automode lines

--- pcdsdevices/sxr_test_absorber.py
+++ pcdsdevices/sxr_test_absorber.py
@@ -3,8 +3,6 @@ Module for the SXR Test Absorbers.
 """
 from __future__ import annotations
 
-from typing import Callable
-
 from ophyd.device import Component as Cpt
 
 from .digital_signals import J120K
@@ -27,21 +25,6 @@ class SxrTestAbsorberStates(TwinCATInOutPositioner):
     """
 
     st3k4_auto = Cpt(PytmcSignal, ':ST3K4_AUTO', io='io', kind='config')
-
-    def set(
-        self,
-        position: str | int,
-        moved_cb: Callable | None = None,
-        timeout: float | None = None,
-    ):
-
-        """
-        if self.st3k4_auto.get():
-           raise ST3K4AutoError(
-               "ST1K4 must follow ST3K4. Move rejected."
-           )
-        """
-        return super().set(position, moved_cb=moved_cb, timeout=timeout)
 
 
 class SxrTestAbsorber(BaseInterface, LightpathInOutCptMixin):
