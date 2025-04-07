885796a7e5bea9c982597aab589775653c3a484e
Jing Yin <jyin@slac.stanford.edu>
2025-04-07 13:16:32 -0700

    modify dream_motion.py

--- pcdsdevices/dream_motion.py
+++ pcdsdevices/dream_motion.py
@@ -11,16 +11,16 @@ from .epics_motor import BeckhoffAxis
 from .interface import BaseInterface
 
 
-class DREAM(BaseInterface, GroupDevice):
+class DREAM_MC_Y(BaseInterface, GroupDevice):
     """
-    DREAM Motion Class
-    This class controls motors fixed to the DREAM Motion system for the
+    DREAM Main Chamber Motion Class
+    This class controls main chamber vertical Motion system for the
     DREAM endstation in TMO.
 
     Parameters
     ----------
     prefix : str
-        Base PV for the DREAM motion system
+        DREAM:MC
     name : str, keyword-only
         Alias for the device
     """
@@ -29,11 +29,70 @@ class DREAM(BaseInterface, GroupDevice):
     tab_component_names = True
 
     # Motor components
-    chamber_y = Cpt(BeckhoffAxis, ':MC:MMS:Y', kind='normal')
-    coil_roll = Cpt(BeckhoffAxis, ':COIL:MMS:ROLL', kind='normal')
-    coil_yaw = Cpt(BeckhoffAxis, ':COIL:MMS:YAW', kind='normal')
-    gas_jet_x = Cpt(BeckhoffAxis, ':GSJP:MMS:X', kind='normal')
-    gas_jet_z = Cpt(BeckhoffAxis, ':GSJP:MMS:Z', kind='normal')
-    gas_nozzle_x = Cpt(BeckhoffAxis, ':GSJN:MMS:X', kind='normal')
-    gas_nozzle_y = Cpt(BeckhoffAxis, ':GSJN:MMS:Y', kind='normal')
-    gas_nozzle_z = Cpt(BeckhoffAxis, ':GSJN:MMS:Z', kind='normal')
+    chamber_y = Cpt(BeckhoffAxis, ':MMS:Y', kind='normal')
+
+class DREAM_CoilMover(BaseInterface, GroupDevice):
+    """
+    DREAM Motion Class
+    This class controls coil motors fixed to the DREAM Motion system for the
+    DREAM endstation in TMO.
+
+    Parameters
+    ----------
+    prefix : str
+        DREAM:COIL
+    name : str, keyword-only
+        Alias for the device
+    """
+    # UI representation
+    _icon = 'fa.minus-square'
+    tab_component_names = True
+
+    # Motor components
+    coil_roll = Cpt(BeckhoffAxis, ':MMS:ROLL', kind='normal')
+    coil_yaw = Cpt(BeckhoffAxis, ':MMS:YAW', kind='normal')
+  
+class DREAM_GasJet(BaseInterface, GroupDevice):
+    """
+    DREAM Motion Class
+    This class controls gas jet X and Y motors fixed to the DREAM Motion system for the
+    DREAM endstation in TMO.
+
+    Parameters
+    ----------
+    prefix : str
+        DREAM:GSJP
+    name : str, keyword-only
+        Alias for the device
+    """
+    # UI representation
+    _icon = 'fa.minus-square'
+    tab_component_names = True
+
+    # Motor components
+    gas_jet_x = Cpt(BeckhoffAxis, ':MMS:X', kind='normal')
+    gas_jet_z = Cpt(BeckhoffAxis, ':MMS:Z', kind='normal')
+
+class DREAM_GasNozzle(BaseInterface, GroupDevice):
+    """
+    DREAM Motion Class
+    This class controls gas nozzle X,Y and Z motors fixed to the DREAM Motion system for the
+    DREAM endstation in TMO.
+
+    Parameters
+    ----------
+    prefix : str
+        DREAM:GSJN
+    name : str, keyword-only
+        Alias for the device
+    """
+    # UI representation
+    _icon = 'fa.minus-square'
+    tab_component_names = True
+
+    # Motor components
+    gas_nozzle_x = Cpt(BeckhoffAxis, ':MMS:X', kind='normal')
+    gas_nozzle_y = Cpt(BeckhoffAxis, ':MMS:Y', kind='normal')
+    gas_nozzle_z = Cpt(BeckhoffAxis, ':MMS:Z', kind='normal')
+
+
