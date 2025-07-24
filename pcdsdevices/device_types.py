10a69fb6cceb9e27fada3728db65eceef5922034
carogee <147095356+carogee@users.noreply.github.com>
2025-07-24 09:58:23 -0700

    Update pcdsdevices/device_types.py
    Co-authored-by: Robert Tang-Kong <35379409+tangkong@users.noreply.github.com>

--- pcdsdevices/device_types.py
+++ pcdsdevices/device_types.py
@@ -5,8 +5,8 @@ from .atm import ArrivalTimeMonitor
 from .attenuator import Attenuator
 from .beam_stats import BeamStats
 from .ccm import CCM
-from .dccm import DCCM
 from .dc_devices import ICT
+from .dccm import DCCM
 from .epics_motor import (IMS, PMC100, BeckhoffAxis, DelayNewport, EpicsMotor,
                           Motor, Newport)
 from .evr import Trigger
