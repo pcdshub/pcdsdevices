Fixing gui energy updates, renaming crystal vars, numerical checks

pcdsdevices/dccm.py
@@ -2,12 +2,13 @@ import logging
 import time
 import typing
 from collections import namedtuple
+from enum import Enum
 from typing import Optional
 
 import numpy as np
 from ophyd.device import Component as Cpt
 from ophyd.device import FormattedComponent as FCpt
-from ophyd.signal import AttributeSignal
+from ophyd.signal import AttributeSignal, InternalSignal
 
 from .beam_stats import BeamEnergyRequest
 from .device import GroupDevice
@@ -15,15 +16,16 @@ from .device import UpdateComponent as UpCpt
 from .epics_motor import BeckhoffAxis
 from .interface import BaseInterface, FltMvInterface
 from .pmps import TwinCATStatePMPS
-from .pseudopos import PseudoPositioner, PseudoSingleInterface
+from .pseudopos import (PseudoPositioner, PseudoSingleInterface,
+                        pseudo_position_argument, real_position_argument)
 from .variety import set_metadata
 
 logger = logging.getLogger(__name__)
 
-crystal_to_dspacing = {
-    'Si111' : 3.1356011499587773,
-    'Si333' : 1.0452003825497919,
-}
+
+class CrystalIndex(float, Enum):
+    Si111 = 3.1356011499587773
+    Si333 = 1.0452003825497919
 
 
 class DCCMEnergy(FltMvInterface, PseudoPositioner):
@@ -40,6 +42,32 @@ class DCCMEnergy(FltMvInterface, PseudoPositioner):
     prefix : str
         The PV prefix of the DCCM motor, e.g. XPP:MON:MPZ:07A
     """
+
+    _extra_sig_md = {
+        'precision': 3,
+        'units': 'mrad',
+    }
+    high_limit_travel = Cpt(
+        InternalSignal,
+        metadata=_extra_sig_md,
+        kind='omitted',
+    )
+    low_limit_travel = Cpt(
+        InternalSignal,
+        metadata=_extra_sig_md,
+        kind='omitted',
+    )
+
+    def __init__(self, prefix: str, *, name: str, **kwargs):
+        super().__init__(prefix, name=name, **kwargs)
+        self.low_limit_travel.put(self.energy.low_limit, internal=True)
+        self.high_limit_travel.put(self.energy.high_limit, internal=True)
+        self.energy.subscribe(self.update_readback, event_type=self.energy.SUB_READBACK)
+
+    def update_readback(self, *args, **kwargs):
+        rb = self.energy.readback
+        rb._run_subs(sub_type=rb.SUB_VALUE, old_value=rb._readback, value=rb.get(), timestamp=time.time())
+
     # Pseudo motor and real motor
     energy = Cpt(
         PseudoSingleInterface,
@@ -55,47 +83,51 @@ class DCCMEnergy(FltMvInterface, PseudoPositioner):
 
     th1 = Cpt(BeckhoffAxis, ":MMS:TH1", doc="Bragg Upstream/TH1 Axis", kind="normal", name='th1')
     th2 = Cpt(BeckhoffAxis, ":MMS:TH2", doc="Bragg Upstream/TH2 Axis", kind="normal", name='th2')
+    # th1 = Cpt(BeckhoffAxis, ":mtr1", doc="Bragg Upstream/TH1 Axis", kind="normal", name='th1')
+    # th2 = Cpt(BeckhoffAxis, ":mtr2", doc="Bragg Upstream/TH2 Axis", kind="normal", name='th2')
 
-    _crystal = 'Si111'
-    crystal = Cpt(AttributeSignal, attr='_crystal', kind='omitted', write_access=False)
+    _crystal_index = CrystalIndex.Si111
+    crystal_index = Cpt(AttributeSignal, attr='_crystal_index', kind='omitted', write_access=False)
 
-    def update_crystal(self, crystal):
-        if crystal not in crystal_to_dspacing.keys():
+    @property
+    def _crystal_index_name(self):
+        return self._crystal_index.name
+    crystal_index_name = Cpt(AttributeSignal, attr='_crystal_index_name', kind='omitted', write_access=False)
+
+    def update_crystal_index(self, crystal_index):
+        if not isinstance(crystal_index, CrystalIndex):
             return
-        self.crystal._metadata.update(write_access=True)
-        self.crystal.put(crystal)
-        self.crystal._metadata.update(write_access=False)
+        self.crystal_index._metadata.update(write_access=True)
+        self.crystal_index.put(crystal_index)
+        self.crystal_index._metadata.update(write_access=False)
         old_value = self.energy.readback.get()
         self._my_move = True
         self._update_position()
         self.energy.readback._run_subs(sub_type=self.energy.readback.SUB_VALUE, old_value=old_value, value=self.energy.readback.get(), timestamp=time.time())
+        cin = self.crystal_index_name
+        cin._run_subs(sub_type=cin.SUB_VALUE, old_value=cin._readback, value=cin.get(), timestamp=time.time())
 
-    def set_Si111(self):
-        self.update_crystal('Si111')
-
-    def set_Si333(self):
-        self.update_crystal('Si333')
-
-    switch_crystal = Cpt(AttributeSignal, attr='_switch_crystal')
-    set_metadata(switch_crystal, dict(variety='command', value=0))
+    switch_crystal_index = Cpt(AttributeSignal, attr='_switch_crystal_index')
+    set_metadata(switch_crystal_index, dict(variety='command', value=0))
 
     @property
-    def _switch_crystal(self):
+    def _switch_crystal_index(self):
         return 0
 
-    @_switch_crystal.setter
-    def _switch_crystal(self, value):
-        if self._crystal == 'Si111':
-            self.set_Si333()
-        elif self._crystal == 'Si333':
-            self.set_Si111()
+    @_switch_crystal_index.setter
+    def _switch_crystal_index(self, value):
+        if self._crystal_index is CrystalIndex.Si111:
+            self.update_crystal_index(CrystalIndex.Si333)
+        elif self._crystal_index is CrystalIndex.Si333:
+            self.update_crystal_index(CrystalIndex.Si111)
 
     @property
     def dspacing(self):
-        return crystal_to_dspacing[self._crystal]
+        return self._crystal_index.value
 
     tab_component_names = True
 
+    @pseudo_position_argument
     def forward(self, pseudo_pos: namedtuple) -> namedtuple:
         """
         PseudoPositioner interface function for calculating the setpoint.
@@ -106,6 +138,7 @@ class DCCMEnergy(FltMvInterface, PseudoPositioner):
         theta = self.energyToBraggAngle(energy)
         return self.RealPosition(th1=theta, th2=theta)
 
+    @real_position_argument
     def inverse(self, real_pos: namedtuple) -> namedtuple:
         """
         PseudoPositioner interface function for calculating the readback.
@@ -114,7 +147,10 @@ class DCCMEnergy(FltMvInterface, PseudoPositioner):
         """
         real_pos = self.RealPosition(*real_pos)
         theta = real_pos.th1
-        energy = self.thetaToEnergy(theta)
+        if theta < 0.1:
+            energy = float('NaN')
+        else:
+            energy = self.thetaToEnergy(theta)
         return self.PseudoPosition(energy=energy)
 
     def energyToBraggAngle(self, energy: float) -> float:
@@ -132,7 +168,7 @@ class DCCMEnergy(FltMvInterface, PseudoPositioner):
             The angle in degrees
         """
         energy = energy * 1000
-        bragg_angle = np.rad2deg(np.arcsin(12398.419/energy/(2*self.dspacing)))
+        bragg_angle = np.rad2deg(np.arcsin(np.float64(12398.419)/energy/(2*self.dspacing)))
         return bragg_angle
 
     def thetaToEnergy(self, theta):
@@ -149,7 +185,7 @@ class DCCMEnergy(FltMvInterface, PseudoPositioner):
         energy: float
              The photon energy (color) in keV.
         """
-        energy = 12398.419/(2*self.dspacing*np.sin(np.deg2rad(theta)))
+        energy = 12398.419/(2*self.dspacing*np.sin(np.deg2rad(np.float64(theta))))
         return energy/1000
 
 
@@ -202,6 +238,7 @@ class DCCMEnergyWithVernier(DCCMEnergy):
             self.hutch = 'TST'
         super().__init__(prefix, **kwargs)
 
+    @pseudo_position_argument
     def forward(self, pseudo_pos: namedtuple) -> namedtuple:
         """
         PseudoPositioner interface function for calculating the setpoint.
@@ -212,16 +249,6 @@ class DCCMEnergyWithVernier(DCCMEnergy):
         theta = self.energyToBraggAngle(energy)
         vernier = energy * 1000
         return self.RealPosition(th1=theta, th2=theta, acr_energy=vernier)
-
-    def inverse(self, real_pos: namedtuple) -> namedtuple:
-        """
-        PseudoPositioner interface function for calculating the readback.
-        Converts the real position of the DCCM theta motor to the calculated energy.
-        """
-        real_pos = self.RealPosition(*real_pos)
-        theta = real_pos.th1
-        energy = self.thetaToEnergy(theta)
-        return self.PseudoPosition(energy=energy)
 
 
 class DCCMEnergyWithACRStatus(DCCMEnergyWithVernier):