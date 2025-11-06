cb0e96e71936bd308a6ee91377bb2d73d59c230e
carogee <147095356+carogee@users.noreply.github.com>
2025-11-06 09:54:21 -0800

    Update lens.py

--- pcdsdevices/lens.py
+++ pcdsdevices/lens.py
@@ -77,6 +77,7 @@ class Prefocus(CombinedInOutRecordPositioner, LightpathInOutMixin):
                                                else 0))
         super().__init__(prefix, name=name, **kwargs)
 
+      
         # motor aliases                                                                                                 
         self.x = self.x_motor
         self.y = self.y_motor
