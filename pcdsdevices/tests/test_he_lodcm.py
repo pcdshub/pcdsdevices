4f777a6339d5c72ca1e790a6d4a75be39c892f81
James Cotter <jcotter@psbuild-rocky9-01.pcdsn>
2026-07-09 13:57:34 -0700

    ENH: Updated tests with values verified with hutch scientist, modified to account for he_lodcm now using grating beam splitter period

--- pcdsdevices/tests/test_he_lodcm.py
+++ pcdsdevices/tests/test_he_lodcm.py
@@ -12,6 +12,12 @@ def fake_he_lodcmenergy():
     return cls('TST:LODCM', name='fake_lodcm_energy')
 
 
+@pytest.fixture(scope='function')
+def target_states():
+    states = ['Unknown', 'OUT', 'TARGET1a', 'TARGET1b']
+    return states
+
+
 def test_real_positioners(fake_he_lodcmenergy):
     energy = fake_he_lodcmenergy
     assert energy.t1ty not in energy._real
@@ -68,7 +74,7 @@ def test_crystal_invalid_t1(fake_he_lodcmenergy):
         energy.get_crystal()
 
 
-def test_forward_si111(fake_he_lodcmenergy):
+def test_forward_si111(fake_he_lodcmenergy, target_states):
     energy = fake_he_lodcmenergy
     motor_setup(energy.t1ty)
     motor_setup(energy.t2ty1)
@@ -76,13 +82,17 @@ def test_forward_si111(fake_he_lodcmenergy):
     energy.t1ty.user_readback.sim_put(5)
     energy.t2ty1.user_readback.sim_put(5)
     energy.t2ty2.user_readback.sim_put(5)
+    energy.beam_splitter.target._late_state_init(enum_strs=target_states)
+    energy.beam_splitter.target.state.sim_put(2)
+    energy.beam_splitter.target.config.state03.state_name.sim_put('TARGET1a')
+    energy.beam_splitter.target.config.state03.setpoint.sim_put(33.105)
     result = energy.forward(10)
-    assert np.isclose(result.t1ry, 11.402710589021279, rtol=1e-6)
-    assert np.isclose(result.t2ry, 11.402710589021279, rtol=1e-6)
-    assert np.isclose(result.t2tz, 1424.5873603527527, rtol=1e-6)
+    assert np.isclose(result.t1ry, 11.391782080118041, rtol=1e-6)
+    assert np.isclose(result.t2ry, 11.391782080118041, rtol=1e-6)
+    assert np.isclose(result.t2tz, 1408.3672341113784, rtol=1e-6)
 
 
-def test_forward_si220(fake_he_lodcmenergy):
+def test_forward_si220(fake_he_lodcmenergy, target_states):
     energy = fake_he_lodcmenergy
     motor_setup(energy.t1ty)
     motor_setup(energy.t2ty1)
@@ -90,13 +100,17 @@ def test_forward_si220(fake_he_lodcmenergy):
     energy.t1ty.user_readback.sim_put(-5)
     energy.t2ty1.user_readback.sim_put(-5)
     energy.t2ty2.user_readback.sim_put(-5)
+    energy.beam_splitter.target._late_state_init(enum_strs=target_states)
+    energy.beam_splitter.target.state.sim_put(2)
+    energy.beam_splitter.target.config.state03.state_name.sim_put('TARGET1a')
+    energy.beam_splitter.target.config.state03.setpoint.sim_put(33.105)
     result = energy.forward(10)
-    assert np.isclose(result.t1ry, 18.835233562795782, rtol=1e-6)
-    assert np.isclose(result.t2ry, 18.835233562795782, rtol=1e-6)
-    assert np.isclose(result.t2tz, 775.8416048950378, rtol=1e-6)
+    assert np.isclose(result.t1ry, 18.82430505389254, rtol=1e-6)
+    assert np.isclose(result.t2ry, 18.82430505389254, rtol=1e-6)
+    assert np.isclose(result.t2tz, 765.1788462217432, rtol=1e-6)
 
 
-def test_inverse_si111(fake_he_lodcmenergy):
+def test_inverse_si111(fake_he_lodcmenergy, target_states):
     energy = fake_he_lodcmenergy
     motor_setup(energy.t1ty)
     motor_setup(energy.t2ty1)
@@ -106,11 +120,15 @@ def test_inverse_si111(fake_he_lodcmenergy):
     energy.t2ty1.user_readback.sim_put(5)
     energy.t2ty2.user_readback.sim_put(5)
     energy.t1ry.user_readback.sim_put(10)
+    energy.beam_splitter.target._late_state_init(enum_strs=target_states)
+    energy.beam_splitter.target.state.sim_put(2)
+    energy.beam_splitter.target.config.state03.state_name.sim_put('TARGET1a')
+    energy.beam_splitter.target.config.state03.setpoint.sim_put(33.105)
     result = energy.inverse(energy.RealPosition(t1ry=10, t2ry=10, t2tz=0))
-    assert np.isclose(result.energy, 11.385303213205994, rtol=1e-6)
+    assert np.isclose(result.energy, 11.374486057438158, rtol=1e-6)
 
 
-def test_inverse_si220(fake_he_lodcmenergy):
+def test_inverse_si220(fake_he_lodcmenergy, target_states):
     energy = fake_he_lodcmenergy
     motor_setup(energy.t1ty)
     motor_setup(energy.t2ty1)
@@ -120,11 +138,15 @@ def test_inverse_si220(fake_he_lodcmenergy):
     energy.t2ty1.user_readback.sim_put(-5)
     energy.t2ty2.user_readback.sim_put(-5)
     energy.t1ry.user_readback.sim_put(10)
+    energy.beam_splitter.target._late_state_init(enum_strs=target_states)
+    energy.beam_splitter.target.state.sim_put(2)
+    energy.beam_splitter.target.config.state03.state_name.sim_put('TARGET1a')
+    energy.beam_splitter.target.config.state03.setpoint.sim_put(33.105)
     result = energy.inverse(energy.RealPosition(t1ry=10, t2ry=10, t2tz=0))
-    assert np.isclose(result.energy, 18.592061988962758, rtol=1e-6)
+    assert np.isclose(result.energy, 18.581244771168045, rtol=1e-6)
 
 
-def test_roundtrip(fake_he_lodcmenergy):
+def test_roundtrip(fake_he_lodcmenergy, target_states):
     energy = fake_he_lodcmenergy
     motor_setup(energy.t1ty)
     motor_setup(energy.t2ty1)
@@ -132,6 +154,10 @@ def test_roundtrip(fake_he_lodcmenergy):
     energy.t1ty.user_readback.sim_put(5)
     energy.t2ty1.user_readback.sim_put(5)
     energy.t2ty2.user_readback.sim_put(5)
+    energy.beam_splitter.target._late_state_init(enum_strs=target_states)
+    energy.beam_splitter.target.state.sim_put(2)
+    energy.beam_splitter.target.config.state03.state_name.sim_put('TARGET1a')
+    energy.beam_splitter.target.config.state03.setpoint.sim_put(33.105)
     target_energy = 10
     real_pos = energy.forward(target_energy)
     result = energy.inverse(real_pos)
