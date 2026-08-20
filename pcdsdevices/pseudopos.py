08a39a4dbd090602e12de35624ca70af2689fc5b
Jane Liu <janeliu@slac.stanford.edu>
2026-08-19 17:28:21 -0700

    Updated documentation in pseudopos.py

--- pcdsdevices/pseudopos.py
+++ pcdsdevices/pseudopos.py
@@ -223,8 +223,8 @@ class PseudoPositioner(ophyd.pseudopos.PseudoPositioner):
         hangs and blocks other operations.
 
         The solution is to have the PseudoPositioner object track the axis it
-        asked to move, instead of instead of trusting which object the callback
-        reports as done.
+        asked to move, instead of trusting which object the callback reports 
+        as done.
         """
         self._real_waiting.extend(self._real)
 
